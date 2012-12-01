cbc151a56bbb24e67610c88cb2a8932bfc254df8
acouch <acinternets@gmail.com>
2012-12-01 13:33:48 +0000

    Removing book module, updating forms and datasets

--- modules/dkan/dkan_forms/dkan_forms.js
+++ modules/dkan/dkan_forms/dkan_forms.js
@@ -11,10 +11,10 @@
     this.each(function () {
       $(this).addClass('compact-form-wrapper');
       var desc = $(this).find('.description').addClass('compact-form-description');
+      var input = $(this).find('input');
       desc.click(function () {
         input.focus();
       });
-      var input = $(this).find('input');
       if ($(input).html() == null) {
         var input = $(this).find('textarea');
       }
@@ -33,68 +33,63 @@
       }
     });
   }
+
+  /**
+   * Shows and hides a description for Autocomplete Deluxe form elements.
+   */
+  $.fn.dkanFormsAutoDeluxeHide = function () {
+    this.each(function () {
+      $(this).addClass('compact-form-wrapper');
+      var desc = $(this).find('.description').addClass('compact-form-description');
+      var input = $(this).find('#autocomplete-deluxe-input');
+      desc.click(function () {
+        input.focus();
+      });
+      input.keyup(function () {
+        if (input.val() != '') {
+          desc.hide();
+        }
+      });
+      if ($('#autocomplete-deluxe-item').html() != null) {
+        desc.css('display', 'none');
+      }
+      if ($(this).find('input').val() != '') {
+        desc.css('display', 'none');
+      }
+    });
+  }
+
+  Drupal.behaviors.dkanAddtional = {
+    attach: function (context, settings) {
+      console.log(settings.dkanAdditional);
+      if (settings.dkanAdditional && context.context) {
+        var pos = $('#zone-content-wrapper').offset();
+        $('html, body').animate({ scrollTop: pos.top}, 'fast');
+        window.history.pushState("", "", '/node/' +  settings.dkanAdditional.nid + '/edit?additional=1');
+        delete settings.dkanAdditional;
+      }
+    }
+  }
   Drupal.behaviors.dkanPush = {
     attach: function (context, settings) {
-      if (settings.dkanPush) {
-        //window.history.pushState("", "", '/node/add/resource?dataset=' + settings.dkanPush.nid);
+      console.log(settings.dkanPush);
+      if (settings.dkanPush && context.context) {
+        var pos = $('#zone-content-wrapper').offset();
+        $('html, body').animate({ scrollTop: pos.top}, 'fast');
+        window.history.pushState("", "", '/node/add/resource?dataset=' + settings.dkanPush.nid);
+        // Make sure this doesn't fire again.
+        delete settings.dkanPush;
       }
     }
   }
 
   Drupal.behaviors.dkanForms = {
     attach: function (context, settings) {
-      // Slugify!
-      if ($('#edit-path-alias').val() != '') {
-        $('#url-edit-preview').hide();
-      }
-      else {
-        // Initially hide the path until clicked.
-        $('#field-tags-wrapper .path-form').hide();
-        // Hidden by default in case js is disabled.
-        $('#url-edit-preview').show();
-        // Force URLs to be url friendly.
-        $('#edit-path-alias').slugify('#edit-path-alias');
-        // Only edit path alias if alias has not been edited.
-        $('.form-type-textfield input').click(function(e) {
-          $('#edit-path-alias').slugify('.form-type-textfield input');
-          $('#url-slug').slugify('.form-type-textfield input');
-        });
-        $('button.btn').click(function(e) {
-          e.preventDefault();
-          $('#url-edit-preview').hide();
-          $('#field-tags-wrapper .path-form').show();
-          $('#edit-path-alias').focus();
-          $('#edit-path-alias').addClass('processed');
-        });
-      }
 
-      // Resource list.
-      $('#block-dkan-forms-dkan-forms-resource-nodes ul li.last').slugify('.form-type-textfield input');
-
-      var elements = "#edit-field-link-file,#edit-field-link-api,.form-item-title,.field-name-body,#views-exposed-form-dataset-page";
+      var elements = "#edit-field-link-file,#edit-field-link-api,.field-name-body,#views-exposed-form-dataset-page";
       $(elements, context).dkanFormsHide();
-      $('#edit-field-tags .description').addClass('compact-form-description');
-      $('#edit-field-tags').addClass('compact-form-wrapper');
-      $('#edit-field-tags .description').click(function () {
-        $('#edit-field-tags .description').fadeOut('fast');
-      });
-      $('#autocomplete-deluxe-input').focus(function () {
-        $('#edit-field-tags .description').fadeOut('fast');
-      });
-      if ($('#autocomplete-deluxe-item').html() != null) {
-        $('#edit-field-tags .description').css('display', 'none');
-      }
-      $('#edit-field-format .description').addClass('compact-form-description');
-      $('#edit-field-format').addClass('compact-form-wrapper');
-      $('#edit-field-format .description').click(function () {
-        $('#edit-field-format .description').fadeOut('fast');
-      });
-      $('#autocomplete-deluxe-input').focus(function () {
-        $('#edit-field-format .description').fadeOut('fast');
-      });
-      if ($('#autocomplete-deluxe-item').html() != null) {
-        $('#edit-field-format .description').css('display', 'none');
-      }
+      var autoDeluxeElements = ".field-name-field-tags,#edit-field-format";
+      $(autoDeluxeElements, context).dkanFormsAutoDeluxeHide();
     }
   }
 
