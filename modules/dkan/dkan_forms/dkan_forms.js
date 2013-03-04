41b17a47e5757f3e55af2e903615d292cf86cca2
acouch <acinternets@gmail.com>
2013-03-04 08:55:14 +0000

    Form improvements

--- modules/dkan/dkan_forms/dkan_forms.js
+++ modules/dkan/dkan_forms/dkan_forms.js
@@ -86,10 +86,63 @@
   Drupal.behaviors.dkanForms = {
     attach: function (context, settings) {
 
+      // Autohide selected elements.
       var elements = "#edit-field-link-file,#edit-field-link-api,.field-name-body,#views-exposed-form-dataset-page,#block-dkan-dataset-dkan-dataset-search-bar";
       $(elements, context).dkanFormsHide();
       var autoDeluxeElements = ".field-name-field-tags";
       $(autoDeluxeElements, context).dkanFormsAutoDeluxeHide();
+
+      // Date module borks if you add a date without a time. This adds a default
+      // time if someone picks a date and time has been added yet.
+      if ($('#edit-field-temporal-coverage').length) {
+        var tempInputDate1 = $('#edit-field-temporal-coverage input:eq(1)');
+        var tempInputTime1 = $('#edit-field-temporal-coverage input:eq(2)');
+        var tempInputDate2 = $('#edit-field-temporal-coverage input:eq(3)');
+        var tempInputTime2 = $('#edit-field-temporal-coverage input:eq(4)');
+        $(tempInputDate1).click(function() {
+          if ($(tempInputTime1).val() == '') {
+            $(tempInputTime1).val('00:00:00');
+          }
+        });
+        $(tempInputDate2).click(function() {
+          if ($(tempInputTime2).val() == '') {
+            $(tempInputTime2).val('00:00:00');
+          }
+        });
+      }
+      $("#node_dataset_form_group_additional .description").each(function() {
+        dkanFormsBeautyTipPrepare(this, 'label');
+      });
+      $(".field-widget-leaflet-widget-widget .fieldset-description").appendTo($(".field-widget-leaflet-widget-widget fieldset"));
+      $(".field-widget-leaflet-widget-widget .fieldset-description").each(function() {
+        console.log(this);
+        dkanFormsBeautyTipPrepare(this, 'legend');
+      });
+      $(".field-type-datetime.field-name-field-temporal-coverage .fieldset-description").appendTo($(".field-type-datetime.field-name-field-temporal-coverage fieldset"));
+      $(".field-type-datetime.field-name-field-temporal-coverage .fieldset-description").each(function() {
+        console.log(this);
+        dkanFormsBeautyTipPrepare(this, 'legend');
+      });
+    }
+  }
+
+  function dkanFormsBeautyTipPrepare(that, label) {
+    var desc = $(that).html();
+    var link = $(that).find('a').attr('href');
+    if (link) {
+      $(that).html('');
+      $(that).prevAll(label).append('<a class="bt-pop" target="_blank" href="' + link + '" title="#"><i class="icon-small icon-external-link">#</i></a>');
+      $(that).prevAll(label).find("a.bt-pop").bt(desc, {
+       spikeLength: 20,
+       spikeGirth: 20,
+       cornerRadius: 5,
+       shrinkToFit: true,
+       padding: 10,
+       width: '400px',
+       fill: '#EAEAEA',
+       strokeWidth: 1,
+       strokeStyle: '#CCC',
+       cssStyles: {color: '#666', width: 'auto'}});
     }
   }
 
