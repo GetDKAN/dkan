f59346d29579f1299ef878e54daf8822fc3b8640
acouch <acinternets@gmail.com>
2013-04-15 17:14:57 +0000

    Updates from renaming

--- themes/dkan/js/front.js
+++ themes/dkan/js/front.js
@@ -6,10 +6,10 @@
   Drupal.behaviors.dkanFront = {
     attach: function (context) {
       // Fade in 'add dataset' block.
-      $('#block-dkan-front-dkan-add-front').delay(1500).fadeIn();
+      $('#block-dkan-sitewide-demo-front-dkan-add-front').delay(1500).fadeIn();
       // Remove 'add dataset' block.
-      $('#block-dkan-front-dkan-add-front a.close').click(function() {
-        $('#block-dkan-front-dkan-add-front').fadeOut();
+      $('#block-dkan-sitewide-demo-front-dkan-add-front a.close').click(function() {
+        $('#block-dkan-sitewide-demo-front-dkan-add-front').fadeOut();
       });
     }
   }
