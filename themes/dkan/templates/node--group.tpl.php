a9874cd1dfb28da03ba2496d80a6606bf04b56bf
Janette Day <janette@nuams.com>
2014-03-19 13:39:25 -0700

    Updating theme css and group template

--- themes/dkan/templates/node--group.tpl.php
+++ themes/dkan/templates/node--group.tpl.php
@@ -3,13 +3,16 @@
     $node = node_load($nid);
     //group image
     $image = field_get_items('node', $node, 'field_image');
-    $group_logo = field_view_value('node', $node, 'field_image', $image[0], array(
-      'type' => 'image',
-      'settings' => array(
-        'image_style' => 'group_medium',
-        'image_link' => 'content',
-      ),
-    ));
+    if(!empty($image)) {
+      $group_logo = field_view_value('node', $node, 'field_image', $image[0], array(
+        'type' => 'image',
+        'settings' => array(
+          'image_style' => 'group_medium',
+          'image_link' => 'content',
+          'image_alt' => 'logo',
+        ),
+      ));
+    }
   ?> 
   <article class="node-teaser">
     <div class="field-name-field-image"><?php print render($group_logo); ?></div>
@@ -29,34 +32,35 @@
 
 <?php else: ?>
 
-<article<?php print $attributes; ?>>
-  <?php print $user_picture; ?>
-  <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-  <header>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-  </header>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php if ($display_submitted): ?>
-  <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
-  <?php endif; ?>  
-  
-  <div<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-  
-  <div class="clearfix">
-    <?php if (!empty($content['links'])): ?>
-      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+  <article<?php print $attributes; ?>>
+    <?php print $user_picture; ?>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page && $title): ?>
+    <header>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+    </header>
     <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php if ($display_submitted): ?>
+    <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
+    <?php endif; ?>  
+    
+    <div<?php print $content_attributes; ?>>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+      ?>
+    </div>
+    
+    <div class="clearfix">
+      <?php if (!empty($content['links'])): ?>
+        <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+      <?php endif; ?>
 
-    <?php print render($content['comments']); ?>
-  </div>
-</article>
+      <?php print render($content['comments']); ?>
+    </div>
+  </article>
+  
 <?php endif; ?>
