570b8e4ebcc95d53aed2b6b4deb597ae0015ac20
Paolo Mainardi <paolo@twinbit.it>
2014-04-26 19:41:57 +0200

    disqus support

--- themes/dkan/templates/node--dataset.tpl.php
+++ themes/dkan/templates/node--dataset.tpl.php
@@ -14,17 +14,29 @@
   <div<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
       hide($content['links']);
+      if (module_esists('comment')) {
+        hide($content['comments']);
+      }
+      if (module_esists('disqus')) {
+        hide($content['disqus']);
+      }
       print render($content);
     ?>
   </div>
-  
+
   <div class="clearfix">
     <?php if (!empty($content['links'])): ?>
       <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
     <?php endif; ?>
 
-    <?php print render($content['comments']); ?>
+    <?php if (module_exists('comment') || module_exists('disqus')) : ?>
+      <h3> <?php print t('Comments') ?> </h3>
+      <?php if (module_exists('disqus')) : ?>
+        <?php print render($content['disqus']) ?>
+      <?php else : ?>
+        <?php print render($content['comments']); ?>
+      <?php endif; ?>
+    <?php endif; ?>
   </div>
 </article>
