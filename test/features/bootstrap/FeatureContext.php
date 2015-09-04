1a2b037e919669e63161cdce38c33eea7a61f1b5
Aaron Couch <acinternets@gmail.com>
2015-09-03 19:47:02 -0500

    Update FeatureContext.php
    * Adds steps for DKAN Dataset REST API

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -8,11 +8,13 @@ use Symfony\Component\Process\Process;
 use Behat\Gherkin\Node\TableNode;
 
 require 'vendor/autoload.php';
+include_once 'dkan_dataset_rest_api_crud.php';
 
 class FeatureContext extends DrupalContext
 {
   // Keep track of created data dashboards so they can be cleaned up.
   protected $data_dashboards = array();
+  private $dataset_nid;
 
   /**
    * @Given /^I scroll to the top$/
@@ -446,5 +448,70 @@ class FeatureContext extends DrupalContext
         $this->getDriver()->nodeDelete($data_dashboard);
       }
     }
+  }
+    /**
+   * @Given /^I use the Dataset REST API to create:$/
+   */
+  public function iUseTheDatasetRestApiToCreate($data) {
+    $base_url = $this->getMinkParameter('base_url');
+    $endpoint = '/api/dataset';
+    $entity = 'node';
+    $login_url = $base_url . $endpoint . '/user/login';
+    // Get cookie_session and csrf_token.
+    $user_login = dkan_dataset_services_user_login($login_url);
+    $cookie_session = $user_login['cookie_session'];
+    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
+    foreach ($data->getHash() as $node_data) {
+      // Node data.
+      $node_data = array(
+        'title' => $node_data['title'],
+        'type' => 'dataset',
+        'body[und][0][value]' => $node_data['body'],
+        'status' => $node_data['status'],
+      );
+      // Create node.
+      $node = dkan_dataset_services_create_node($node_data, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
+      $this->dataset_nid = $node->nid;
+    }
+    return true;
+  }
+  /**
+   * @Given /^I use the Dataset REST API to update "test dataset":$/
+   */
+  public function iUseTheDatasetRestApiToUpdateTestDataset($data) {
+    $base_url = $this->getMinkParameter('base_url');
+    $endpoint = '/api/dataset';
+    $entity = 'node';
+    $login_url = $base_url . $endpoint . '/user/login';
+    // Get cookie_session and csrf_token.
+    $user_login = dkan_dataset_services_user_login($login_url);
+    $cookie_session = $user_login['cookie_session'];
+    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
+    foreach ($data->getHash() as $node_data) {
+      // Node data.
+      $node_data = array(
+        'title' => 'Dataset updated',
+        'body[und][0][value]' => $node_data['body'],
+      );
+      // Update node.
+      $response = dkan_dataset_services_update_node($node_data, $this->dataset_nid, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
+    }
+    return true;
+  }
+  /**
+   * @Given /^I use the Dataset REST API to delete "Dataset updated":$/
+   */
+  public function iUseTheDatasetRestApiToDeleteDatasetUpdated() {
+    $base_url = $this->getMinkParameter('base_url');
+    $endpoint = '/api/dataset';
+    $entity = 'node';
+    $login_url = $base_url . $endpoint . '/user/login';
+    // Get cookie_session and csrf_token.
+    $user_login = dkan_dataset_services_user_login($login_url);
+    $cookie_session = $user_login['cookie_session'];
+    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
+   //Delete node.
+   $response = dkan_dataset_services_delete_node($this->dataset_nid, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
+    return true;
   }
 }
