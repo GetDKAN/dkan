fdacdb98ce8067b4438e64c2555d74c490ecaa5b
Frank Carey <frank@devinci.io>
2015-10-28 00:41:56 -0400

    circle-install.sh is failing silently, try some workarounds.

--- test/circle-install.sh
+++ test/circle-install.sh
@@ -5,6 +5,7 @@ if [ -f ~/backups/dkan-install.sql ]; then
 else
     # Do full installation
     echo "===> No cached database found at ~/backups/dkan-install.sql. Installing from scratch."
+    echo "drush si dkan --sites-subdir=default --db-url=$DATABASE_URL --account-name=admin --account-pass=admin  --site-name="DKAN" install_configure_form.update_status_module='array(FALSE,FALSE)' --yes"
     drush si dkan --sites-subdir=default --db-url=$DATABASE_URL --account-name=admin --account-pass=admin  --site-name="DKAN" install_configure_form.update_status_module='array(FALSE,FALSE)' --yes
     drush cc drush; drush cc all --yes
     #store cached backup
