cdd5d4532087441566dde3df86da84f39ebd5664
Frank Carey <frank@devinci.io>
2015-10-28 00:15:47 -0400

    Cache the installed database to save 1-2 minutes.

---
+++ test/circle-install.sh
@@ -0,0 +1,13 @@
+if [ -f ~/backups/dkan-install.sql ]; then
+    # Use the cached install backup.
+    echo "===> Loading install from cached database found at ~/backups/dkan-install.sql."
+    drush sql-drop -y && drush sqlc < ~/backups/dkan-install.sql
+else
+    # Do full installation
+    echo "===> No cached database found at ~/backups/dkan-install.sql. Installing from scratch."
+    drush si dkan --sites-subdir=default --db-url=$DATABASE_URL --account-name=admin --account-pass=admin  --site-name="DKAN" install_configure_form.update_status_module='array(FALSE,FALSE)' --yes
+    drush cc drush; drush cc all --yes
+    #store cached backup
+    mkdir -p ~/backups
+    drush sql-dump > ~/backups/dkan-install.sql
+fi
