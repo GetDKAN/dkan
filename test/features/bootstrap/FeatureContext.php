1722029b03c304aca3c3240ce1abc343e34092c0
Frank Carey <frank@devinci.io>
2016-02-04 16:12:24 -0500

    Add back an empty FeatureContext.php on the request of @dafeder it make it easier to experiment locally with custom steps. Not to be used for long term changes though.

---
+++ test/features/bootstrap/FeatureContext.php
@@ -0,0 +1,14 @@
+<?php
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends RawDrupalContext
+{
+
+// This file is only meant for temporary custom step functions or overrides to the dkanextension.
+// Changes should be implemented in dkanextension so that it works across all projects.
+
+}
