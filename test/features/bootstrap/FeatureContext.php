535b44aee27bca50bdc04d44156e93ca3bf3d285
Sol Villar <msol.villar@gmail.com>
2016-04-11 11:11:56 -0300

    Moved custom Dataset REST API steps to Dkanextension.

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -1,14 +1,11 @@
 <?php
 use Drupal\DKANExtension\Context\RawDKANContext;
 
-include_once 'dkan_dataset_rest_api_crud.php';
-
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends RawDKANContext
 {
-  private $dataset_nid;
   protected $old_global_user;
   // This file is only meant for temporary custom step functions or overrides to the dkanextension.
   // Changes should be implemented in dkanextension so that it works across all projects.
@@ -71,71 +68,5 @@ class FeatureContext extends RawDKANContext
     if (null === $element) {
       throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $item));
     }
-  }
-
-  /**
-    * @Given /^I use the Dataset REST API to create:$/
-    */
-  public function iUseTheDatasetRestApiToCreate($data) {
-    $base_url = $this->getMinkParameter('base_url');
-    $endpoint = '/api/dataset';
-    $entity = 'node';
-    $login_url = $base_url . $endpoint . '/user/login';
-    // Get cookie_session and csrf_token.
-    $user_login = dkan_dataset_services_user_login($login_url);
-    $cookie_session = $user_login['cookie_session'];
-    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
-    foreach ($data->getHash() as $node_data) {
-      // Node data.
-      $node_data = array(
-        'title' => $node_data['title'],
-        'type' => 'dataset',
-        'body[und][0][value]' => $node_data['body'],
-        'status' => $node_data['status'],
-      );
-      // Create node.
-      $node = dkan_dataset_services_create_node($node_data, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
-      $this->dataset_nid = $node->nid;
-    }
-    return true;
-  }
-  /**
-   * @Given /^I use the Dataset REST API to update "test dataset":$/
-   */
-  public function iUseTheDatasetRestApiToUpdateTestDataset($data) {
-    $base_url = $this->getMinkParameter('base_url');
-    $endpoint = '/api/dataset';
-    $entity = 'node';
-    $login_url = $base_url . $endpoint . '/user/login';
-    // Get cookie_session and csrf_token.
-    $user_login = dkan_dataset_services_user_login($login_url);
-    $cookie_session = $user_login['cookie_session'];
-    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
-    foreach ($data->getHash() as $node_data) {
-      // Node data.
-      $node_data = array(
-        'title' => 'Dataset updated',
-        'body[und][0][value]' => $node_data['body'],
-      );
-      // Update node.
-      $response = dkan_dataset_services_update_node($node_data, $this->dataset_nid, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
-    }
-    return true;
-  }
-  /**
-   * @Given /^I use the Dataset REST API to delete "Dataset updated":$/
-   */
-  public function iUseTheDatasetRestApiToDeleteDatasetUpdated() {
-    $base_url = $this->getMinkParameter('base_url');
-    $endpoint = '/api/dataset';
-    $entity = 'node';
-    $login_url = $base_url . $endpoint . '/user/login';
-    // Get cookie_session and csrf_token.
-    $user_login = dkan_dataset_services_user_login($login_url);
-    $cookie_session = $user_login['cookie_session'];
-    $csrf_token = dkan_dataset_services_get_csrf($cookie_session, $user_login['curl'], $base_url);
-   //Delete node.
-   $response = dkan_dataset_services_delete_node($this->dataset_nid, $csrf_token, $cookie_session, $base_url, $endpoint, $entity);
-    return true;
   }
 }
