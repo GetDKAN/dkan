34cc5fddefb9ac3dc664a097ae1c2592c4c101cd
Sol Villar <msol.villar@gmail.com>
2016-04-12 13:47:17 -0300

    Moved POD related custom steps to PODContext on dkanextension.

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -80,13 +80,5 @@ class FeatureContext extends RawDKANContext
         throw new \InvalidArgumentException(sprintf('Could not evaluate CSS selector: "%s"', $item));
     }
   }
-  /**
-   * @When I should see valid data.json on the /data.json page
-   */
-  public function iShouldSeeValidDatasjonOnTheDatajsonPage() {
-    $results = open_data_schema_pod_process_validate($this->getMinkParameter('base_url') . '/data.json', TRUE);
-    if ($results['errors']) {
-      throw new Exception(sprintf('Data.json is not valid.'));
-    }
-  }
+
 }
