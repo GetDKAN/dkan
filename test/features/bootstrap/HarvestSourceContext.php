08ba629487321cb7572d4ddc86b8d5caf9026c81
Riadh Habbachi <habbachi.riadh@gmail.com>
2016-06-24 13:51:00 +0100

    Add harvest behat custom steps
    List of added steps:
    * Given The :machine_name source is harvested
    * Given I should see a harvest event log table
    * Then the harvest event log table should have :number row(s)

--- test/features/bootstrap/HarvestSourceContext.php
+++ test/features/bootstrap/HarvestSourceContext.php
@@ -33,4 +33,76 @@ class HarvestSourceContext extends RawDKANEntityContext {
     parent::addMultipleFromTable($harvestSourcesTable);
   }
 
+  /**
+   * Run a harvest on a harvest source.
+   *
+   * @param $machine_name harvest source machine name
+   *
+   * @Given The :machine_name source is harvested
+   *
+   * @throw \Exception
+   */
+  public function theHarvestSourceIsHarvested($machine_name) {
+    $harvest_source = HarvestSource::getSourceByMachineName($machine_name);
+    if (!$harvest_source) {
+      throw new \Exception("Harvest source '$machine_name' not found.");
+    }
+
+    global $user;
+    // Save the original user to set it back later
+    $global_user = $user;
+
+    $user = user_load(1);
+
+    // Harvest Cache
+    dkan_harvest_cache_sources(array($harvest_source));
+    // Harvest Migration of the test data.
+    dkan_harvest_migrate_sources(array($harvest_source));
+
+    // Back global user to the original user. Probably an anonymous.
+    $user = $global_user;
+  }
+
+  /**
+   * Retrieve a table row containing specified text from a given element.
+   *
+   * @Given I should see a harvest event log table
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *
+   * @throws \Exception
+   */
+  public function assertHarvestEventLogTable() {
+    $page = $this->getSession()->getPage();
+    $table = $page->findAll('css', 'table.harvest-event-log');
+    if (empty($table)) {
+      throw new \Exception(sprintf('No Harvest Event Log table found on the page %s', $this->getSession()->getCurrentUrl()));
+    }
+
+// The current use case
+    return array_pop($table);
+  }
+
+  /**
+   * Check on the number of rows a harvest event log should have.
+   *
+   * @Then the harvest event log table should have :number row(s)
+   *
+   * @throws \Exception
+   */
+  public function assertHarvestEventLogTableRowNumber($number) {
+    if (!is_numeric($number)) {
+      throw new \Exception(sprintf('Expected "number" to be numeric'));
+    }
+
+    $table = $this->assertHarvestEventLogTable();
+    $rows = $table->findAll('css', 'tr');
+
+    // The first row is for the header. bump.
+    array_pop($rows);
+
+    if (count($rows) != $number) {
+          throw new \Exception(sprintf('Found %s rows in the harvest event log table instead of the expected %s.', count($rows), $number));
+    }
+  }
 }
