9c469907b5724108bf6ff0a6c3ccca4ae24a7aad
Janette Day <janette@nucivic.com>
2016-10-06 15:42:23 -0500

    Clean up dkan_datastore files, update changelog to incluce dkan_datastore info

--- test/phpunit/boot.php
+++ test/phpunit/boot.php
@@ -7,7 +7,6 @@
 use Drupal\Driver\DrupalDriver;
 use Drupal\Driver\Cores\Drupal7;
 
-<<<<<<< HEAD
 require __DIR__ . '/../vendor/autoload.php';
 
 // Path to Drupal.
@@ -17,17 +16,7 @@ $dir = implode('/', array(__DIR__, '..', '..', 'docroot'));
 $uri = getenv('DKAN_WEB_1_PORT_80_TCP') ? str_replace('tcp://', 'http://', getenv('DKAN_WEB_1_PORT_80_TCP')) : 'http://127.0.0.1:8888';
 
 $driver = new DrupalDriver($dir, $uri);
-=======
-require '../../vendor/autoload.php';
 
-// Path to Drupal.
-$path = '/var/www/docroot';
-
-// Host.
-$uri = 'http://web';
-
-$driver = new DrupalDriver($path, $uri);
->>>>>>> datastore-merge
 $driver->setCoreFromVersion();
 
 // Bootstrap Drupal.
