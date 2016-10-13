5d25b04ef5675751834b8a5e184a1bfef9b5a2b6
Mariano Carballal <marianocarballal@gmail.com>
2016-10-13 17:33:54 -0300

    Add circle behat balancer

--- .ahoy/.scripts/circle-behat.rb
+++ .ahoy/.scripts/circle-behat.rb
@@ -4,18 +4,18 @@
 # Ex. There are 3 VMs setup: (3)
 require "pathname"
 require "./dkan/.ahoy/.scripts/behat-parse-params.rb"
+require "./dkan/.ahoy/.scripts/circle-behat-balancer.rb"
 require "pp"
 
-unless ENV.has_key?("CIRCLE_NODE_INDEX")
-  puts "No parrallelism found, setting defaults to run all tests."
-  CIRCLE_NODE_TOTAL=1
-  CIRCLE_NODE_INDEX=0
-else 
-  CIRCLE_NODE_TOTAL = ENV['CIRCLE_NODE_TOTAL'].to_i
-  CIRCLE_NODE_INDEX = ENV['CIRCLE_NODE_INDEX'].to_i
-end
-
 CIRCLE_ARTIFACTS =  ENV.has_key?("CIRCLE_ARTIFACTS") ? ENV['CIRCLE_ARTIFACTS'] : "assets"
+CIRCLE_NODE_TOTAL = node_total()
+CIRCLE_NODE_INDEX = node_index()
+TOKENS = {
+  "s" => (1),
+  "m" => (60),
+  "h" => (60 * 60),
+  "d" => (60 * 60 * 24)
+}
 
 puts "CIRCLE_NODE_TOTAL = #{CIRCLE_NODE_TOTAL}"
 puts "CIRCLE_NODE_INDEX = #{CIRCLE_NODE_INDEX}"
@@ -23,22 +23,20 @@ puts "CIRCLE_NODE_INDEX = #{CIRCLE_NODE_INDEX}"
 
 error = 0
 parsed = behat_parse_params(ARGV)
-files = parsed[:files]
 params = behat_join_params parsed[:params]
+files = balance(parsed[:files])
 
-files.each_index do |i|
-  if (i % CIRCLE_NODE_TOTAL)  == CIRCLE_NODE_INDEX
-    file = Pathname(files[i]).realpath.to_s
-    puts "RUNNING: ahoy dkan test #{file} --format=pretty --out=std --format=junit --out='#{CIRCLE_ARTIFACTS}/junit' #{params} --colors"
-    IO.popen(
-    "ahoy dkan test #{file} --format=pretty --out=std --format=junit --out='#{CIRCLE_ARTIFACTS}/junit' #{params} --colors"
-    ) do |io|
-      while line = io.gets
-        print line
-      end
-      io.close
-      error = 1 unless $?.success?
+files[CIRCLE_NODE_INDEX].each_index do |i|
+  file = Pathname(files[CIRCLE_NODE_INDEX][i]).realpath.to_s
+  puts "RUNNING: ahoy dkan test #{file} --format=pretty --out=std --format=junit --out='#{CIRCLE_ARTIFACTS}/junit' #{params} --colors"
+  IO.popen(
+  "ahoy dkan test #{file} --format=pretty --out=std --format=junit --out='#{CIRCLE_ARTIFACTS}/junit' #{params} --colors"
+  ) do |io|
+    while line = io.gets
+      print line
     end
+    io.close
+    error = 1 unless $?.success?
   end
 end
 
