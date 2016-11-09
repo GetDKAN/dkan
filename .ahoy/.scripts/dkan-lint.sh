378e6b95c38401c7de077a97d65574897f34b27f
dkinzer <dtkinzer@gmail.com>
2016-11-08 22:00:20 -0500

    Fix curl call.

--- .ahoy/.scripts/dkan-lint.sh
+++ .ahoy/.scripts/dkan-lint.sh
@@ -6,7 +6,7 @@ bash .ahoy/.scripts/composer-install.sh test
 test/bin/phpcs --config-set installed_paths test/vendor/drupal/coder/coder_sniffer
 
 if [ $CI ]; then
-  files=curl -s  $CIRCLE_REPOSITORY_URL/compare/7.x-1.x...$CIRCLE_SHA1.diff | grep +++ | sed 's/+++ b\///g'
+  files=`curl -s  $CIRCLE_REPOSITORY_URL/compare/7.x-1.x...$CIRCLE_SHA1.diff | grep +++ | sed 's/+++ b\///g'`
 fi
 
 if [ ! -z "$1" ]; then
