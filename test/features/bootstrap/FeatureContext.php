b4aa82fb850eecf0fb10cc72c9dd4cd5d91bc2f9
Mariano Carballal <marianocarballal@gmail.com>
2016-12-06 22:28:44 +0200

    Add cache support for recline previews (#1573)
    * Use Point dkan to branch recline cache embed #CIVIC-4089
    
    * Update tests and composer
    
    * Fix composer.lock
    
    * Add javascript tags
    
    * Point to recline make
    
    * Update composer lock
    
    * Update drupal-org.make
    
    * Updated tests.
    
    * Updated dkanextension

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -1,13 +1,13 @@
 <?php
 
 use Drupal\DKANExtension\Context\RawDKANContext;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends RawDKANContext
 {
-
   protected $old_global_user;
 
   // This file is only meant for temporary custom step functions or overrides to the dkanextension.
