4a91b93518c3e2e5c91def1e226d88eba0880846
Riadh Habbachi <rhabbachi@users.noreply.github.com>
2017-01-16 22:30:20 +0100

    [CIVIC-5161] Harvest will not process Resources URL that does not have a scheme (#1644)
    * Refactor harvest phpunit tests for better cleanup
    
    * Re-enable commented Harvest PHPUnit tests
    
    * Support schemeless URLs during resources harvest
    
    * Refactor and add tests for schemeless resource
    
    * Drop the "https://" from the schemeless data.json
    
    * Switch the last tests order

--- test/phpunit/dkan_harvest/DatajsonHarvestMigrationScenariosTest.php
+++ test/phpunit/dkan_harvest/DatajsonHarvestMigrationScenariosTest.php
@@ -486,6 +486,32 @@ class DatajsonHarvestMigrationScenariosTest extends PHPUnit_Framework_TestCase {
   }
 
   /**
+   * Test remote file support for URLs without scheme.
+   *
+   * Ticket: https://jira.govdelivery.com/browse/CIVIC-5161
+   */
+  public function testResourceSchemeless() {
+    // Harvest a source that have resources without scheme.
+    dkan_harvest_cache_sources(array(self::getResourceSchemeless()));
+    dkan_harvest_migrate_sources(array(self::getResourceSchemeless()));
+
+    // Get imported dataset.
+    $dataset_nids = $this->getTestDatasetNid(self::getResourceSchemeless());
+    $dataset_node = entity_metadata_wrapper('node', array_pop($dataset_nids));
+
+    // One resource should exists.
+    $this->assertEquals(count($dataset_node->field_resources), 1);
+
+    $migration = dkan_harvest_get_migration(self::getResourceSchemeless());
+    $migrationMap = $this->getMapTableFromMigration($migration);
+    $migrationLog = $this->getLogTableFromMigration($migration);
+    $migrationMessage = $this->getMessageTableFromMigration($migration);
+
+    // No error should be recorded.
+    $this->assertEmpty($migrationMessage);
+  }
+
+  /**
    * Check Error logging for the harvest.
    *
    * Ticket: https://jira.govdelivery.com/browse/CIVIC-4498
@@ -495,7 +521,6 @@ class DatajsonHarvestMigrationScenariosTest extends PHPUnit_Framework_TestCase {
    * drops some taxonomies which will make following tests fail.
    */
   public function testHarvestError() {
-    return;
     // Delete the format vocabulary.
     $vocab_format = taxonomy_vocabulary_machine_name_load('format');
     if ($vocab_format) {
