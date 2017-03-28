ee00d390970bb6f0e65c9423b0a51440d0825322
dkinzer <dtkinzer@gmail.com>
2017-03-28 17:24:35 -0400

    Do not override CIRCLECI linting config.

--- .ahoy/.scripts/dkan-lint.sh
+++ .ahoy/.scripts/dkan-lint.sh
@@ -5,10 +5,6 @@ bash .ahoy/.scripts/composer-install.sh test
 
 test/bin/phpcs --config-set installed_paths test/vendor/drupal/coder/coder_sniffer
 
-if [ $CI ]; then
-  files=`curl -s  $CIRCLE_REPOSITORY_URL/compare/7.x-1.x...$CIRCLE_SHA1.diff | grep +++ | sed 's/+++ b\///g'`
-fi
-
 if [ ! -z "$1" ]; then
   files="$files $@"
 fi
@@ -17,6 +13,11 @@ if [ ! -z "$files" ]; then
   files=`git diff --name-only`
 fi
 
+if [ $CI ]; then
+  files=`curl -s  $CIRCLE_REPOSITORY_URL/compare/7.x-1.x...$CIRCLE_SHA1.diff | grep +++ | sed 's/+++ b\///g'`
+fi
+
 if [ ! -z "$files" ]; then
+  echo "Linting: $files \n"
   test/bin/phpcs --standard=Drupal,DrupalPractice -n $files
 fi
