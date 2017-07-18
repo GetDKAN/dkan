e955e4a2ca6976d4c2fb3685504ba43204d7bc86
Sol Villar <msol.villar@gmail.com>
2017-07-18 10:36:55 -0300

    Moved step out from Feature Context

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -12,22 +12,5 @@ class FeatureContext extends RawDKANContext
 
   // This file is only meant for temporary custom step functions or overrides to the dkanextension.
   // Changes should be implemented in dkanextension so that it works across all projects.
-  /**
-   * Assert selector count in given region.
-   *
-   * @Then I should see exactly :number :selector in region :region
-   */
-  public function iShouldSeeExactlyInSelectorInRegion($number, $selector, $region) {
-    $session = $this->getSession();
-    $region_obj = $session->getPage()->find('region', $region);
-    if ($region_obj == null) {
-      throw new \Exception(sprintf('The region "%s" was not found on the page %s', $region, $this->getSession()->getCurrentUrl()));
-    }
-    $elements = $region_obj->findAll('css', $selector);
-    $count = count($elements);
-    if ($count != $number) {
-      throw new \Exception(sprintf('The selector "%s" was found %d times in the %s region on the page %s', $selector, $count, $region, $this->getSession()->getCurrentUrl()));
-    }
-  }
 
 }
