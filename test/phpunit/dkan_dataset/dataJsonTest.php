40b5293269888e689b539873263c33b986a5f11b
Dan Gurin <dan@dangur.com>
2017-07-25 11:45:14 -0400

    removes data. from json file and switches back to file get contents function

--- test/phpunit/dkan_dataset/dataJsonTest.php
+++ test/phpunit/dkan_dataset/dataJsonTest.php
@@ -9,9 +9,9 @@ class dataJsonTest extends PHPUnit_Framework_TestCase {
   public function testDataJson()
   {
     $base_url = 'http://127.0.0.1:8888/';
-    $path = 'jason';
+    $path = 'json';
     $file = $base_url . $path;
-    $contents = file($file);
+    $contents = file_get_contents($file);
     $decoded = json_decode($contents, TRUE);
     // Asserts that landingPage key is in data.json file.
     $this->assertArrayHasKey('landingPage', $decoded['dataset'][0]);
