dd1db827f723c1f5d5624df4d487c7fbf7c90289
Dan Feder <dafeder@gmail.com>
2017-07-26 10:20:35 -0400

    Try restoring data.json path to dataJsonTest.php

--- test/phpunit/dkan_dataset/dataJsonTest.php
+++ test/phpunit/dkan_dataset/dataJsonTest.php
@@ -9,9 +9,9 @@ class dataJsonTest extends PHPUnit_Framework_TestCase {
   public function testDataJson()
   {
     $base_url = 'http://127.0.0.1:8888/';
-    $path = 'json';
+    $path = 'data.json';
     $file = $base_url . $path;
-    $contents = file_get_contents($file);
+    $contents = file_get_contetns($file);
     $decoded = json_decode($contents, TRUE);
     // Asserts that landingPage key is in data.json file.
     $this->assertArrayHasKey('landingPage', $decoded['dataset'][0]);
