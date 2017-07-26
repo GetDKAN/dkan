36a82a6e193c7ce6d6d42834d71dd78b278f1816
dkinzer <dtkinzer@gmail.com>
2017-07-26 17:22:05 -0300

    Fix linter.

--- test/phpunit/dkan_dataset/dataJsonTest.php
+++ test/phpunit/dkan_dataset/dataJsonTest.php
@@ -1,19 +1,23 @@
 <?php
 
 /**
- * Class dataJsonTest
+ * Class DataJsonTest.
  *
  * Tests for landingPage key in /data.json file datasets.
  */
-class dataJsonTest extends PHPUnit_Framework_TestCase {
-  public function testDataJson()
-  {
+class DataJsonTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Verify data.json exists.
+   */
+  public function testDataJson() {
     $base_url = 'http://127.0.0.1:8888/';
     $path = 'data.json';
     $file = $base_url . $path;
-    $contents = file_get_contetns($file);
+    $contents = file_get_contents($file);
     $decoded = json_decode($contents, TRUE);
     // Asserts that landingPage key is in data.json file.
     $this->assertArrayHasKey('landingPage', $decoded['dataset'][0]);
   }
+
 }
