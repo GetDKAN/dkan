4429e4604eb15a0be92085675426c78b3b60cd7f
Dan Feder <dafeder@gmail.com>
2017-07-27 10:25:42 -0400

    Add ODSM memory settings to circle.settings.php

--- test/circle.settings.php
+++ test/circle.settings.php
@@ -19,3 +19,16 @@ $databases = array (
     ),
   ),
 );
+
+// Conditionally manage memory.
+$high_memory_paths = array();
+
+// ODSM edit forms.
+$high_memory_paths[] = 'admin/config/services/odsm/edit';
+// Standarize node edit paths for validation.
+$current_path = preg_replace("/\/\d+/", '/%node', $_GET['q']);
+foreach ($high_memory_paths as $high_memory_path) {
+  if ((strpos($current_path, $high_memory_path) === 0)) {
+    ini_set('memory_limit', '512M');
+  }
+}
