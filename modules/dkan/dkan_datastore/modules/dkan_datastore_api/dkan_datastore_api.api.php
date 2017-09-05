54a361d6d146f6e4d691e954e9030c3f41cdbcea
Kristian Ducharme <kducharm@3072643.no-reply.drupal.org>
2017-09-05 12:11:49 -0700

    Added hook_dkan_datastore_api_field_excluded_alter

---
+++ modules/dkan/dkan_datastore/modules/dkan_datastore_api/dkan_datastore_api.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by DKAN Datastore API module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Allows fields exclusions to be altered for API queries.
+ *
+ * @param array $excludes
+ *   List of fields to exclude.
+ */
+function hook_dkan_datastore_api_field_excluded_alter(&$excludes) {
+  $excludes[] = 'exclude_this_field';
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
