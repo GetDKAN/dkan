c5caae2de489c59f6593d6c777e70f1cc252e798
Ethan Teague <metrowebz@gmail.com>
2018-02-08 08:11:38 -0500

    Excluding features files from phpcs

--- .ahoy/.scripts/dkan-lint.sh
+++ .ahoy/.scripts/dkan-lint.sh
@@ -20,7 +20,7 @@ fi
 
 if [ ! -z "$files" ]; then
   echo "Linting: $files"
-  test/bin/phpcs --standard=Drupal,DrupalPractice -n $files --ignore=test/dkanextension/*
+  test/bin/phpcs --standard=Drupal,DrupalPractice -n $files --ignore=test/dkanextension/*,*.features*,*.facetapi_defaults*,*.views_default*
 else
   echo "No Drupal file changes available for linting."
 fi
