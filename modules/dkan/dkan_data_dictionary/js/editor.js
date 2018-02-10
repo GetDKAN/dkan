8733bc818ffb119ff7888b8fab84924793d7063d
Dan Feder <dafeder@gmail.com>
2018-02-10 08:41:36 -0500

    Some cleanup, add to dkan.info

--- modules/dkan/dkan_data_dictionary/js/editor.js
+++ modules/dkan/dkan_data_dictionary/js/editor.js
@@ -7,6 +7,7 @@
 
   Drupal.behaviors.jsonEditor = {
     attach: function (context) {
+      // Get the existing value
       var field = document.getElementById('edit-field-conformsto-schema-und-0-value');
       var json;
       try {
@@ -14,16 +15,20 @@
       } catch(e) {
         console.warn('An error ocurred trying to parse the dictionary schema.');
       }
+      // Insert the editor
       var container = document.getElementById('field-conformsto-schema-add-more-wrapper');
       var options = {
         mode: 'code',
         modes: ['code', 'form', 'tree']
       };
       var editor = new JSONEditor(container, options);
+      // Store reference to object for easier manipulation of API
       container.jsoneditor = editor;
       editor.set(json);
+      // Remove the old field
       $('.form-item-field-conformsto-schema-und-0-value .resizable-textarea').css({display: "none"});
 
+      // Submit!
       $('#resource-node-form').submit(function( event ) {
         var json = editor.get();
         field.value = JSON.stringify(json);
