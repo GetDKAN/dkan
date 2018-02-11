647f49c97f49625422ff0c72bdc0f6ce3624ea44
Dan Feder <dafeder@gmail.com>
2018-02-11 11:36:28 -0500

    Fix JS to run only once

--- modules/dkan/dkan_data_dictionary/js/editor.js
+++ modules/dkan/dkan_data_dictionary/js/editor.js
@@ -6,7 +6,7 @@
 (function ($) {
 
   Drupal.behaviors.jsonEditor = {
-    attach: function (context) {
+    attach: function (context, settings) {
       // Get the existing value
       var field = document.getElementById('edit-field-conformsto-schema-und-0-value');
       var json;
@@ -16,17 +16,19 @@
         console.warn('An error ocurred trying to parse the dictionary schema.');
       }
       // Insert the editor
-      var container = document.getElementById('field-conformsto-schema-add-more-wrapper');
-      var options = {
-        mode: 'code',
-        modes: ['code', 'form', 'tree']
-      };
-      var editor = new JSONEditor(container, options);
-      // Store reference to object for easier manipulation of API
-      container.jsoneditor = editor;
-      editor.set(json);
-      // Remove the old field
-      $('.form-item-field-conformsto-schema-und-0-value .resizable-textarea').css({display: "none"});
+      $('#field-conformsto-schema-add-more-wrapper', context).once(function(){
+        var container = document.getElementById('field-conformsto-schema-add-more-wrapper');
+        var options = {
+          mode: 'code',
+          modes: ['code', 'form', 'tree']
+        };
+        var editor = new JSONEditor(container, options);
+        // Store reference to object for easier manipulation of API
+        container.jsoneditor = editor;
+        editor.set(json);
+        // Remove the old field
+        $('.form-item-field-conformsto-schema-und-0-value .resizable-textarea').css({display: "none"});
+      });
 
       // Submit!
       $('#resource-node-form').submit(function( event ) {
