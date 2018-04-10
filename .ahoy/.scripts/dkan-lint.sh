5caab83ebfb6ba91385b75cc4150e6f5c204d363
Janette Day <janette.day@civicactions.com>
2018-04-10 12:18:21 -0500

    Run lint before running tests

--- .ahoy/.scripts/dkan-lint.sh
+++ .ahoy/.scripts/dkan-lint.sh
@@ -15,7 +15,7 @@ fi
 
 if [ "$CI_PULL_REQUEST" ]; then
   echo Diff URL: "$CI_PULL_REQUEST".diff
-  files=`curl -sL "$CI_PULL_REQUEST".diff | grep "^+++" | sed 's/+++ b\///g' | grep "$DRUPAL_FILES"`
+  files=`GET "$CI_PULL_REQUEST"/files | grep "^+++" | sed 's/+++ b\///g' | grep "$DRUPAL_FILES"`
 fi
 
 if [ ! -z "$files" ]; then
