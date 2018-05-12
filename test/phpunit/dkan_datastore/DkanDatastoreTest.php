35c92e667433161af276e105e2e552eaa5883b3c
fmizzell <fmizzell@1312210.no-reply.drupal.org>
2018-05-12 15:09:16 -0500

    Adding some tests, cleaning the public API, and adding autoloading configuration to the circle ci build.

--- test/phpunit/dkan_datastore/DkanDatastoreTest.php
+++ test/phpunit/dkan_datastore/DkanDatastoreTest.php
@@ -1,58 +1,53 @@
 <?php
 
-/**
- * @file
- * Base phpunit tests for DkanDatastoreFastImport class.
- */
+use Dkan\Datastore\Manager\SimpleImport;
 
 /**
- * DkanDatastoreFastImport class.
+ * Class DkanDatastoreTest.
  */
 class DkanDatastoreTest extends \PHPUnit_Framework_TestCase {
 
-  /**
-   * Test updateFromFileUri.
-   *
-   * @covers DkanDatastore::updateFromFileUri
-   */
-  public function testUpdateFromFileUri() {
-    // Create a stub for the DkanDatastore class.
-    $dkanDatastorestub = $this->getMockBuilder(DkanDatastore::class)
-      ->setMethods(['updateFromFile'])
-      ->disableOriginalConstructor()
-      ->disableOriginalClone()
-      ->getMock();
+  public function test() {
+    print_r(PHP_EOL);
 
-    // Configure the stub.
-    $dkanDatastorestub->expects($this->once())
-      ->method('updateFromFile')
-      ->willReturn(TRUE);
+    $resource = new \Dkan\Datastore\Resource("999999", __DIR__ . "/data/countries.csv");
 
-    $testfileuri = __DIR__ . '/data/countries.csv';
-    // Calling $stub->doSomething() will now return
-    // 'foo'.
-    $this->assertEquals(TRUE, $dkanDatastorestub->updateFromFileUri($testfileuri));
-  }
+    /* @var $datastore \Dkan\Datastore\Manager\SimpleImport */
+    $datastore = \Dkan\Datastore\Manager\Factory::create($resource, SimpleImport::class);
 
-  /**
-   * Test updateFromFileUri when no file is provided.
-   *
-   * @covers DkanDatastore::updateFromFileUri
-   */
-  public function testUpdateFromFileUriNoFile() {
-    // Create a stub for the DkanDatastore class.
-    $dkanDatastorestub = $this->getMockBuilder(DkanDatastore::class)
-      ->setMethods(['updateFromFile'])
-      ->disableOriginalConstructor()
-      ->disableOriginalClone()
-      ->getMock();
+    $status = $datastore->getStatus();
+    $this->assertEquals(SimpleImport::STORAGE_UNINITIALIZED, $status['storage']);
+    $this->assertEquals(SimpleImport::DATA_IMPORT_UNINITIALIZED, $status['data_import']);
 
-    // Configure the DkanDatastorestub method.
-    $dkanDatastorestub->expects($this->never())
-      ->method('updateFromFile')
-      ->willReturn(TRUE);
+    $datastore->import();
 
-    $this->assertEquals(FALSE, $dkanDatastorestub->updateFromFileUri());
+    $status = $datastore->getStatus();
+    $this->assertEquals(SimpleImport::STORAGE_INITIALIZED, $status['storage']);
+    $this->assertEquals(SimpleImport::DATA_IMPORT_DONE, $status['data_import']);
+
+
+    $query = db_query("SELECT * FROM {$datastore->getTableName()};");
+    $results = $query->fetchAllAssoc("country");
+    $json = json_encode($results);
+    $this->assertEquals(
+      "{\"US\":{\"country\":\"US\",\"population\":\"315209000\",\"id\":\"1\",\"timestamp\":\"1359062329\"},\"CA\":{\"country\":\"CA\",\"population\":\"35002447\",\"id\":\"2\",\"timestamp\":\"1359062329\"},\"AR\":{\"country\":\"AR\",\"population\":\"40117096\",\"id\":\"3\",\"timestamp\":\"1359062329\"},\"JP\":{\"country\":\"JP\",\"population\":\"127520000\",\"id\":\"4\",\"timestamp\":\"1359062329 \"}}",
+      $json);
+
+    $this->assertEquals(4, $datastore->numberOfRecordsImported());
+
+    $datastore->deleteRows();
+    $this->assertEquals(0, $datastore->numberOfRecordsImported());
+
+    $status = $datastore->getStatus();
+    $this->assertEquals(SimpleImport::STORAGE_INITIALIZED, $status['storage']);
+    $this->assertEquals(SimpleImport::DATA_IMPORT_UNINITIALIZED, $status['data_import']);
+
+    $datastore->drop();
+    $this->assertFalse(db_table_exists($datastore->getTableName()));
+
+    $status = $datastore->getStatus();
+    $this->assertEquals(SimpleImport::STORAGE_UNINITIALIZED, $status['storage']);
+    $this->assertEquals(SimpleImport::DATA_IMPORT_UNINITIALIZED, $status['data_import']);
   }
 
 }
