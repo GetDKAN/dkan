d8f2949c3e670561db14b0385d8c39082f6a2a3a
Janette Day <janette.day@civicactions.com>
2018-07-18 21:40:39 -0500

    Remove unneccessary options

--- modules/dkan/dkan_dataset/includes/getRemoteFileInfo.php
+++ modules/dkan/dkan_dataset/includes/getRemoteFileInfo.php
@@ -171,11 +171,9 @@ class GetRemoteFileInfo {
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
     curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
     curl_setopt($ch, CURLOPT_WRITEFUNCTION, $writefn);
     $data = curl_exec($ch);
     curl_close($ch);
