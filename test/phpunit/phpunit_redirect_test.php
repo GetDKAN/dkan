ffb98b5395eae8f724cf13ee2fd8a110699cb479
Janette Day <janette.day@civicactions.com>
2018-07-25 19:19:07 -0500

    Add test for the 255 char limit fallback to effective url

--- test/phpunit/phpunit_redirect_test.php
+++ test/phpunit/phpunit_redirect_test.php
@@ -1,6 +1,6 @@
 <?php
 /* Redirect browser */
-header("Location: http://demo.getdkan.com/sites/default/files/Polling_Places_Madison_0_0.csv");
+header("Location: http://demo.getdkan.com/profiles/dkan/test/files/dkan/Polling_Places_Madison_test.csv");
 
 exit;
 ?>
