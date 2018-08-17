e7ad7d617b08835df4633cec23495bd3bf00c068
Dan Feder <dafeder@gmail.com>
2018-08-16 21:12:04 -0400

    Stuff for json_schema_field I probably won't use.

--- modules/custom/json_schema_field/src/Plugin/Field/FieldType/JsonSchemaItem.php
+++ modules/custom/json_schema_field/src/Plugin/Field/FieldType/JsonSchemaItem.php
@@ -5,52 +5,20 @@ namespace Drupal\field_example\Plugin\Field\FieldType;
 use Drupal\Core\Field\FieldItemBase;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
+use Drupal\jsonb\Plugin\Field\FieldType\JsonItem;
 
 /**
- * Plugin implementation of the 'field_example_rgb' field type.
+ * Plugin implementation of the 'json' field type.
  *
  * @FieldType(
- *   id = "field_example_rgb",
- *   label = @Translation("Example Color RGB"),
- *   module = "field_example",
- *   description = @Translation("Demonstrates a field composed of an RGB color."),
- *   default_widget = "field_example_text",
- *   default_formatter = "field_example_simple_text"
+ *   id = "json_schema",
+ *   label = @Translation("JSON Schema Field"),
+ *   description = @Translation("This field stores a JSON object or an array of JSON objects."),
+ *   category = @Translation("Document"),
+ *   default_widget = "jsonb_textarea",
+ *   default_formatter = "jsonb_default"
  * )
  */
 class JsonSchemaItem extends JsonItem {
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function schema(FieldStorageDefinitionInterface $field_definition) {
-    return [
-      'columns' => [
-        'value' => [
-          'type' => 'text',
-          'size' => 'tiny',
-          'not null' => FALSE,
-        ],
-      ],
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isEmpty() {
-    $value = $this->get('value')->getValue();
-    return $value === NULL || $value === '';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    $properties['value'] = DataDefinition::create('string')
-      ->setLabel(t('Hex value'));
-
-    return $properties;
-  }
 
 }
