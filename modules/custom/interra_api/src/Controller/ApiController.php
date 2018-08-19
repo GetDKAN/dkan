d4d71fa07bc98c829f93b326fd0d653642758191
acouch <acinternets@gmail.com>
2018-08-19 16:27:00 -0400

    Updates API

--- modules/custom/interra_api/src/Controller/ApiController.php
+++ modules/custom/interra_api/src/Controller/ApiController.php
@@ -4,6 +4,10 @@ namespace Drupal\interra_api\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 /**
 * An example controller.
 */
@@ -12,11 +16,20 @@ class ApiController extends ControllerBase {
   /**
   * Returns a render-able array for a test page.
   */
-  public function routes() {
-    $build = [
-    '#markup' => $this->t('Hello World!'),
-    ];
+  public function routes( Request $request ) {
+    $response['data'] = 'Some test data to return';
+    $response['method'] = 'GET';
 
-    return $build;
+    return new JsonResponse( $response );
   }
+}
+
+function getRoutes() {
+}
+
+function getCollections() {
+  return array(
+    'datasets',
+    'organizations',
+  );
 }
