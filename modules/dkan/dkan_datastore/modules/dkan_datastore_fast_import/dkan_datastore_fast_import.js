76b0ada40391604c7a94257e7cdaae65ff040896
fmizzell <fmizzell@users.noreply.github.com>
2018-09-13 13:07:14 -0500

    Refactoring the datastore code to not be dependent on the feeds or data module and to support multiple datastore manager implementations more cleanly.

--- modules/dkan/dkan_datastore/modules/dkan_datastore_fast_import/dkan_datastore_fast_import.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.dkanDatastoreFastImport = {
-    attach: function (context, settings) {
-      $('#edit-feedsflatstoreprocessor-geolocate', context).on('change', function (e) {
-        $('#edit-use-fast-import').attr('checked', false);
-      });
-    }
-  };
-}(jQuery));
