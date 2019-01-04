2811dd93e2b12c4fec9f337bc5822d9432169ba3
Riadh Habbachi <riadh.habbachi@gmail.com>
2019-01-04 11:12:59 +0100

    update CSV data dictionary rendering logic

--- modules/dkan/dkan_data_dictionary/modules/dkan_data_dictionary_csv/src/CsvDataDictionary.php
+++ modules/dkan/dkan_data_dictionary/modules/dkan_data_dictionary_csv/src/CsvDataDictionary.php
@@ -2,8 +2,10 @@
 
 namespace Dkan\DataDictionary;
 
+use League\Csv\Reader;
+
 /**
- * Class Info.
+ * CsvDataDictionary Class.
  *
  * Validator metadata.
  */
@@ -12,8 +14,21 @@ class CsvDataDictionary extends DataDictionaryBase {
   /**
    * {@inheritdoc}
    */
-  public static function validateSchema($schema) {
-    // @TODO
+  public static function validateSchema($descriptor) {
+    $is_file = filter_var($descriptor, FILTER_VALIDATE_URL);
+
+    try {
+      if ($is_file) {
+        Reader::createFromPath($descriptor, 'r');
+      }
+      else {
+        Reader::createFromString($descriptor);
+      }
+    }
+    catch (\Exception $e) {
+      return array($e->getMessage());
+    }
+
     return array();
   }
 
@@ -28,9 +43,56 @@ class CsvDataDictionary extends DataDictionaryBase {
    *
    * @throws
    */
-  public static function schemaFormatterView($discriptor, $display_type) {
-    // @TODO
-    return array('#markup' => "TODO");
+  public static function schemaFormatterView($descriptor, $display_type) {
+    if ($display_type == 'text_schema_table') {
+      $is_file = filter_var($descriptor, FILTER_VALIDATE_URL);
+
+      $reader = NULL;
+      try {
+        if ($is_file) {
+          $reader = Reader::createFromPath($descriptor, 'r');
+        }
+        else {
+          $reader = Reader::createFromString($descriptor);
+        }
+      }
+      catch (\Exception $e) {
+        watchdog('CsvDataDictionary', "Failed to parse the CSV discriptor: @descriptor.", WATCHDOG_ERROR);
+      }
+
+      if (empty($reader)) {
+        return array(
+          '#markup' => t("Failed to parse the CSV descriptor"),
+        );
+      }
+
+      $reader->setHeaderOffset(0);
+      $headers = $reader->getHeader();
+
+      $rows = array();
+
+      foreach ($reader as $key => $record) {
+        $row = array();
+
+        foreach ($record as $item) {
+          $row[] = array('data' => $item, 'class' => array('dkan-datadict-item', 'dkan-datadict-' . $key));
+        }
+
+        $rows[] = $row;
+      }
+
+      return array(
+        '#theme' => 'table',
+        '#header' => $headers,
+        '#rows' => $rows,
+        '#attributes' => array('class' => array('dkan-datadict', 'json-schema')),
+      );
+    }
+    else {
+      // If the supplied value isn't valid JSON or it is valid JSON but
+      // isn't a schema containing fields - simply output the raw text.
+      return array('#markup' => $descriptor);
+    }
   }
 
 }
