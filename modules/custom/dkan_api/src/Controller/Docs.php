9d2d731ffa2c021ec9cd0e9eb1ec4e0c257d3850
thierrydallacroce <thierry.dallacroce@gmail.com>
2019-12-04 15:11:16 -0800

    Publicly accessible version of api docs (#264)
    * Publicly-accessible version of docs endpoint, with updated unit tests
    * Update the dkan_api docs service

--- modules/custom/dkan_api/src/Controller/Docs.php
+++ modules/custom/dkan_api/src/Controller/Docs.php
@@ -9,6 +9,7 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Serialization\Yaml;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Serves openapi spec for dataset-related endpoints.
@@ -30,6 +31,13 @@ class Docs implements ContainerInjectionInterface {
   private $moduleHandler;
 
   /**
+   * Request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  private $requestStack;
+
+  /**
    * Serializer to translate yaml to json.
    *
    * @var \Symfony\Component\Serializer\Serializer
@@ -40,25 +48,29 @@ class Docs implements ContainerInjectionInterface {
    * Inherited.
    *
    * @{inheritdocs}
-   *
-   * @codeCoverageIgnore
    */
   public static function create(ContainerInterface $container) {
-    $moduleHandler = $container->get('module_handler');
-    return new Docs($moduleHandler);
+    return new Docs(
+      $container->get('module_handler'),
+      $container->get('request_stack')
+    );
   }
 
   /**
    * Constructor.
    */
-  public function __construct(ModuleHandlerInterface $moduleHandler) {
+  public function __construct(
+    ModuleHandlerInterface $moduleHandler,
+    RequestStack $requestStack
+  ) {
     $this->moduleHandler = $moduleHandler;
+    $this->requestStack = $requestStack;
     $this->serializer = new Yaml();
     $this->spec = $this->getJsonFromYmlFile();
   }
 
   /**
-   * Ger version.
+   * Get version.
    */
   public function getVersions() {
     return new JsonResponse(["version" => 1, "url" => "/api/1"]);
@@ -73,24 +85,9 @@ class Docs implements ContainerInjectionInterface {
   public function getJsonFromYmlFile() {
     $modulePath = $this->moduleHandler->getModule('dkan_api')->getPath();
     $ymlSpecPath = $modulePath . '/docs/dkan_api_openapi_spec.yml';
-    $ymlSpec = $this->fileGetContents($ymlSpecPath);
+    $ymlSpec = file_get_contents($ymlSpecPath);
 
     return $this->serializer->decode($ymlSpec);
-  }
-
-  /**
-   * Wrapper around file_get_contents to facilitate testing.
-   *
-   * @param string $path
-   *   Path for our yml spec.
-   *
-   * @return false|string
-   *   Our yml file, or FALSE.
-   *
-   * @codeCoverageIgnore
-   */
-  private function fileGetContents($path) {
-    return file_get_contents($path);
   }
 
   /**
@@ -100,9 +97,83 @@ class Docs implements ContainerInjectionInterface {
    *   OpenAPI spec response.
    */
   public function getComplete() {
-    $jsonSpec = json_encode($this->spec);
+    if ($this->requestStack->getCurrentRequest()->get('authentication') === "false") {
+      $spec = $this->getPublic();
+    }
+    else {
+      $spec = $this->spec;
+    }
 
+    $jsonSpec = json_encode($spec);
     return $this->sendResponse($jsonSpec);
+  }
+
+  /**
+   * Return a publicly-accessible version of the API spec.
+   *
+   * Remove any endpoint requiring authentication, as well as the security
+   * schemes components from the api spec.
+   *
+   * @return array
+   *   The modified API spec, without authentication-related items.
+   */
+  private function getPublic() {
+    $publicSpec = $this->removeAuthenticatedEndpoints($this->spec);
+    $cleanSpec = $this->cleanUpEndpoints($publicSpec);
+    unset($cleanSpec['components']['securitySchemes']);
+    return $cleanSpec;
+  }
+
+  /**
+   * Remove API spec endpoints requiring authentication.
+   *
+   * @param array $spec
+   *   The original spec.
+   *
+   * @return array
+   *   The modified API spec, without authenticated endpoints.
+   */
+  private function removeAuthenticatedEndpoints(array $spec) {
+    foreach ($spec['paths'] as $path => $operations) {
+      $this->removeAuthenticatedOperations($operations, $path, $spec);
+    }
+    return $spec;
+  }
+
+  /**
+   * Within a path, remove operations requiring authentication.
+   *
+   * @param array $operations
+   *   Operations for the current path.
+   * @param string $path
+   *   The path being processed.
+   * @param array $spec
+   *   Our modified dataset-specific openapi spec.
+   */
+  private function removeAuthenticatedOperations(array $operations, string $path, array &$spec) {
+    foreach ($operations as $operation => $details) {
+      if (isset($spec['paths'][$path][$operation]['security'])) {
+        unset($spec['paths'][$path][$operation]);
+      }
+    }
+  }
+
+  /**
+   * Clean up empty endpoints from the spec.
+   *
+   * @param array $spec
+   *   The original spec.
+   *
+   * @return array
+   *   The cleaned up API spec.
+   */
+  private function cleanUpEndpoints(array $spec) {
+    foreach ($spec['paths'] as $path => $operations) {
+      if (empty($operations)) {
+        unset($spec['paths'][$path]);
+      }
+    }
+    return $spec;
   }
 
   /**
