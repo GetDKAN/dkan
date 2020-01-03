a1ce28a8d74c286b71fd99988d9adf97e767ec47
fmizzell <fmizzell@users.noreply.github.com>
2020-01-02 18:02:13 -0600

    Add all dataset pages in dkan_frontend. (#270)

--- modules/custom/dkan_frontend/src/Routing/RouteProvider.php
+++ modules/custom/dkan_frontend/src/Routing/RouteProvider.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\dkan_frontend\Routing;
 
+use Drupal\Core\Entity\Query\QueryFactory;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -11,12 +12,14 @@ use Symfony\Component\Routing\RouteCollection;
 class RouteProvider {
 
   private $appRoot;
+  private $entityQuery;
 
   /**
    * Constructor.
    */
-  public function __construct(string $appRoot) {
+  public function __construct(string $appRoot, QueryFactory $entityQuery) {
     $this->appRoot = $appRoot;
+    $this->entityQuery = $entityQuery;
   }
 
   /**
@@ -25,26 +28,8 @@ class RouteProvider {
   public function routes() {
     $routes = new RouteCollection();
 
-    $base = $this->appRoot . "/data-catalog-frontend/public";
-    $possible_pages = $this->expandDirectories($base);
-
-    foreach ($possible_pages as $possible_page) {
-      if (file_exists($possible_page . "/index.html")) {
-        $name = self::getNameFromPath($possible_page);
-        $path = str_replace($base, "", $possible_page);
-        $routes->add($name, $this->routeHelper($path, $name));
-      }
-    }
-
-    $route = new Route(
-          "/home",
-          [
-            '_controller' => '\Drupal\dkan_frontend\Controller\Page::page',
-            'name' => 'home',
-          ]
-      );
-    $route->setMethods(['GET']);
-    $routes->add('home', $route);
+    $this->addStaticPages($routes);
+    $this->addDatasets($routes);
 
     $routes->addRequirements(['_access' => 'TRUE']);
 
@@ -99,6 +84,49 @@ class RouteProvider {
       );
     $route->setMethods(['GET']);
     return $route;
+  }
+
+  /**
+   * Private.
+   */
+  private function addStaticPages(RouteCollection $routes) {
+    $base = $this->appRoot . "/data-catalog-frontend/public";
+    $possible_pages = $this->expandDirectories($base);
+
+    foreach ($possible_pages as $possible_page) {
+      if (file_exists($possible_page . "/index.html")) {
+        $name = self::getNameFromPath($possible_page);
+        $path = str_replace($base, "", $possible_page);
+        $routes->add($name, $this->routeHelper($path, $name));
+      }
+    }
+
+    $route = new Route(
+      "/home",
+      [
+        '_controller' => '\Drupal\dkan_frontend\Controller\Page::page',
+        'name' => 'home',
+      ]
+    );
+    $route->setMethods(['GET']);
+    $routes->add('home', $route);
+  }
+
+  /**
+   * Private.
+   */
+  private function addDatasets(RouteCollection $routes) {
+    $query = $this->entityQuery->get("node");
+    $query->condition('type', 'data');
+    $query->condition('field_data_type', 'dataset');
+    $result = $query->execute();
+
+    foreach ($result as $item) {
+      $node = node_load($item);
+      $uuid = $node->get("uuid")->value;
+      $name = "dataset__{$uuid}";
+      $routes->add($name, $this->routeHelper("/dataset/{$uuid}", $name));
+    }
   }
 
 }
