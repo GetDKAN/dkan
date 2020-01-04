825c30d04990fa23b4a4cd2ac022c345deb396b9
thierrydallacroce <thierry.dallacroce@gmail.com>
2020-01-03 17:38:22 -0800

    Refactor web service api docs (#281)
    * Break apart main function in dataset-specific docs endpoint into smaller ones
    * Unit test incorrectly passed a second argument to getDatasetSpecific()
    * Add an sql endpoint to test yml spec to increase code coverage

--- modules/custom/dkan_metastore/tests/src/Unit/WebServiceApiDocsTest.php
+++ modules/custom/dkan_metastore/tests/src/Unit/WebServiceApiDocsTest.php
@@ -22,21 +22,10 @@ class WebServiceApiDocsTest extends TestCase {
   public function testGetDatasetSpecific() {
     $mockChain = $this->getCommonMockChain();
 
-    // Test against ./docs/dkan_api_openapi_spec.yml.
-    $endpointsToKeep = [
-      // Target paths.
-      '/api/1/metastore/schemas/dataset/items/{identifier}' => ['get'],
-      '/api/1/datastore/sql' => ['get'],
-      // Non-existent operation.
-      '/api/1/some/other/path' => ['get'],
-      // Non-existent path.
-      'api/1/non/existent/path' => ['put'],
-    ];
-
     $controller = WebServiceApiDocs::create($mockChain->getMock());
-    $response = $controller->getDatasetSpecific(1, $endpointsToKeep);
+    $response = $controller->getDatasetSpecific(1);
 
-    $spec = '{"openapi":"3.0.1","info":{"title":"API Documentation","version":"Alpha"},"paths":{"\/api\/1\/metastore\/schemas\/dataset\/items\/1":{"get":{"summary":"Get this dataset","tags":["Dataset"],"responses":{"200":{"description":"Ok"}}}}},"tags":[{"name":"Dataset"},{"name":"SQL Query"}]}';
+    $spec = '{"openapi":"3.0.1","info":{"title":"API Documentation","version":"Alpha"},"tags":[{"name":"Dataset"},{"name":"SQL Query"}],"paths":{"\/api\/1\/datastore\/sql":{"get":{"summary":"Query resources","tags":["SQL Query"],"responses":{"200":{"description":"Ok"}}}},"\/api\/1\/metastore\/schemas\/dataset\/items\/1":{"get":{"summary":"Get this dataset","tags":["Dataset"],"responses":{"200":{"description":"Ok"}}}}}}';
 
     $this->assertEquals($spec, $response->getContent());
   }
@@ -46,6 +35,7 @@ class WebServiceApiDocsTest extends TestCase {
    */
   private function getCommonMockChain() {
     $serializer = new Yaml();
+    // Test against ./docs/dkan_api_openapi_spec.yml.
     $yamlSpec = file_get_contents(__DIR__ . "/docs/dkan_api_openapi_spec.yml");
 
     $mockChain = new Chain($this);
