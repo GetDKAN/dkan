ce397192fa7bc101a0da4463a012f19f9d653cf4
fmizzell <fmizzell@users.noreply.github.com>
2020-01-13 13:00:17 -0600

    Fixing revert and creating 'happy path' unit tests. (#293)

--- modules/custom/dkan_harvest/test/src/Unit/HarvesterTest.php
+++ modules/custom/dkan_harvest/test/src/Unit/HarvesterTest.php
@@ -1,10 +1,12 @@
 <?php
 
-use Harvest\Harvester;
-use MockChain\Chain;
-use Drupal\dkan_harvest\Storage\DatabaseTableFactory;
 use PHPUnit\Framework\TestCase;
-use Drupal\dkan_harvest\Storage\DatabaseTable;
+use MockChain\Chain;
+use Contracts\FactoryInterface;
+use Contracts\Mock\Storage\Memory;
+use Harvest\Harvester;
+use Drupal\dkan_datastore\Storage\DatabaseTable;
+use Drupal\dkan_harvest\Storage\DatabaseTableFactory;
 use Drupal\dkan_harvest\Harvester as HarvestService;
 
 /**
@@ -12,9 +14,82 @@ use Drupal\dkan_harvest\Harvester as HarvestService;
  */
 class HarvesterTest extends TestCase {
 
-  /**
-   *
-   */
+  private $storageFactory;
+
+  public function test() {
+    $service = new HarvestService($this->getStorageFactory());
+
+    $plan = (object) [
+      'identifier' => 'test_plan',
+      'extract' => (object) [
+        "type" => \Harvest\ETL\Extract\DataJson::class,
+        "uri" => "file://" . __DIR__ . '/../../files/data.json'
+      ],
+      'transforms' => [],
+      'load' => (object) [
+        "type" => \Harvest\ETL\Load\Simple::class
+      ]
+    ];
+
+    // Register a harvest.
+    $result = $service->registerHarvest($plan);
+
+    $this->assertEquals('test_plan', $result);
+
+    $storedTestPlan = json_decode($this->getStorageFactory()->getInstance('harvest_plans')->retrieve('test_plan'));
+    $this->assertEquals('test_plan', $storedTestPlan->identifier);
+
+    // Run a harvest.
+    $result = $service->runHarvest('test_plan');
+
+    $this->assertEquals("SUCCESS", $result['status']['extract']);
+    $this->assertEquals(2, count($result['status']['extracted_items_ids']));
+    $this->assertEquals(json_encode(["NEW", "NEW"]), json_encode(array_values($result['status']['load'])));
+
+    $storedObject = $this->getStorageFactory()->getInstance('harvest_test_plan_items')->retrieve("cedcd327-4e5d-43f9-8eb1-c11850fa7c55");
+    $this->assertTrue(is_string($storedObject));
+    $storedObject = json_decode($storedObject);
+    $this->assertTrue(is_object($storedObject));
+
+    // Run havest again, no changes.
+    $result = $service->runHarvest('test_plan');
+
+    $this->assertEquals("SUCCESS", $result['status']['extract']);
+    $this->assertEquals(2, count($result['status']['extracted_items_ids']));
+    $this->assertEquals(json_encode(["UNCHANGED", "UNCHANGED"]), json_encode(array_values($result['status']['load'])));
+
+    // Run harvest with changes.
+    $plan2 = clone $plan;
+    $plan2->extract->uri = "file://" . __DIR__ . '/../../files/data2.json';
+    $service->registerHarvest($plan2);
+    $result = $service->runHarvest('test_plan');
+
+    $this->assertEquals("SUCCESS", $result['status']['extract']);
+    $this->assertEquals(2, count($result['status']['extracted_items_ids']));
+    $this->assertEquals(json_encode(["UPDATED", "UNCHANGED"]), json_encode(array_values($result['status']['load'])));
+
+    $storedObject = $this->getStorageFactory()->getInstance('harvest_test_plan_items')->retrieve("cedcd327-4e5d-43f9-8eb1-c11850fa7c55");
+    $this->assertTrue(is_string($storedObject));
+    $storedObject = json_decode($storedObject);
+    $this->assertTrue(is_object($storedObject));
+    $this->assertEquals("Florida Bike Lanes 2", $storedObject->title);
+
+    // Revert harvest.
+    $service->revertHarvest('test_plan');
+    $storageTypes = [
+      'harvest_test_plan_items',
+      'harvest_test_plan_hashes',
+      'harvest_test_plan_runs'
+    ];
+    foreach ($storageTypes as $storageId) {
+      $this->assertEquals(0, count($this->getStorageFactory()->getInstance($storageId)->retrieveAll()));
+    }
+
+    // Deregister harvest.
+    $service->deregisterHarvest('test_plan');
+    $this->assertEquals(0, count($this->getStorageFactory()->getInstance('harvest_plans')->retrieveAll()));
+  }
+
   public function testGetHarvestPlan() {
     $storeFactory = (new Chain($this))
       ->add(DatabaseTableFactory::class, "getInstance", DatabaseTable::class)
@@ -26,87 +101,6 @@ class HarvesterTest extends TestCase {
     $this->assertEquals("Hello", $plan);
   }
 
-  /**
-   *
-   */
-  public function testDeregisterHarvest() {
-    $storeFactory = (new Chain($this))
-      ->add(DatabaseTableFactory::class, "getInstance", DatabaseTable::class)
-      ->add(DatabaseTable::class, "retrieve", "Hello")
-      ->add(DatabaseTable::class, "destroy", NULL)
-      ->add(DatabaseTable::class, "remove", "Hello")
-      ->getMock();
-
-    $dkanHarvester = (new Chain($this))
-      ->add(Harvester::class, "revert", NULL)
-      ->getMock();
-
-    $service = $this->getMockBuilder(HarvestService::class)
-      ->setConstructorArgs([$storeFactory])
-      ->setMethods(['getDkanHarvesterInstance'])
-      ->getMock();
-
-    $service->method('getDkanHarvesterInstance')->willReturn($dkanHarvester);
-
-    $result = $service->deregisterHarvest("test");
-    $this->assertEquals("Hello", $result);
-  }
-
-  /**
-   *
-   */
-  public function testRunHarvest() {
-    $storeFactory = (new Chain($this))
-      ->add(DatabaseTableFactory::class, "getInstance", DatabaseTable::class)
-      ->add(DatabaseTable::class, "retrieveAll", ["Hello"])
-      ->add(DatabaseTable::class, "retrieve", "Hello")
-      ->add(DatabaseTable::class, "store", "Hello")
-      ->getMock();
-
-    $dkanHarvester = (new Chain($this))
-      ->add(Harvester::class, "harvest", "Hello")
-      ->getMock();
-
-    $service = $this->getMockBuilder(HarvestService::class)
-      ->setConstructorArgs([$storeFactory])
-      ->setMethods(['getDkanHarvesterInstance'])
-      ->getMock();
-
-    $service->method('getDkanHarvesterInstance')->willReturn($dkanHarvester);
-
-    $result = $service->runHarvest("test");
-    $this->assertEquals("Hello", $result);
-  }
-
-  /**
-   *
-   */
-  public function testGetAllHarvestRunInfo() {
-    $storeFactory = (new Chain($this))
-      ->add(DatabaseTableFactory::class, "getInstance", DatabaseTable::class)
-      ->add(DatabaseTable::class, "retrieveAll", ["Hello"])
-      ->add(DatabaseTable::class, "retrieve", "Hello")
-      ->add(DatabaseTable::class, "store", "Hello")
-      ->getMock();
-
-    $dkanHarvester = (new Chain($this))
-      ->add(Harvester::class, "harvest", "Hello")
-      ->getMock();
-
-    $service = $this->getMockBuilder(HarvestService::class)
-      ->setConstructorArgs([$storeFactory])
-      ->setMethods(['getDkanHarvesterInstance'])
-      ->getMock();
-
-    $service->method('getDkanHarvesterInstance')->willReturn($dkanHarvester);
-
-    $result = $service->getAllHarvestRunInfo("test");
-    $this->assertTrue(is_array($result));
-  }
-
-  /**
-   *
-   */
   public function testGetHarvestRunInfo() {
     $storeFactory = (new Chain($this))
       ->add(DatabaseTableFactory::class, "getInstance", DatabaseTable::class)
@@ -128,6 +122,32 @@ class HarvesterTest extends TestCase {
 
     $result = $service->getHarvestRunInfo("test", "1");
     $this->assertFalse($result);
+  }
+
+  private function getStorageFactory() {
+    if (!isset($this->storageFactory)) {
+      $this->storageFactory = new class() implements FactoryInterface {
+        private $stores = [];
+
+        public function getInstance(string $identifier, array $config = [])
+        {
+          if (!isset($this->stores[$identifier])) {
+            $this->stores[$identifier] = new class() extends Memory {
+              public function retrieveAll(): array
+              {
+                return array_keys(parent::retrieveAll());
+              }
+              public function destroy() {
+                $this->storage = [];
+              }
+            };
+          }
+          return $this->stores[$identifier];
+        }
+      };
+    }
+
+    return $this->storageFactory;
   }
 
 }
