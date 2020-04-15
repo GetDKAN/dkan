3684caa1542db1b4a0622ad6ece74214664a1cab
Janette Day <janette.day@civicactions.com>
2020-04-15 16:14:52 -0500

    Move from profile to modules (#373)

---
+++ modules/dkan_schema/src/SchemaRetriever.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\dkan_schema;
+
+use Contracts\RetrieverInterface;
+use Drupal\Core\Extension\ExtensionList;
+
+/**
+ * Class.
+ */
+class SchemaRetriever implements RetrieverInterface {
+
+  /**
+   * Directory.
+   *
+   * @var string
+   */
+  protected $directory;
+
+  /**
+   * Public.
+   */
+  public function __construct($appRoot, ExtensionList $extensionList) {
+    $this->findSchemaDirectory($appRoot, $extensionList);
+  }
+
+  /**
+   * Public.
+   */
+  public function getAllIds() {
+    return [
+      'catalog',
+      'dataset',
+      'dataset.ui',
+    ];
+  }
+
+  /**
+   * Public.
+   */
+  public function getSchemaDirectory() {
+    return $this->directory;
+  }
+
+  /**
+   * Public.
+   */
+  public function retrieve(string $id): ?string {
+
+    $filename = $this->getSchemaDirectory() . "/collections/{$id}.json";
+
+    if (in_array($id, $this->getAllIds())
+          && is_readable($filename)
+      ) {
+      return file_get_contents($filename);
+    }
+    throw new \Exception("Schema {$id} not found.");
+  }
+
+  /**
+   * Private.
+   */
+  protected function findSchemaDirectory($appRoot, $extensionList) {
+
+    $drupalRoot = $appRoot;
+
+    if (is_dir($drupalRoot . "/schema")) {
+      $this->directory = $drupalRoot . "/schema";
+    }
+    elseif (($directory = $this->getDefaultSchemaDirectory($extensionList))
+          && is_dir($directory)
+      ) {
+      $this->directory = $directory;
+    }
+    else {
+      throw new \Exception("No schema directory found.");
+    }
+  }
+
+  /**
+   * Determine default location of schema folder for dkan2.
+   *
+   * @todo There may be easier way to do this and without hardcoding paths.
+   *
+   * @return string
+   *   Path.
+   */
+  protected function getDefaultSchemaDirectory($extensionList) {
+    /** @var \Drupal\Core\Extension\ExtensionList $extensionList */
+    $extensionList = $extensionList;
+    $infoFile = $extensionList->getPathname('dkan2');
+
+    return dirname($infoFile) . '/schema';
+  }
+
+}
