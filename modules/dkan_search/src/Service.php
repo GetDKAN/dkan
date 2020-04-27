3f4c29b602e9ac501cb459845168759f0768c104
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-04-26 17:46:15 -0700

    Move most of WebServiceApi's internals to new dkan_search's Service

--- modules/dkan_search/src/Service.php
+++ modules/dkan_search/src/Service.php
@@ -5,6 +5,7 @@ namespace Drupal\dkan_search;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\dkan_metastore\Service as Metastore;
+use Drupal\search_api\Query\ResultSet;
 use Drupal\search_api\Utility\QueryHelper;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -20,7 +21,14 @@ class Service implements ContainerInjectionInterface {
    *
    * @var \Drupal\search_api\IndexInterface
    */
-  private $searchIndex;
+  private $index;
+
+  /**
+   * The query being built then executed.
+   *
+   * @var \Drupal\search_api\Query\QueryInterface
+   */
+  private $query;
 
   /**
    * Metastore service.
@@ -85,9 +93,9 @@ class Service implements ContainerInjectionInterface {
   private function setSearchIndex(string $id) {
     $storage = $this->entityTypeManager
       ->getStorage("search_api_index");
-    $this->searchIndex = $storage->load($id);
+    $this->index = $storage->load($id);
 
-    if (!$this->searchIndex) {
+    if (!$this->index) {
       throw new \Exception("An index named [{$id}] does not exist.");
     }
   }
@@ -95,11 +103,34 @@ class Service implements ContainerInjectionInterface {
   /**
    * Search.
    *
-   * @return array
-   *   Result array.
+   * @param array $params
+   *
+   * @return object
+   *   StdClass containing total, results and facets.
    */
-  public function search() {
-    return ["foo"];
+  public function search(array $params) {
+    $fields = array_keys($this->index->getFields());
+    $this->query = $this->queryHelper->createQuery($this->index);
+
+    $this->setFullText($params);
+    $this->setFieldConditions($fields, $params);
+
+    $facets = $this->getFacets($fields);
+
+    $this->setSort($params, $fields);
+    $this->setRange($params);
+
+    $result = $this->query->execute();
+
+    $count = $result->getResultCount();
+
+    $data = $this->getData($result);
+
+    return (object) [
+      "total" => $count,
+      "results" => $data,
+      "facets" => $facets,
+    ];
   }
 
   /**
@@ -115,6 +146,209 @@ class Service implements ContainerInjectionInterface {
    */
   public function searchByIndexField(string $id, string $value) {
     return ["bar"];
+  }
+
+  /**
+   * Private.
+   *
+   * @param array $params
+   *   Array of search parameters.
+   */
+  private function setFullText(array $params) {
+    if (!isset($params['fulltext'])) {
+      return;
+    }
+
+    $fulltextFields = $this->index->getFulltextFields();
+    if (empty($fulltextFields)) {
+      return;
+    }
+
+    $values = [];
+    foreach ($fulltextFields as $field) {
+      $values[$field][] = $params['fulltext'];
+    }
+
+    $this->createConditionGroup($values, 'OR');
+  }
+
+  /**
+   * Private.
+   *
+   * @param string $conjuction
+   */
+  private function createConditionGroup($array, $conjuction = 'AND') {
+    $cg = $this->query->createConditionGroup($conjuction);
+    foreach ($array as $field => $values) {
+      foreach ($values as $value) {
+        $cg->addCondition($field, $value);
+      }
+    }
+    $this->query->addConditionGroup($cg);
+  }
+
+  /**
+   * Private.
+   */
+  private function setFieldConditions($fields, $params) {
+    foreach ($fields as $field) {
+      if (isset($params[$field])) {
+        $values[$field] = $this->getValuesFromCommaSeparatedString($params[$field]);
+        $this->createConditionGroup($this->query, $values);
+      }
+    }
+  }
+
+  /**
+   * Private.
+   *
+   * @param string $string
+   *
+   * @return array
+   */
+  private function getValuesFromCommaSeparatedString(string $string) {
+    $values = [];
+    foreach (explode(",", $string) as $value) {
+      $values[] = trim($value);
+    }
+    return $values;
+  }
+
+  /**
+   * Private.
+   *
+   * @param $fields
+   *
+   * @return array
+   */
+  private function getFacets($fields) {
+    $facetsTypes = ['theme', 'keyword', 'publisher__name'];
+    $facets = [];
+
+    foreach ($facetsTypes as $type) {
+      if (in_array($type, $fields)) {
+        $facets = array_merge($facets, $this->getFacetsForType($type));
+      }
+    }
+
+    return $facets;
+  }
+
+  /**
+   * Private.
+   *
+   * @param string $type
+   *
+   * @return array
+   */
+  private function getFacetsForType(string $type) {
+    $facets = [];
+    $field = '';
+
+    // Prepare facets for fields that correspond to objects.
+    $matches = [];
+    if (preg_match('/(.*)__(.*)/', $type, $matches)) {
+      $schema = $matches[1];
+      $field = $matches[2];
+    }
+    else {
+      $schema = $type;
+    }
+    foreach ($this->metastoreService->getAll($schema) as $thing) {
+      $facet_name = empty($field) ? $thing->data : $thing->data->{$field};
+      $facets[] = $this->getFacetHelper($this->query, $type, $facet_name);
+    }
+
+    return $facets;
+  }
+
+  /**
+   * Private.
+   *
+   * @param string $type
+   * @param string $facet_name
+   *
+   * @return array
+   */
+  private function getFacetHelper(string $type, string $facet_name) {
+    $cloneQuery = clone $this->query;
+    $cloneQuery->addCondition($type, $facet_name);
+    $result = $cloneQuery->execute();
+    return [
+      'type' => $type,
+      'name' => $facet_name,
+      'total' => $result->getResultCount(),
+    ];
+  }
+
+  /**
+   * Private.
+   *
+   * @param array $params
+   * @param array $fields
+   */
+  private function setSort(array $params, array $fields) {
+    if (isset($params['sort']) && in_array($params['sort'], $fields)) {
+      $this->query->sort($params['sort'], $this->getSortOrder($this->query, $params));
+      return;
+    }
+
+    $this->query->sort('search_api_relevance', $this->query::SORT_DESC);
+  }
+
+  /**
+   * Private.
+   *
+   * @param array $params
+   *
+   * @return mixed
+   */
+  private function getSortOrder(array $params) {
+    $default = $this->query::SORT_ASC;
+    if (!isset($params['sort-order'])) {
+      return $default;
+    }
+    if ($params['sort-order'] != 'asc' && $params['sort-order'] != 'desc') {
+      return $default;
+    }
+    return ($params['sort-order'] == 'asc') ? $this->query::SORT_ASC : $this->query::SORT_DESC;
+  }
+
+  /**
+   * Private.
+   *
+   * @param array $params
+   */
+  private function setRange(array $params) {
+    $end = ($params['page'] * $params['page-size']);
+    $start = $end - $params['page-size'];
+    $this->query->range($start, $params['page-size']);
+  }
+
+  /**
+   * Private.
+   *
+   * @param \Drupal\search_api\Query\ResultSet $result
+   *
+   * @return array
+   */
+  private function getData(ResultSet $result) {
+    /* @var  $metastore Metastore */
+    $metastore = $this->metastoreService;
+
+    return array_filter(array_map(
+      function ($item) use ($metastore) {
+        $id = $item->getId();
+        $id = str_replace("dkan_dataset/", "", $id);
+        try {
+          return json_decode($metastore->get("dataset", $id));
+        }
+        catch (\Exception $e) {
+          return NULL;
+        }
+      },
+      $result->getResultItems()
+    ));
   }
 
 }
