cd0351de09c6f8f56fef90a19765fd35cae85131
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-04-27 14:10:14 -0700

    Fix dkan_search WebServiceApi test

--- modules/dkan_search/tests/src/Unit/WebServiceApiTest.php
+++ modules/dkan_search/tests/src/Unit/WebServiceApiTest.php
@@ -2,42 +2,26 @@
 
 namespace Drupal\dkan_search;
 
-use Drupal\search_api\Item\Item;
-use Drupal\search_api\Query\ConditionGroup;
-use Drupal\search_api\Query\ResultSet;
+use Drupal\Component\DependencyInjection\Container;
 use MockChain\Chain;
 use MockChain\Options;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Drupal\Component\DependencyInjection\Container;
-use Drupal\Core\Entity\EntityManager;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\search_api\IndexInterface;
-use Drupal\search_api\Query\QueryInterface;
-use Drupal\search_api\Utility\QueryHelperInterface;
-use Drupal\dkan_metastore\Service;
+
 
 /**
- *
+ * Dkan search web service api tests.
  */
 class WebServiceApiTest extends TestCase {
 
   /**
-   *
+   * Test for search.
    */
-  public function test() {
-
+  public function testSearch() {
     $paramsBag = (new Chain($this))
-      ->add(ParameterBag::class, 'all',
-        [
-          'page-size' => 500,
-          'fulltext' => 'hello',
-          'description' => 'goodbye',
-          'sort' => 'description',
-          'sort-order' => 'asc',
-        ])
+      ->add(ParameterBag::class, 'all', ['page-size' => 500])
       ->getMock();
 
     $request = (new Chain($this))
@@ -49,64 +33,30 @@ class WebServiceApiTest extends TestCase {
     $reflection_property->setAccessible(TRUE);
     $reflection_property->setValue($request, $paramsBag);
 
-    $options = (new Options())
-      ->add('entity_type.manager', EntityManager::class)
-      ->add('request_stack', RequestStack::class)
-      ->add('search_api.query_helper', QueryHelperInterface::class)
-      ->add('dkan_metastore.service', Service::class)
-      ->index(0);
+    $expected = (object) [
+      'total' => 1,
+      'results' => [],
+      'facets' => [],
+    ];
 
-    $item = (new Chain($this))
-      ->add(Item::class, 'getId', 1)
-      ->getMock();
-
-    $thing = (object) ['title' => 'hello', 'description' => 'goodbye', 'publisher__name' => 'Steve'];
-    $facet = (object) ['data' => (object) ['name' => 'Steve']];
-    $expect = (object) ['type' => 'publisher__name', 'name' => 'Steve', 'total' => 1];
-
-    $container = (new Chain($this))
-      ->add(Container::class, "get", $options)
-      ->add(EntityManager::class, 'getStorage', EntityStorageInterface::class)
-      ->add(EntityStorageInterface::class, 'load', IndexInterface::class)
-      ->add(IndexInterface::class, 'getFields', ['description' => 'blah', 'publisher__name' => 'blah'])
-      ->add(IndexInterface::class, 'getFulltextFields', ['title'])
+    $container = $this->getCommonMockChain()
       ->add(RequestStack::class, 'getCurrentRequest', $request)
-      ->add(QueryHelperInterface::class, 'createQuery', QueryInterface::class)
-      ->add(QueryInterface::class, 'execute', ResultSet::class)
-      ->add(QueryInterface::class, 'createConditionGroup', ConditionGroup::class)
-      ->add(ResultSet::class, 'getResultCount', 1)
-      ->add(ResultSet::class, 'getResultItems', [$item])
-      ->add(Service::class, 'get', json_encode($thing))
-      ->add(Service::class, 'getAll', [$facet])
-      ->getMock();
+      ->add(Service::class, 'search', $expected);
 
-    \Drupal::setContainer($container);
+    $controller = WebServiceApi::create($container->getMock());
 
-    $controller = new WebServiceApi();
-
-    /* @var $response \Symfony\Component\HttpFoundation\JsonResponse */
     $response = $controller->search();
-    $this->assertEquals(
-      json_encode((object) ['total' => 1, 'results' => [$thing], 'facets' => [$expect]]),
-      $response->getContent());
+    $this->assertEquals(json_encode($expected), $response->getContent());
   }
 
-  /**
-   *
-   */
-  public function testNoIndex() {
-    $container = (new Chain($this))
-      ->add(Container::class, "get", EntityManager::class)
-      ->add(EntityManager::class, 'getStorage', EntityStorageInterface::class)
-      ->getMock();
+  public function getCommonMockChain() {
+    $options = (new Options())
+      ->add('dkan_search.service', Service::class)
+      ->add('request_stack', RequestStack::class)
+      ->index(0);
 
-    \Drupal::setContainer($container);
-
-    $controller = new WebServiceApi();
-
-    /* @var $response \Symfony\Component\HttpFoundation\JsonResponse */
-    $response = $controller->search();
-    $this->assertEquals(json_encode((object) ['message' => "An index named [dkan] does not exist."]), $response->getContent());
+    return (new Chain($this))
+      ->add(Container::class, 'get', $options);
   }
 
 }
