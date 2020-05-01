dba39a55cdf69301dc7f16c3d703a532b4c797bc
fmizzell <fmizzell.dev@gmail.com>
2020-05-01 02:13:45 -0500

    Bringing phpunit tests back correctly.

--- modules/common/src/Storage/AbstractDatabaseTable.php
+++ modules/common/src/Storage/AbstractDatabaseTable.php
@@ -8,7 +8,7 @@ use Contracts\StorerInterface;
 use Dkan\Datastore\Storage\StorageInterface;
 use Dkan\Datastore\Storage\Database\SqlStorageTrait;
 use Drupal\Core\Database\Connection;
-use Drupal\dkan_datastore\Storage\Query;
+use Drupal\datastore\Storage\Query;
 
 /**
  * AbstractDatabaseTable class.
