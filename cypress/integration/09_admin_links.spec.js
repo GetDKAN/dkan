eeb65bab028cfd555c4be76bd783449f00988ad3
Janette Day <janette.day@civicactions.com>
2020-05-14 16:16:19 -0500

    blurp

--- cypress/integration/09_admin_links.spec.js
+++ cypress/integration/09_admin_links.spec.js
@@ -18,13 +18,12 @@ context('Administration pages', () => {
 
   it('I should see a link for the SQL endpoint configuration', () => {
     cy.visit(baseurl + "/user")
-    cy.wait(2000)
     cy.get('.toolbar-icon-system-admin-config').contains('Configuration').next('.toolbar-menu').then($el=>{
         cy.wrap($el).invoke('show')
         cy.wait(2000)
         cy.wrap($el).contains('DKAN').next('.toolbar-menu').then($el=>{
           cy.wrap($el).invoke('show')
-          cy.wait(2000)
+          cy.wait(5000)
           cy.wrap($el).contains('SQL endpoint').click()
           cy.wait(5000)
           cy.get('label').should('have.text', 'Rows limit')
