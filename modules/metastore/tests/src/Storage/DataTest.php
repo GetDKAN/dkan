f09e382d9bdb1cade46d30e9d8e787d73d4ed88b
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-06-09 22:53:18 -0700

    Fix unit tests for metastore service, storage data

--- modules/metastore/tests/src/Storage/DataTest.php
+++ modules/metastore/tests/src/Storage/DataTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\common\Unit\Storage;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -23,7 +25,7 @@ class DataTest extends TestCase {
    *
    */
   public function testRetrieveAll() {
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->setSchema('dataset');
     $all = $store->retrieveAll();
 
@@ -37,7 +39,7 @@ class DataTest extends TestCase {
    */
   public function testRetrieve() {
     $this->node = $this->getNodeMock();
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->setSchema('dataset');
     $obj = $store->retrieve(1);
 
@@ -53,7 +55,7 @@ class DataTest extends TestCase {
     $this->node = $this->getNodeMock();
     $object = '{"name":"blah"}';
 
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->setSchema('dataset');
     $id = $store->store($object, 1);
 
@@ -67,7 +69,7 @@ class DataTest extends TestCase {
     $this->node = NULL;
     $object = '{"name":"blah"}';
 
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->setSchema('dataset');
     $id = $store->store($object, 1);
 
@@ -79,7 +81,7 @@ class DataTest extends TestCase {
    */
   public function testRemove() {
     $this->node = $this->getNodeMock();
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->setSchema('dataset');
     $removed = $store->remove(1);
 
@@ -91,7 +93,7 @@ class DataTest extends TestCase {
    */
   public function testRetrieveAllException() {
     $this->expectExceptionMessage("Data schemaId not set in retrieveAll()");
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->retrieveAll();
   }
 
@@ -100,7 +102,7 @@ class DataTest extends TestCase {
    */
   public function testRetrieveException() {
     $this->expectExceptionMessage("Data schemaId not set in retrieve()");
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->retrieve(1);
   }
 
@@ -110,7 +112,7 @@ class DataTest extends TestCase {
   public function testStoreException() {
     $this->expectExceptionMessage("Data schemaId not set in store()");
     $object = '{"name":"blah"}';
-    $store = new Data($this->getNodeStorageMock());
+    $store = new Data($this->getNodeStorageMock(), $this->getConfigFactoryMock());
     $store->store($object, 1);
   }
 
@@ -136,6 +138,36 @@ class DataTest extends TestCase {
       ->willReturn($this->getNodeMock());
 
     return $nodeStorage;
+  }
+
+  /**
+   *
+   */
+  private function getConfigFactoryMock() {
+    $configFactory = $this->getMockBuilder(ConfigFactoryInterface::class)
+      ->disableOriginalConstructor()
+      ->setMethods(['get'])
+      ->getMockForAbstractClass();
+
+    $configFactory->method('get')
+      ->willReturn($this->getImmutableConfigMock());
+
+    return $configFactory;
+  }
+
+  /**
+   *
+   */
+  private function getImmutableConfigMock() {
+    $immutableConfig = $this->getMockBuilder(ImmutableConfig::class)
+      ->disableOriginalConstructor()
+      ->setMethods(['get'])
+      ->getMock();
+
+    $immutableConfig->method('get')
+      ->willReturn(Data::PUBLISH_NOW);
+
+    return $immutableConfig;
   }
 
   /**
