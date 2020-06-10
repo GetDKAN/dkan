0f150b5e94cd7a39f1d1ef75686dff7e99c27c4b
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-06-09 22:20:56 -0700

    Add a Drush publish command, wrapper around service

---
+++ modules/metastore/src/Drush.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\metastore;
+
+use Drupal\metastore\Storage\Data;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Metastore drush commands.
+ */
+class Drush extends DrushCommands {
+
+  /**
+   * Metastore data storage service.
+   *
+   * @var \Drupal\metastore\Storage\Data
+   */
+  protected $storage;
+
+  /**
+   * Drush constructor.
+   *
+   * @param \Drupal\metastore\Storage\Data $storage
+   *   Metastore data storage.
+   */
+  public function __construct(Data $storage) {
+    parent::__construct();
+    $this->storage = $storage;
+  }
+
+  /**
+   * Publish the latest version of a dataset.
+   *
+   * @param string $uuid
+   *   Dataset identifier.
+   *
+   * @command dkan:metastore:publish
+   */
+  public function publish(string $uuid) {
+    // @todo: Remove once Storage\Data accepts schema string as 3rd parameter.
+    $this->storage->setSchema('dataset');
+
+    try {
+      $this->storage->publish($uuid);
+      $this->logger()->success("Dataset {$uuid} published.");
+    }
+    catch (\Exception $e) {
+      $this->logger()->error("Error while attempting to publish dataset {$uuid}: " . $e->getMessage());
+    }
+  }
+}
