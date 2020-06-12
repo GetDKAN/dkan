00e4e1036d7c20e9e04e4c8bb40ce58895d7bbc3
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-06-11 19:26:31 -0700

    WIP conditionally publish new nodes

--- modules/metastore/src/Storage/Data.php
+++ modules/metastore/src/Storage/Data.php
@@ -261,6 +261,7 @@ class Data implements ContainerInjectionInterface, StorerInterface, RetrieverInt
           'uuid' => $id,
           'field_data_type' => $this->schemaId,
           'field_json_metadata' => json_encode($data),
+          'status' => ($this->getPublishMethod() === Data::PUBLISH_NOW) ? 1 : 0,
         ]
       );
     $node->setRevisionLogMessage("Created on " . $this->formattedTimestamp());
