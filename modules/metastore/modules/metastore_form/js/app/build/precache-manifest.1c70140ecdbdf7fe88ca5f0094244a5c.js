c17f6d53cbf02dcd4226a072068aa23c0d76d17d
Janette Day <janette.day@civicactions.com>
2020-08-12 09:22:42 -0500

    Remove unneeded distribution schema fields (#3144)

---
+++ modules/metastore/modules/metastore_form/js/app/build/precache-manifest.1c70140ecdbdf7fe88ca5f0094244a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b96ab7d00a50aa0adaff96cc627fde",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/index.html"
+  },
+  {
+    "revision": "b9b70ef4ed2bab8b9ac5",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/css/2.0ebff1f4.chunk.css"
+  },
+  {
+    "revision": "43b834285655fd131df3",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/css/main.b9557a38.chunk.css"
+  },
+  {
+    "revision": "b9b70ef4ed2bab8b9ac5",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/js/2.84bb8275.chunk.js"
+  },
+  {
+    "revision": "b2ba6c03d4dd4aa1fd34a1871034824e",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/js/2.84bb8275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b834285655fd131df3",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/js/main.1406e976.chunk.js"
+  },
+  {
+    "revision": "e5e56b1892046a5005d3",
+    "url": "/modules/contrib/dkan/modules/metastore_form/js/app/build/static/js/runtime-main.91201ab4.js"
+  }
+]);
