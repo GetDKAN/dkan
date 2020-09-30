a0078183badfae275548d414abb2926177fa7ac5
Janette Day <janette.day@civicactions.com>
2020-09-30 12:31:15 -0500

    d1

--- modules/datastore/src/WebServiceApi.php
+++ modules/datastore/src/WebServiceApi.php
@@ -6,6 +6,7 @@ use Drupal\common\Resource;
 use Drupal\datastore\Service\DatastoreQuery;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\common\JsonResponseTrait;
 
@@ -218,6 +219,51 @@ class WebServiceApi implements ContainerInjectionInterface {
     }
 
     return $this->getResponse($result, 200);
+  }
+
+  /**
+   * Export a CSV of query results.
+   */
+  public function buildCsv(array $keys, array $data) {
+    // Use PHP's built in file handler functions to create a temporary file.
+    $handle = fopen('php://temp', 'w+');
+
+    // Set up the header as the first line of the CSV file.
+    // Blank strings are used for multi-cell values where there is a count of
+    // the "keys" and a list of the keys with the count of their usage.
+    $header = [];
+    foreach ($keys as $key) {
+      $header[] = $key;
+    }
+    // Add the header as the first line of the CSV.
+    fputcsv($handle, $header);
+
+    // Iterate through the data.
+    foreach ($data as $row) {
+      fputcsv($handle, $row);
+    }
+    // Reset where we are in the CSV.
+    rewind($handle);
+
+    // Retrieve the data from the file handler.
+    $csv_data = stream_get_contents($handle);
+
+    // Close the file handler since we don't need it anymore.
+    // We are not storing this file anywhere in the filesystem.
+    fclose($handle);
+
+    // Once the data is built, we can return it as a response.
+    $response = new Response();
+
+    // By setting these 2 header options, the browser will see the URL
+    // used by this Controller to return a CSV file called "export.csv".
+    $response->headers->set('Content-Type', 'text/csv');
+    $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+
+    // This line physically adds the CSV data we created.
+    $response->setContent($csv_data);
+
+    return $response;
   }
 
 }
