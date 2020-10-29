86554ab46eae391bd1eb347f0fa7e10ad84eef40
Dan Feder <dan.feder@civicactions.com>
2020-10-28 21:18:15 -0400

    Add tests for SelectFactory and query service

---
+++ modules/common/tests/src/Unit/Storage/QueryDataProvider.php
@@ -0,0 +1,404 @@
+<?php
+
+namespace Drupal\Tests\common\Unit\Storage;
+
+use Drupal\common\Storage\Query;
+
+/**
+ * Data provider for testing the SelectFactory class.
+ *
+ * Provides a number of query objects with expected SQL equivalents or exception
+ * messages. Methods are public static so that other tests can easily pull them
+ * in individually.
+ *
+ * @see Drupal\Tests\common\Storage\SelectFactoryTest
+ * @see Drupal\Tests\datastore\Service\QueryTest
+ */
+class QueryDataProvider {
+
+  const QUERY_OBJECT = 1;
+  const SQL = 2;
+  const EXCEPTION = 3;
+
+  public function getAllData($return) {
+    $tests = [
+      'noPropertiesQuery',
+      'propertiesQuery',
+      'badPropertyQuery',
+      'unsafePropertyQuery',
+      'expressionQuery',
+      'nestedExpressionQuery',
+      'badExpressionOperandQuery',
+      'conditionQuery',
+      'nestedConditionGroupQuery',
+      'sortQuery',
+      'badSortQuery',
+      'offsetQuery',
+      'limitOffsetQuery',
+      'joinsQuery',
+      'joinWithPropertiesFromBothQuery',
+      'countQuery',
+    ];
+    $data = [];
+    foreach ($tests as $test) {
+      $data[] = [
+        self::$test(self::QUERY_OBJECT),
+        self::$test(self::SQL),
+        self::$test(self::EXCEPTION),
+      ];
+    }
+    return $data;
+  }
+
+  public static function noPropertiesQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        return $query;
+
+      case self::SQL:
+        return "SELECT t.* FROM {table} t LIMIT 500 OFFSET 0";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function propertiesQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = ["field1", "field2"];
+        return $query;
+
+      case self::SQL:
+        return "SELECT t.field1 AS field1, t.field2 AS field2 FROM {table} t";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function badPropertyQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = [(object) ["collection" => "t"]];
+        return $query;
+
+      case self::SQL:
+        return FALSE;
+
+      case self::EXCEPTION:
+        return "Bad query property";
+
+    }
+  }
+
+  public static function unsafePropertyQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = ["l.field3"];
+        return $query;
+
+      case self::SQL:
+        return FALSE;
+
+      case self::EXCEPTION:
+        return "Unsafe property name: l.field3";
+
+    }
+  }
+
+  public static function expressionQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = [
+          (object) [
+            "alias" => "add_one",
+            "expression" => (object) [
+              "operator" => "+",
+              "operands" => ["field1", 1],
+            ],
+          ],
+          (object) [
+            "alias" => "add_two",
+            "expression" => (object) [
+              "operator" => "+",
+              "operands" => [
+                (object) ["property" => "field2", "collection" => "t"],
+                2,
+              ],
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "SELECT (t.field1 + 1) AS add_one, (t.field2 + 2) AS add_two FROM {table} t";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function nestedExpressionQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = [
+          (object) [
+            "alias" => "nested",
+            "expression" => (object) [
+              "operator" => "*",
+              "operands" => [
+                (object) [
+                  "operator" => "+",
+                  "operands" => ["field1", "field2"],
+                ],
+                (object) [
+                  "operator" => "+",
+                  "operands" => ["field3", "field4"],
+                ],
+              ],
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "SELECT ((t.field1 + t.field2) * (t.field3 + t.field4)) AS nested FROM {table} t";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function badExpressionOperandQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = [
+          (object) [
+            "alias" => "bad_expression",
+            "expression" => (object) [
+              "operator" => "AVG",
+              "operands" => ["field1", "field2"],
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return FALSE;
+
+      case self::EXCEPTION:
+        return "Only basic arithmetic expressions currently supported.";
+
+    }
+  }
+
+  public static function conditionQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->conditions = [
+          (object) [
+            "property" => "field1",
+            "collection" => "t",
+            "value" => "value",
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "WHERE t.field1 = :db_condition_placeholder_0";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function nestedConditionGroupQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->conditions = [
+          (object) [
+            "groupOperator" => "or",
+            "conditions" => [
+              (object) [
+                "collection" => "t",
+                "property" => "field1",
+                "value" => "value1",
+                "operator" => "<",
+              ],
+              (object) [
+                "groupOperator" => "and",
+                "conditions" => [
+                  (object) [
+                    "collection" => "t",
+                    "property" => "field2",
+                    "value" => "value2",
+                  ],
+                  (object) [
+                    "collection" => "t",
+                    "property" => "field3",
+                    "value" => "value3",
+                  ],
+                ],
+              ]
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "WHERE (t.field1 = :db_condition_placeholder_0) OR ((t.field2 = :db_condition_placeholder_1) AND (t.field3 = :db_condition_placeholder_2))";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function sortQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->sort = [
+          "asc" => [],
+          "desc" => ["field1"],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "ORDER BY t.field1 DESC";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function badSortQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->sort = ["foo" => ["field1"]];
+        return $query;
+
+      case self::SQL:
+        return FALSE;
+
+      case self::EXCEPTION:
+        return "Invalid sort.";
+
+    }
+  }
+
+  public static function offsetQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->offset = 5;
+        return $query;
+
+      case self::SQL:
+        return "LIMIT 500 OFFSET 5";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function limitOffsetQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->offset = 5;
+        $query->limit = 15;
+        return $query;
+
+      case self::SQL:
+        return "LIMIT 15 OFFSET 5";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function joinsQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->joins = [
+          (object) [
+            "collection" => "table2",
+            "alias" => "l",
+            "on" => [
+              (object) ["collection" => "t", "property" => "field1"],
+              (object) ["collection" => "l", "property" => "field1"],
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "INNER JOIN {table2} l ON t.field1 = l.field1";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function joinWithPropertiesFromBothQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->properties = [
+          (object) [
+            "property" => "field2",
+            "collection" => "t",
+          ],
+          (object) [
+            "property" => "field3",
+            "collection" => "l",
+          ],
+        ];
+        $query->joins = [
+          (object) [
+            "collection" => "table2",
+            "alias" => "l",
+            "on" => [
+              (object) ["collection" => "t", "property" => "field1"],
+              (object) ["collection" => "l", "property" => "field1"],
+            ],
+          ],
+        ];
+        return $query;
+
+      case self::SQL:
+        return "SELECT t.field2 AS field2, l.field3 AS field3 FROM {table} t INNER JOIN {table2} l";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+  public static function countQuery($return) {
+    switch ($return) {
+      case self::QUERY_OBJECT:
+        $query = new Query();
+        $query->count = TRUE;
+        return $query;
+
+      case self::SQL:
+        return "SELECT COUNT(*) AS expression";
+
+      case self::EXCEPTION:
+        return FALSE;
+    }
+  }
+
+}
---
+++ modules/datastore/tests/src/Service/QueryTest.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Drupal\Tests\datastore\Service;
+
+use Drupal\common\Resource;
+use Drupal\Core\DependencyInjection\Container;
+use Drupal\common\Storage\JobStoreFactory;
+use Drupal\common\Storage\Query;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Tests\datastore\Traits\TestHelperTrait;
+use MockChain\Chain;
+use MockChain\Options;
+use Drupal\datastore\Service;
+use Drupal\datastore\Service\Factory\Import;
+use Drupal\datastore\Service\ResourceLocalizer;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\datastore\Service\DatastoreQuery;
+use Drupal\datastore\Service\Import as ServiceImport;
+use Drupal\datastore\Storage\DatabaseTable;
+use Drupal\metastore\Storage\Data;
+use Drupal\metastore\Storage\DataFactory;
+use Drupal\Tests\common\Unit\Storage\QueryDataProvider as QueryData;
+
+/**
+ * @coversDefaultClass \Drupal\datastore\WebServiceApi
+ * @group dkan
+ */
+class QueryTest extends TestCase {
+  use TestHelperTrait;
+
+  /**
+   * @test
+   *
+   * @dataProvider queryCompareProvider()
+   */
+  public function testQueryCompare($testName) {
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $datastoreQuery = $this->getDatastoreQueryFromJson($testName);
+    $dkanQuery = $datastoreService->populateQuery($datastoreQuery);
+    $dkanQueryCompare = QueryData::$testName(QueryData::QUERY_OBJECT);
+    $dkanQueryCompare->showDbColumns = TRUE;
+    $this->assertEquals(json_encode($dkanQuery, JSON_PRETTY_PRINT), json_encode($dkanQueryCompare, JSON_PRETTY_PRINT));
+  }
+
+  /**
+   * Test a basic datastore query and response for expected properties.
+   */
+  public function testResultsQuery() {
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $datastoreQuery = $this->getDatastoreQueryFromJson("propertiesQuery");
+    $response = $datastoreService->runQuery($datastoreQuery);
+    $this->assertTrue(is_object($response->results[0]));
+    $this->assertTrue(is_numeric($response->count));
+    $this->assertTrue(is_array($response->schema->asdf->fields));
+    $this->assertInstanceOf(DatastoreQuery::class, $response->query);
+    $this->assertIsObject($response->results[0]);
+  }
+
+  /**
+   * Test no keys behavior (array instead of keyed object).
+   */
+  public function testNoKeysQuery() {
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $payload = file_get_contents(__DIR__ . "/../../data/query/propertiesQuery.json");
+    $datastoreQuery = $this->getDatastoreQueryFromJson("propertiesQuery");
+    $datastoreQuery->keys = FALSE;
+    $response = $datastoreService->runQuery($datastoreQuery);
+    $this->assertIsArray($response->results[0]);
+  }
+
+  public function testBadCondition() {
+    $this->expectExceptionMessage("Invalid condition");
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $datastoreQuery = $this->getDatastoreQueryFromJson("badConditionQuery");
+    $datastoreService->runQuery($datastoreQuery);
+  }
+
+  public function testBadQueryProperty() {
+    $this->expectExceptionMessage("Bad query property.");
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $datastoreQuery = $this->getDatastoreQueryFromJson("badPropertyQuery");
+    $datastoreService->runQuery($datastoreQuery);
+  }
+
+  public function testTooManyResourcesQuery() {
+    $this->expectExceptionMessage("Too many resources specified.");
+    $datastoreService = Service::create($this->getCommonMockChain());
+    $datastoreQuery = $this->getDatastoreQueryFromJson("tooManyResourcesQuery");
+    $datastoreService->runQuery($datastoreQuery);
+  }
+
+  /**
+   * Data provider for query compare tests.
+   */
+  public function queryCompareProvider() {
+    return [
+      ["propertiesQuery"],
+      ["expressionQuery"],
+      ["nestedExpressionQuery"],
+      ["nestedConditionGroupQuery"],
+      ["sortQuery"],
+      ["joinWithPropertiesFromBothQuery"],
+    ];
+  }
+
+  private function getDatastoreQueryFromJson($payloadName): DatastoreQuery {
+    $payload = file_get_contents(__DIR__ . "/../../data/query/$payloadName.json");
+    return DatastoreQuery::hydrate($payload);
+  }
+  
+  /**
+   * Build our mockChain.
+   */
+  public function getCommonMockChain() {
+
+    $options = (new Options())
+      ->add("datastore.service", Service::class)
+      ->add('dkan.datastore.service.resource_localizer', ResourceLocalizer::class)
+      ->add("dkan.datastore.service.factory.import", Import::class)
+      ->add('queue', QueueFactory::class)
+      ->add('request_stack', RequestStack::class)
+      ->add('dkan.common.job_store', JobStoreFactory::class)
+      ->add('dkan.metastore.storage', DataFactory::class)
+      ->index(0);
+
+    $resource = '{"data":{"%Ref:downloadURL":[{"data":{"identifier":"qwerty","version":"uiop"}}]}}';
+    $queryResult = [(object) ["expression" => 123]];
+
+    $chain = (new Chain($this))
+      ->add(Container::class, "get", $options)
+      ->add(RequestStack::class, 'getCurrentRequest', Request::class)
+      ->add(DataFactory::class, "getInstance", Data::class)
+      ->add(Data::class, "retrieve", $resource)
+      ->add(QueueFactory::class, "get", [])
+      ->add(ResourceLocalizer::class, "get", Resource::class)
+      ->add(Import::class, "getInstance", ServiceImport::class)
+      ->add(ServiceImport::class, "getStorage", DatabaseTable::class)
+      ->add(DatabaseTable::class, "query", $queryResult)
+      ->add(DatabaseTable::class, "getSchema", ["fields" => ["a", "b"]])
+      ->add(DatabaseTable::class, "getTableName", "table2");
+
+    $container = $chain->getMock();
+    \Drupal::setContainer($container);
+    return $container;
+  }
+
+}
