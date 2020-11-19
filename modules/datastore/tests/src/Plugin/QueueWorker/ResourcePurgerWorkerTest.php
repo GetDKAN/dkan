7e64943df3f6b8ba622f043bec9ecbf1245b5160
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-11-19 06:03:51 -0800

    Unit test

---
+++ modules/datastore/tests/src/Plugin/QueueWorker/ResourcePurgerWorkerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\Tests\datastore\Plugin\QueuWorker;
+
+use Drupal\Core\DependencyInjection\Container;
+use Drupal\datastore\Plugin\QueueWorker\ResourcePurgerWorker;
+use Drupal\datastore\Service\ResourcePurger;
+use MockChain\Chain;
+use MockChain\Options;
+use PHPUnit\Framework\TestCase;
+
+class ResourcePurgerWorkerTest extends TestCase {
+
+  public function test() {
+
+    $options = (new Options())
+      ->add('dkan.datastore.service.resource_purger', ResourcePurger::class)
+      ->index(0);
+
+    $containerChain = (new Chain($this))
+      ->add(Container::class, 'get', $options)
+      ->add(ResourcePurger::class, 'schedulePurging', NULL);
+    $container = $containerChain->getMock();
+
+    \Drupal::setContainer($container);
+
+    $worker = ResourcePurgerWorker::create($container, [], '', '');
+    $voidReturn = $worker->processItem(['uuids' => [], 'allRevisions' => FALSE]);
+    $this->assertNull($voidReturn);
+  }
+
+}
