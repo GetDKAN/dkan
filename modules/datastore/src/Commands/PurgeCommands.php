b6a581aed9b43103b4620f5d3991be9745a20836
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-11-19 06:45:24 -0800

    Drush

--- modules/datastore/src/Commands/PurgeCommands.php
+++ modules/datastore/src/Commands/PurgeCommands.php
@@ -7,15 +7,7 @@ use Drush\Commands\DrushCommands;
 use Drush\Utils\StringUtils;
 
 /**
- * A Drush commandfile.
- *
- * In addition to this file, you need a drush.services.yml
- * in root of your module, and a composer.json file that provides the name
- * of the services file to use.
- *
- * See these files for an example of injecting Drupal services:
- *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
- *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ * Datastore-related Drush commands.
  */
 class PurgeCommands extends DrushCommands {
 
