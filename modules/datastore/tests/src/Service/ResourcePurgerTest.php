d2a01de1a4b246e5607956c640a57cf558d3dc84
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-11-22 09:05:21 -0800

    Unit test

--- modules/datastore/tests/src/Service/ResourcePurgerTest.php
+++ modules/datastore/tests/src/Service/ResourcePurgerTest.php
@@ -31,11 +31,7 @@ class ResourcePurgerTest extends TestCase {
   public function testNoPurgeConfig() {
 
     $chain = $this->getCommonChain()
-      ->add(ImmutableConfig::class, 'get', 0)
-      ->add(Data::class, 'getNodeStorage', NodeStorageInterface::class)
-      ->add(NodeStorageInterface::class, 'getQuery', QueryInterface::class)
-      ->add(QueryInterface::class, 'condition', QueryInterface::class)
-      ->add(QueryInterface::class, 'execute', []);
+      ->add(ImmutableConfig::class, 'get', 0);
 
     $resourcePurger = ResourcePurger::create($chain->getMock());
     $voidResult = $resourcePurger->schedule([], FALSE, TRUE);
@@ -48,7 +44,7 @@ class ResourcePurgerTest extends TestCase {
   public function testQueueing() {
 
     $chain = $this->getCommonChain()
-      ->add(ImmutableConfig::class, 'get', 1)
+      ->add(QueryInterface::class, 'execute', [1 => 1])
       ->add(Service::class, 'getQueueFactory', QueueFactory::class)
       ->add(QueueFactory::class, 'get', QueueInterface::class)
       ->add(QueueInterface::class, 'createItem', 1);
@@ -58,27 +54,27 @@ class ResourcePurgerTest extends TestCase {
     $this->assertNull($voidResult);
   }
 
-  /**
-   * Test figuring out resource purging.
-   */
-  public function testResourcePurger() {
-
-    $uuids = ['1111-1111'];
-    $vids = [];
-    $latestRevision = (new Chain($this))
-      ->add(NodeInterface::class)
-      ->getMock();
-
-    $chain = $this->getCommonChain()
-      ->add(ImmutableConfig::class, 'get', 1)
-      ->add(Data::class, 'getNodeLatestRevision', $latestRevision)
-      ->add(Data::class, 'getNodeStorage', NodeStorageInterface::class)
-      ->add(NodeStorageInterface::class, 'revisionIds', $vids);
-
-    $resourcePurger = ResourcePurger::create($chain->getMock());
-    $voidResult = $resourcePurger->schedule($uuids, FALSE);
-    $this->assertNull($voidResult);
-  }
+//  /**
+//   * Test figuring out resource purging.
+//   */
+//  public function testResourcePurger() {
+//
+//    $uuids = ['1111-1111'];
+//    $vids = [];
+//    $latestRevision = (new Chain($this))
+//      ->add(NodeInterface::class)
+//      ->getMock();
+//
+//    $chain = $this->getCommonChain()
+//      ->add(ImmutableConfig::class, 'get', 1)
+//      ->add(Data::class, 'getNodeLatestRevision', $latestRevision)
+//      ->add(Data::class, 'getNodeStorage', NodeStorageInterface::class)
+//      ->add(NodeStorageInterface::class, 'revisionIds', $vids);
+//
+//    $resourcePurger = ResourcePurger::create($chain->getMock());
+//    $voidResult = $resourcePurger->schedule($uuids, FALSE);
+//    $this->assertNull($voidResult);
+//  }
 
   /**
    * Get common chain.
@@ -94,7 +90,16 @@ class ResourcePurgerTest extends TestCase {
     return (new Chain($this))
       ->add(Container::class, 'get', $options)
       ->add(ConfigFactoryInterface::class, 'get', ImmutableConfig::class)
-      ->add(DataFactory::class, 'getInstance', Data::class);
+      ->add(ImmutableConfig::class, 'get', 1)
+      ->add(DataFactory::class, 'getInstance', Data::class)
+      ->add(Data::class, 'getNodeStorage', NodeStorageInterface::class)
+      ->add(NodeStorageInterface::class, 'getQuery', QueryInterface::class)
+      ->add(QueryInterface::class, 'accessCheck', QueryInterface::class)
+      ->add(QueryInterface::class, 'condition', QueryInterface::class)
+      ->add(QueryInterface::class, 'execute', [1 => 1])
+      ->add(NodeStorageInterface::class, 'loadRevision', NodeInterface::class)
+      ->add(NodeInterface::class, 'uuid', '1234-abcd')
+    ;
   }
 
 }
