2692ffbf90dd9f7b7f81560daf7e47974e0bd6bd
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2021-01-13 21:34:25 -0800

    Improve unit tests coverage

--- modules/common/tests/src/DatasetInfoTest.php
+++ modules/common/tests/src/DatasetInfoTest.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\Tests\common;
 
+use Drupal\common\Resource;
 use Drupal\common\DatasetInfo;
 use Drupal\Core\DependencyInjection\Container;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\datastore\Service as Datastore;
+use Drupal\datastore\Storage\DatabaseTable;
 use Drupal\metastore\ResourceMapper;
 use Drupal\metastore\Service as Metastore;
 use Drupal\metastore\Storage\Data;
@@ -22,36 +25,7 @@ class DatasetInfoTest extends TestCase {
 
     $expected = [
       'uuid' => 'foo',
-      'notice' => 'The DKAN Metastore module is not enabled, reducing the available information.',
-    ];
-    $result = $datasetInfo->gather('foo');
-
-    $this->assertEquals($expected, $result);
-  }
-
-  public function testDatastoreNotEnabled() {
-    $datasetInfo = DatasetInfo::create($this->getCommonChain()->getMock());
-    $datasetInfo->setMetastore($this->getMockMetastore()->getMock());
-    $datasetInfo->setStorage($this->getMockStorage()->getMock());
-
-    $expected = [
-      'uuid' => 'foo',
-      'notice' => 'The DKAN Datastore module is not enabled, reducing the available information.',
-    ];
-    $result = $datasetInfo->gather('foo');
-
-    $this->assertEquals($expected, $result);
-  }
-
-  public function testHappyPath() {
-    $datasetInfo = DatasetInfo::create($this->getCommonChain()->getMock());
-    $datasetInfo->setMetastore($this->getMockMetastore()->getMock());
-    $datasetInfo->setStorage($this->getMockStorage()->getMock());
-    $datasetInfo->setDatastore($this->getMockDatastore()->getMock());
-    $datasetInfo->setResourceMapper($this->getMockResourceMapper()->getMock());
-
-    $expected = [
-      'uuid' => 'foo',
+      'notice' => 'The DKAN Metastore module is not enabled.',
     ];
     $result = $datasetInfo->gather('foo');
 
@@ -78,30 +52,86 @@ class DatasetInfoTest extends TestCase {
     $this->assertEquals($expected, $result);
   }
 
+  public function testHappyPath() {
+    $datasetInfo = DatasetInfo::create($this->getCommonChain()->getMock());
+    $datasetInfo->setMetastore($this->getMockMetastore()->getMock());
+    $datasetInfo->setStorage($this->getMockStorage()->getMock());
+    $datasetInfo->setDatastore($this->getMockDatastore()->getMock());
+    $datasetInfo->setResourceMapper($this->getMockResourceMapper()->getMock());
+
+    $expected = [
+      'uuid' => 'foo',
+      'node id' => 1,
+      'latest revision' => [
+        'revision id' => 3,
+        'moderation state' => 'published',
+        'modified date' => 'timestamp',
+        'distributions' => [
+          0 => [
+            0 => [
+              'identifier' => 'c9c387a12a',
+              'version' => '1610000000',
+              'file path' => 'public://resources/identifier_version/file.csv',
+              'table name' => 'bar',
+            ],
+          ],
+        ],
+      ],
+    ];
+    $result = $datasetInfo->gather('foo');
+
+    $this->assertEquals($expected, $result);
+  }
+
+  private function getMockDistribution() {
+    return [
+      (object) [
+        '%Ref:downloadURL' => [
+          (object) [
+            'data' => (object) [
+              'identifier' => 'c9c387a12a',
+              'version' => '1610000000',
+              'file path' => 'public://resources/identifier_version/file.csv',
+              'table name' => 'bar',
+            ],
+          ],
+        ],
+      ],
+    ];
+  }
+
   private function getMockMetastore() {
     return (new Chain($this))
-      ->add(Metastore::class);
+      ->add(Metastore::class, 'getResources', $this->getMockDistribution());
   }
 
   private function getMockNode() {
     return (new Chain($this))
-      ->add(Node::class);
+      ->add(Node::class, 'id', 1)
+      ->add(Node::class, 'uuid', 'foo')
+      ->add(Node::class, 'get', FieldItemListInterface::class)
+      ->add(FieldItemListInterface::class, 'getString', 'published')
+      ->add(Node::class, 'getRevisionId', 3)
+      ->add(Node::class, 'getChangedTime', 'timestamp');
   }
 
   private function getMockStorage() {
     return (new Chain($this))
       ->add(DataFactory::class, 'getInstance', Data::class)
-      ->add(Data::class, 'getNodeLatestRevision', $this->getMockNode()->getMock());
+      ->add(Data::class, 'getNodeLatestRevision', $this->getMockNode()->getMock())
+      ->add(Data::class, 'getNodePublishedRevision', $this->getMockNode()->getMock());
   }
 
   private function getMockDatastore() {
     return (new Chain($this))
-      ->add(Datastore::class);
+      ->add(Datastore::class, 'getStorage', DatabaseTable::class)
+      ->add(DatabaseTable::class, 'getTableName', 'bar');
   }
 
   private function getMockResourceMapper() {
     return (new Chain($this))
-      ->add(ResourceMapper::class);
+      ->add(ResourceMapper::class, 'get', Resource::class)
+      ->add(Resource::class, 'getFilePath', 'public://resources/identifier_version/file.csv');
   }
 
   private function getCommonChain() {
