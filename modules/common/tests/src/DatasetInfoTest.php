d59ddbbae6ca9a91ce697b5343afa18aa967c2f6
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2021-01-18 08:54:42 -0800

    Use functional test4 for happy path, keep unit test for edge cases

--- modules/common/tests/src/DatasetInfoTest.php
+++ modules/common/tests/src/DatasetInfoTest.php
@@ -33,15 +33,18 @@ class DatasetInfoTest extends TestCase {
   }
 
   public function testUuidNotFound() {
-    $mockStorage = $this->getMockStorage()
+    $mockStorage = (new Chain($this))
       ->add(DataFactory::class, 'getInstance', Data::class)
       ->add(Data::class, 'getNodeLatestRevision', FALSE);
+    $mockDatastore = (new Chain($this))
+      ->add(Datastore::class);
+    $mockResourceMapper = (new Chain($this))
+      ->add(ResourceMapper::class);
 
     $datasetInfo = DatasetInfo::create($this->getCommonChain()->getMock());
-    $datasetInfo->setMetastore($this->getMockMetastore()->getMock());
     $datasetInfo->setStorage($mockStorage->getMock());
-    $datasetInfo->setDatastore($this->getMockDatastore()->getMock());
-    $datasetInfo->setResourceMapper($this->getMockResourceMapper()->getMock());
+    $datasetInfo->setDatastore($mockDatastore->getMock());
+    $datasetInfo->setResourceMapper($mockResourceMapper->getMock());
 
     $expected = [
       'uuid' => 'foo',
@@ -52,97 +55,13 @@ class DatasetInfoTest extends TestCase {
     $this->assertEquals($expected, $result);
   }
 
-  public function testHappyPath() {
-    $datasetInfo = DatasetInfo::create($this->getCommonChain()->getMock());
-    $datasetInfo->setMetastore($this->getMockMetastore()->getMock());
-    $datasetInfo->setStorage($this->getMockStorage()->getMock());
-    $datasetInfo->setDatastore($this->getMockDatastore()->getMock());
-    $datasetInfo->setResourceMapper($this->getMockResourceMapper()->getMock());
-
-    $expected = [
-      'uuid' => 'foo',
-      'node id' => 1,
-      'latest revision' => [
-        'revision id' => 3,
-        'moderation state' => 'published',
-        'modified date' => 'timestamp',
-        'distributions' => [
-          0 => [
-            0 => [
-              'identifier' => 'c9c387a12a',
-              'version' => '1610000000',
-              'file path' => 'public://resources/identifier_version/file.csv',
-              'table name' => 'bar',
-            ],
-          ],
-        ],
-      ],
-    ];
-    $result = $datasetInfo->gather('foo');
-
-    $this->assertEquals($expected, $result);
-  }
-
-  private function getMockDistribution() {
-    return [
-      (object) [
-        '%Ref:downloadURL' => [
-          (object) [
-            'data' => (object) [
-              'identifier' => 'c9c387a12a',
-              'version' => '1610000000',
-              'file path' => 'public://resources/identifier_version/file.csv',
-              'table name' => 'bar',
-            ],
-          ],
-        ],
-      ],
-    ];
-  }
-
-  private function getMockMetastore() {
-    return (new Chain($this))
-      ->add(Metastore::class, 'getResources', $this->getMockDistribution());
-  }
-
-  private function getMockNode() {
-    return (new Chain($this))
-      ->add(Node::class, 'id', 1)
-      ->add(Node::class, 'uuid', 'foo')
-      ->add(Node::class, 'get', FieldItemListInterface::class)
-      ->add(FieldItemListInterface::class, 'getString', 'published')
-      ->add(Node::class, 'getRevisionId', 3)
-      ->add(Node::class, 'getChangedTime', 'timestamp');
-  }
-
-  private function getMockStorage() {
-    return (new Chain($this))
-      ->add(DataFactory::class, 'getInstance', Data::class)
-      ->add(Data::class, 'getNodeLatestRevision', $this->getMockNode()->getMock())
-      ->add(Data::class, 'getNodePublishedRevision', $this->getMockNode()->getMock());
-  }
-
-  private function getMockDatastore() {
-    return (new Chain($this))
-      ->add(Datastore::class, 'getStorage', DatabaseTable::class)
-      ->add(DatabaseTable::class, 'getTableName', 'bar');
-  }
-
-  private function getMockResourceMapper() {
-    return (new Chain($this))
-      ->add(ResourceMapper::class, 'get', Resource::class)
-      ->add(Resource::class, 'getFilePath', 'public://resources/identifier_version/file.csv');
-  }
-
   private function getCommonChain() {
     $options = (new Options())
       ->add('module_handler', ModuleHandlerInterface::class)
       ->index(0);
 
-    $commonChain = (new Chain($this))
+    return (new Chain($this))
       ->add(Container::class, 'get', $options);
-
-    return $commonChain;
   }
 
 }
