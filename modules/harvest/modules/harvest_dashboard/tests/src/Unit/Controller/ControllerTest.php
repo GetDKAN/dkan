2675e92f312d1aeb7d6671de0ef9f2b0e157a55b
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2021-02-10 21:58:22 -0800

    Refactor unit tests

--- modules/harvest/modules/harvest_dashboard/tests/src/Unit/Controller/ControllerTest.php
+++ modules/harvest/modules/harvest_dashboard/tests/src/Unit/Controller/ControllerTest.php
@@ -3,58 +3,70 @@
 namespace Drupal\Tests\dashboard\Unit\Controller;
 
 use Drupal\Core\DependencyInjection\Container;
-use Drupal\harvest_dashboard\Controller\Controller;
-use Drupal\datastore\Service\Info\ImportInfo;
+use Drupal\common\DatasetInfo;
 use Drupal\harvest\Service as Harvest;
-use Drupal\metastore\Service as Metastore;
+use Drupal\harvest_dashboard\Controller\Controller;
 use MockChain\Chain;
 use MockChain\Options;
 use PHPUnit\Framework\TestCase;
 
 class ControllerTest extends TestCase {
 
-  public function testNoHarvest() {
-
-    $container = $this->containerChain('base')->getMock();
+  public function testNoHarvests() {
+    $container = $this->getCommonMockChain()
+      ->add(Harvest::class, 'getAllHarvestIds', [])
+      ->getMock();
 
     \Drupal::setContainer($container);
 
     $controller = new Controller();
     $response = $controller->harvests();
-    $json = json_encode($response);
 
-    $strings = ["Harvest ID", "Last Run", "# of Datasets"];
+    $json = json_encode($response);
+    $strings = array_merge(Controller::HARVEST_HEADERS, ['No harvests found']);
+
     foreach ($strings as $string) {
       $this->assertStringContainsString($string, $json);
     }
   }
 
-  function testRegisteredHarvest() {
+  public function testRegisteredHarvest() {
+    $container = $this->getCommonMockChain()
+      ->add(Harvest::class, 'getAllHarvestRunInfo', [])
+      ->getMock();
 
-    \Drupal::setContainer($this->containerChain('registered')->getMock());
+    \Drupal::setContainer($container);
 
     $controller = new Controller();
     $response = $controller->harvests();
-    $json = json_encode($response);
 
-    $strings = ["Harvest ID", "Last Run", "# of Datasets", "test", "Never", "N\/A"];
+    $json = json_encode($response);
+    $strings = array_merge(Controller::HARVEST_HEADERS, ['No harvests found']);
 
     foreach ($strings as $string) {
       $this->assertStringContainsString($string, $json);
     }
-
   }
 
   public function testGoodHarvestRun() {
-    \Drupal::setContainer($this->containerChain('good_harvest')->getMock());
+    $time = time();
+
+    $container = $this->getCommonMockChain()
+      ->add(Harvest::class, 'getAllHarvestRunInfo', [$time])
+      ->getMock();
+
+    \Drupal::setContainer($container);
 
     $controller = new Controller();
     $response = $controller->harvests();
-    $json = json_encode($response);
 
-    date_default_timezone_set('EST');
-    $strings = ["Harvest ID", "Last Run", "# of Datasets", "test", "#url", "SUCCESS",
-      json_encode(date('m/d/y H:m:s T', time())), 1];
+    $json = json_encode((array)$response);
+    $strings = array_merge(Controller::HARVEST_HEADERS, [
+      'harvest_link',
+      'SUCCESS',
+      json_encode(date('m/d/y H:m:s T', $time)),
+      '1',
+    ]);
 
     foreach ($strings as $string) {
       $this->assertStringContainsString($string, $json);
@@ -62,132 +74,96 @@ class ControllerTest extends TestCase {
   }
 
   public function testNoDatasets() {
+    $time = time();
 
-    \Drupal::setContainer($this->containerChain('no_datasets')->getMock());
+    $container = $this->getCommonMockChain()
+      ->add(Harvest::class, 'getAllHarvestRunInfo', [$time])
+      ->add(DatasetInfo::class, 'gather', ['notice' => 'Not found'])
+      ->getMock();
+
+    \Drupal::setContainer($container);
 
     $controller = new Controller();
     $response = $controller->harvestDatasets('test');
+
     $json = json_encode($response);
+    $strings = array_merge(Controller::DATASET_HEADERS,);
 
-    $strings = [
-      'Dataset ID',
-      'Title',
-      'Modified Date (Metadata)',
-      'Modified Date (DKAN)',
-      'Status',
-      'Resources'
-    ];
-
+    $this->assertEmpty($response['#rows']);
     foreach ($strings as $string) {
       $this->assertStringContainsString($string, $json);
     }
   }
 
   public function testDataset() {
-    \Drupal::setContainer($this->containerChain('datasets')->getMock());
+    $time = time();
+
+    $info = [
+      'uuid' => 'dataset-1',
+      'title' => 'Title',
+      'revision_id' => '2',
+      'moderation_state' => 'published',
+      'modified_date_metadata' => '2020-01-15',
+      'modified_date_dkan' => '2021-02-11',
+    ];
+
+    $distribution = [
+      'distribution_uuid' => 'dist-1',
+      'fetcher_status' => 'done',
+      'fetcher_percent_done' => 100,
+      'importer_status' => 'done',
+      'importer_percent_done' => 100,
+    ];
+
+    $infoWithDist = array_merge($info, ['distributions' => [$distribution]]);
+
+    $datasetInfo = [
+      'latest_revision' => $infoWithDist,
+    ];
+
+    $container = $this->getCommonMockChain()
+      ->add(Harvest::class, 'getAllHarvestRunInfo', [$time])
+      ->add(DatasetInfo::class, 'gather', $datasetInfo)
+      ->getMock();
+
+    \Drupal::setContainer($container);
 
     $controller = new Controller();
     $response = $controller->harvestDatasets('test');
-    $json = json_encode($response);
+    print_r($response);
 
-    $strings = [
-      'Dataset ID',
-      'Title', 'Modified Date (Metadata)',
-      'Modified Date (DKAN)',
-      'Status',
-      'Resources',
-      'dataset-1',
-      'distro-1'
-    ];
+    $json = json_encode($response);
+    $strings = array_merge(
+      Controller::DATASET_HEADERS,
+      Controller::DISTRIBUTION_HEADERS,
+      $info,
+      $distribution
+    );
 
     foreach ($strings as $string) {
       $this->assertStringContainsString($string, $json);
     }
   }
 
-  private function containerChain(string $mode = 'base'): Chain {
+  private function getCommonMockChain() : Chain {
+    $options = (new Options())
+      ->add('dkan.harvest.service', Harvest::class)
+      ->add('dkan.common.dataset_info', DatasetInfo::class)
+      ->index(0);
 
-    switch($mode) {
-      case 'base':
+    $runInfo = (object) [
+      'status' => (object) [
+        'extract' => 'SUCCESS',
+        'load' => [
+          'dataset-1' => "NEW"
+        ]
+      ]
+    ];
 
-        $options = (new Options())
-          ->add('dkan.harvest.service', Harvest::class)
-          ->add('dkan.metastore.service', Metastore::class)
-          ->add('dkan.datastore.import_info', ImportInfo::class)
-          ->index(0);
-
-        $cc = (new Chain($this))
-          ->add(Container::class, 'get', $options)
-          ->add(Harvest::class, 'getAllHarvestIds', []);
-        break;
-
-      case 'registered':
-
-        $cc = $this->containerChain('base');
-        $cc
-          ->add(Harvest::class, 'getAllHarvestIds', ['test'])
-          ->addd('getAllHarvestRunInfo', []);
-        break;
-
-      case 'good_harvest':
-
-        $runInfo = (object) [
-          'status' => (object) [
-            'extract' => 'SUCCESS',
-            'load' => [
-              'dataset-1' => "NEW"
-            ]
-          ]
-        ];
-
-        $cc = $this->containerChain('registered');
-        $cc
-          ->add(Harvest::class,'getAllHarvestRunInfo', [time()])
-          ->addd('getHarvestRunInfo', json_encode($runInfo));
-        break;
-
-      case 'no_datasets':
-        $cc = $this->containerChain('good_harvest');
-        $cc
-          ->add(Metastore::class, 'get', new \Exception(""));
-        break;
-
-      case 'datasets':
-        $importInfo = (object) [
-          'fileFetcherStatus' => "done",
-          'fileFetcherPercentDone' => "100",
-          'importerStatus' => "done",
-          'importerPercentDone' => '100'
-        ];
-
-        $resourceRef = [
-          '%Ref:downloadURL' =>
-            [
-              (object) ['identifier' => 'file-1', 'data' => [
-                'identifier' => 'file-1', 'version' => 'v1'
-              ]]
-            ]
-        ];
-
-        $dataset = [
-          'title' => "Hello World!",
-          'modified' => "01/20/1985",
-          '%Ref:distribution' =>
-            [
-              (object) ['identifier' => 'distro-1', 'data' => $resourceRef]
-            ],
-          '%modified' => "01/20/1985"
-        ];
-
-        $cc = $this->containerChain('no_datasets');
-        $cc
-          ->add(Metastore::class, 'get', json_encode($dataset))
-          ->add(ImportInfo::class, 'getItem', $importInfo);
-        break;
-
-    }
-
-    return $cc;
+    return (new Chain($this))
+      ->add(Container::class, 'get', $options)
+      ->add(Harvest::class, 'getAllHarvestIds', ['test'])
+      ->add(Harvest::class,'getHarvestRunInfo', json_encode($runInfo));
   }
 
 }
