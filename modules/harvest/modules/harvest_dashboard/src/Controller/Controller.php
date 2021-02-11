0c9c8ec051308a98e19ddea924b4d499b1a0d7de
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2021-02-10 18:24:15 -0800

    Add information to, and refactor harvest dashboard using render arrays

--- modules/harvest/modules/harvest_dashboard/src/Controller/Controller.php
+++ modules/harvest/modules/harvest_dashboard/src/Controller/Controller.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\harvest_dashboard\Controller;
 
+use Drupal\Core\Link;
 use Drupal\Core\Url;
 
 /**
@@ -14,120 +15,140 @@ class Controller {
    */
   public function harvests(): array {
 
+    $harvestsHeader = [
+      'Harvest ID',
+      'Extract Status',
+      'Last Run',
+      '# of Datasets',
+    ];
+
     /** @var \Drupal\harvest\Service $harvestService */
     $harvestService = \Drupal::service('dkan.harvest.service');
 
-    $headers = ["Harvest ID", "Extract Status", "Last Run", "# of Datasets"];
-    $renderArray = $this->table($headers);
+    date_default_timezone_set('EST');
 
+    $rows = [];
     foreach ($harvestService->getAllHarvestIds() as $harvestId) {
-      $tds = [$harvestId, 'Registered', 'Never', 'N/A'];
-      $renderArray[$harvestId] = $this->tr($tds);
-
       $runIds = $harvestService->getAllHarvestRunInfo($harvestId);
-      $runId = end($runIds);
 
-      if ($runId) {
-        $json = $harvestService->getHarvestRunInfo($harvestId, $runId);
-        $info = json_decode($json);
+      if ($runId = end($runIds)) {
+        $info = json_decode($harvestService->getHarvestRunInfo($harvestId, $runId));
 
-        date_default_timezone_set('EST');
-        $time = date('m/d/y H:m:s T', $runId);
-
-        $datasets = array_keys((array) $info->status->load);
-
-        $tds = ["", $info->status->extract, $time, count($datasets)];
-        $renderArray[$harvestId] = $this->tr($tds);
-
-        $url = Url::fromRoute("dkan.harvest.dashboard.datasets",
-          ['harvestId' => $harvestId]);
-
-        $renderArray[$harvestId][1] = array_merge($this->html('<td>'),
-          ['#type' => 'link', '#url' => $url, '#title' => $harvestId]);
+        $rows[] = $this->buildHarvestRow($harvestId, $runId, $info);
       }
     }
 
-    return $renderArray;
+    return [
+      '#theme' => 'table',
+      '#header' => $harvestsHeader,
+      '#rows' => $rows,
+    ];
+  }
+
+  /**
+   * Private.
+   */
+  private function buildHarvestRow(string $harvestId, string $runId, $info) {
+    $url = Url::fromRoute("dkan.harvest.dashboard.datasets", ['harvestId' => $harvestId]);
+
+    return [
+      'harvest_link' => Link::fromTextAndUrl($harvestId, $url),
+      'extract_status' => $info->status->extract,
+      'last_run' => date('m/d/y H:m:s T', $runId),
+      'dataset_count' => count(array_keys((array) $info->status->load)),
+    ];
   }
 
   /**
    * Datasets information for a specific harvest.
    */
   public function harvestDatasets($harvestId) {
+
+    $datasetsHeader = [
+      'Dataset UUID',
+      'Dataset Title',
+      'Revision ID',
+      'Publication Status',
+      'Harvest Status',
+      'Modified Date Metadata',
+      'Modified Date DKAN',
+      'Resources',
+    ];
+
     $load = $this->getHarvestLoadStatus($harvestId);
     $datasets = array_keys($load);
 
-    $headers = array_merge(["Dataset ID", "Title", "Modified Date (Metadata)"],
-      ["Modified Date (DKAN)", "Status", "Resources"]);
+    /** @var \Drupal\common\DatasetInfo $datasetInfoService */
+    $datasetInfoService = \Drupal::service('dkan.common.dataset_info');
 
-    $renderArray = $this->table($headers);
-
+    $rows = [];
     foreach ($datasets as $datasetId) {
-      $dataset = $this->getDataset($datasetId);
-      $renderArray[$datasetId] = $this->html('<tr>');
-
-      $renderArray[$datasetId][1] = $this->td($datasetId);
-      $renderArray[$datasetId][2] = [
-        '#prefix' => '<td colspan="5">',
-        '#suffix' => '</td>',
-        '#markup' => 'Not Published',
-      ];
-
-      if (!empty($dataset)) {
-        $dkanModified = $dataset->{"%modified"};
-
-        $tds = array_merge([$datasetId, $dataset->title, $dataset->modified],
-          [$dkanModified, $load[$datasetId]]);
-
-        $renderArray[$datasetId] = $this->tr($tds);
-
-        $renderArray[$datasetId][6] = $this->html("<td>");
-        $renderArray[$datasetId][6][] = $this->resourcesTable(
-          $this->getResources($datasetId));
-      }
+      $datasetInfo = $datasetInfoService->gather($datasetId);
+      $datasetRow = $this->buildDatasetRow($datasetInfo, $load[$datasetId]);
+      $rows = array_merge($rows, $datasetRow);
     }
 
-    return $renderArray;
+    return [
+      '#theme' => 'table',
+      '#header' => $datasetsHeader,
+      '#rows' => $rows,
+    ];
+  }
+
+  /**
+   * May build 2 rows if data has both published and draft version.
+   */
+  private function buildDatasetRow(array $revisions, string $harvestStatus) : array {
+    $rows = [];
+
+    foreach ($revisions as $revision) {
+      $rows[] = [
+        $revision['uuid'],
+        $revision['title'],
+        $revision['revision_id'],
+        $revision['moderation_state'],
+        $harvestStatus,
+        $revision['modified_date_metadata'],
+        $revision['modified_date_dkan'],
+        $this->buildResourcesTable($revision['distributions']),
+      ];
+    }
+
+    return $rows;
   }
 
   /**
    * Private.
    */
-  private function resourcesTable($resources) {
-    if (empty($resources)) {
-      return ['#markup' => "No Resources"];
+  private function buildResourcesTable(array $distributions) {
+
+    $distributionsHeader = [
+      'Distribution UUID',
+      'Fetch',
+      '%',
+      'Store',
+      '%',
+    ];
+
+    $rows = [];
+    foreach ($distributions as $distribution) {
+      $rows[] = [
+        $distribution['distribution_uuid'],
+        $distribution['fetcher_status'],
+        $distribution['fetcher_percent_done'],
+        $distribution['importer_status'],
+        $distribution['importer_percent_done'],
+      ];
     }
 
-    $headers = ["Identifier", "Drush ID", "Local File", "", "Datastore", ""];
-    $renderArray = $this->table($headers);
+    $build['resourcesTable'] = [
+      '#theme' => 'table',
+      '#header' => $distributionsHeader,
+      '#rows' => $rows,
+      '#empty' => 'No resources',
+    ];
 
-    $service = \Drupal::service('dkan.datastore.import_info');
-    foreach ($resources as $resource) {
-      $identifier = $resource->identifier;
-
-      $data = $resource->data->{"%Ref:downloadURL"}[0]->data;
-
-      $tds = [$identifier, "N/A", "", "N/A", ""];
-
-      $renderArray[$identifier] = $this->tr($tds);
-
-      if ($data) {
-        $info = $service->getItem($data->identifier, $data->version);
-
-        $tds = [
-          $identifier,
-          "{$resource->data->{"%Ref:downloadURL"}[0]->data->identifier}",
-          $info->fileFetcherStatus,
-          "{$info->fileFetcherPercentDone}%",
-          $info->importerStatus,
-          "{$info->importerPercentDone}%",
-        ];
-
-        $renderArray[$identifier] = $this->tr($tds);
-      }
-    }
-
-    return $renderArray;
+    return render($build);
   }
 
   /**
@@ -143,88 +164,6 @@ class Controller {
     $info = json_decode($json);
     $status = $info->status;
     return (array) $status->load;
-  }
-
-  /**
-   * Private.
-   */
-  private function getDataset($datasetId) {
-    $metastore = \Drupal::service('dkan.metastore.service');
-
-    try {
-      $datasetJson = $metastore->get('dataset', $datasetId);
-      $dataset = json_decode($datasetJson);
-      return $dataset;
-    }
-    catch (\Exception $e) {
-      return NULL;
-    }
-  }
-
-  /**
-   * Private.
-   */
-  private function getResources($datasetId) {
-    $dataset = $this->getDataset($datasetId);
-    return isset($dataset->{"%Ref:distribution"})
-      ? $dataset->{"%Ref:distribution"} : [];
-  }
-
-  /**
-   * Private.
-   */
-  private function table($headers): array {
-    $renderArray = $this->html('<table>');
-
-    $renderArray['table_header'] = $this->html('<tr>');
-
-    foreach ($headers as $header) {
-      $renderArray['table_header'][] = $this->th($header);
-    }
-
-    return $renderArray;
-  }
-
-  /**
-   * Private.
-   */
-  private function tr($tds): array {
-    $renderArray = $this->html("<tr>");
-    $counter = 1;
-    foreach ($tds as $td) {
-      $renderArray[$counter] = $this->td($td);
-      $counter++;
-    }
-    return $renderArray;
-  }
-
-  /**
-   * Private.
-   */
-  private function th(string $content) {
-    $html = $this->html('<th>');
-    $html['#markup'] = $content;
-    return $html;
-  }
-
-  /**
-   * Private.
-   */
-  private function td(string $content) {
-    $html = $this->html('<td>');
-    $html['#markup'] = $content;
-    return $html;
-  }
-
-  /**
-   * Private.
-   */
-  private function html(string $tag) {
-    $closing = str_replace("<", "</", $tag);
-    return [
-      '#prefix' => $tag,
-      '#suffix' => $closing,
-    ];
   }
 
 }
