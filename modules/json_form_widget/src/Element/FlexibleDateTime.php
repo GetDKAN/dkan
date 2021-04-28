5e9859f8f424a272b8fba5d587c7f32196976d9c
Dharizza Espinach B <dharizza@gmail.com>
2021-04-28 10:42:46 -0600

    Fix codeclimate.

--- modules/json_form_widget/src/Element/FlexibleDateTime.php
+++ modules/json_form_widget/src/Element/FlexibleDateTime.php
@@ -3,7 +3,6 @@
 namespace Drupal\json_form_widget\Element;
 
 use Drupal\Core\Datetime\Element\Datetime;
-use Drupal\Core\Datetime\Entity\DateFormat;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -18,49 +17,9 @@ class FlexibleDateTime extends Datetime {
    * {@inheritdoc}
    */
   public function getInfo() {
-    $date_format = '';
-    $time_format = '';
-    // Date formats cannot be loaded during install or update.
-    if (!defined('MAINTENANCE_MODE')) {
-      if ($date_format_entity = DateFormat::load('html_date')) {
-        /** @var $date_format_entity \Drupal\Core\Datetime\DateFormatInterface */
-        $date_format = $date_format_entity->getPattern();
-      }
-      if ($time_format_entity = DateFormat::load('html_time')) {
-        /** @var $time_format_entity \Drupal\Core\Datetime\DateFormatInterface */
-        $time_format = $time_format_entity->getPattern();
-      }
-    }
-
-    $class = get_class($this);
-
-    // Note that since this information is cached, the #date_timezone property
-    // is not set here, as this needs to vary potentially by-user.
-    return [
-      '#input' => TRUE,
-      '#element_validate' => [
-        [$class, 'validateDatetime'],
-      ],
-      '#process' => [
-        [$class, 'processDatetime'],
-        [$class, 'processAjaxForm'],
-        [$class, 'processGroup'],
-      ],
-      '#pre_render' => [
-        [$class, 'preRenderGroup'],
-      ],
-      '#theme' => 'datetime_form',
-      '#theme_wrappers' => ['datetime_wrapper'],
-      '#date_date_format' => $date_format,
-      '#date_date_element' => 'date',
-      '#date_date_callbacks' => [],
-      '#date_time_format' => $time_format,
-      '#date_time_element' => 'time',
-      '#date_time_callbacks' => [],
-      '#date_year_range' => '1900:2050',
-      '#date_increment' => 1,
-      '#date_time_required' => FALSE,
-    ];
+    $info = parent::getInfo();
+    $info['#date_time_required'] = FALSE;
+    return $info;
   }
 
   /**
