35642887d53b26f21986f5f321b94bd4e7396414
Dan Feder <dan.feder@civicactions.com>
2021-05-25 10:29:58 -0400

    Fix cypress test

--- cypress/integration/09_admin_links.spec.js
+++ cypress/integration/09_admin_links.spec.js
@@ -8,10 +8,10 @@ context('Administration pages', () => {
     cy.visit(baseurl + "/admin")
     cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
         cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Dataset properties')
+        cy.wrap($el).contains('Configure referencing')
     })
     cy.visit(baseurl + "/admin/dkan/properties")
-    cy.get('.fieldset-legend').should('have.text', 'List of dataset properties with referencing and API endpoint')
+    cy.get('.option').should('contain.text', 'Distribution (distribution)')
   })
 
   it('I should see a link for the SQL endpoint configuration', () => {
