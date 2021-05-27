8e0fe0a86e46e2ac1aa15ada8855400bce0a4997
fmizzell <fmizzell.dev@gmail.com>
2021-05-27 14:59:03 -0500

    Test coverage.

---
+++ modules/metastore/tests/src/Unit/NodeWrapper/DataTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\metastore\Unit\NodeWrapper;
+
+use Drupal\metastore\NodeWrapper\Data;
+use Drupal\node\Entity\Node;
+use MockChain\Chain;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Container;
+
+class DataTest extends TestCase
+{
+  public function test() {
+    $node = (new Chain($this))
+      ->add(Node::class, 'bundle', 'data')
+      ->addd('__isset', true)
+      ->addd('__get', Node::class)
+      ->getMock();
+
+    $container = (new Chain($this))
+      ->add(Container::class)
+      ->getMock();
+
+    \Drupal::setContainer($container);
+
+    $wrapper = new Data($node);
+    $this->assertTrue(
+      $wrapper->getOriginal() instanceof Data
+    );
+  }
+}
