ec9f6e4b1f25477762cdcc7ff9b91cfc0c3df6da
Dharizza Espinach B <dharizza@gmail.com>
2021-07-17 14:57:36 -0600

    Added tests for schema properties helper service.

---
+++ modules/metastore/tests/src/Unit/SchemaPropertiesHelperTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\Tests\metastore\Unit;
+
+use Drupal\Core\DependencyInjection\Container;
+use Drupal\metastore\SchemaPropertiesHelper;
+use Drupal\metastore\SchemaRetriever;
+use MockChain\Chain;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class SchemaPropertiesHelperTest extends TestCase {
+
+  /**
+   * Test.
+   */
+  public function test() {
+    $schema = '{
+      "properties":{
+        "test":{
+          "type":"string",
+          "title":"Test field"
+        },
+        "downloadURL":{
+          "title":"Download URL",
+          "description":"This is an URL field.",
+          "type":"string",
+          "format":"uri"
+        }
+      },
+      "type":"object"
+    }';
+    $expected = [
+      'test' => 'Test field (test)',
+      'downloadURL' => 'Download URL (downloadURL)',
+    ];
+
+    $chain = (new Chain($this))
+      ->add(Container::class, 'get', SchemaRetriever::class)
+      ->add(SchemaRetriever::class, 'retrieve', $schema);
+
+    $schemaPropertiesHelper = SchemaPropertiesHelper::create($chain->getMock());
+    $this->assertEquals($expected, $schemaPropertiesHelper->retrieveSchemaProperties());
+  }
+
+}
