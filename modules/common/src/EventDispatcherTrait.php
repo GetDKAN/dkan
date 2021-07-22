8591595bf364d0cdb40c6eced6a61286a95f6b40
Dan Feder <dan.feder@civicactions.com>
2021-07-22 15:50:31 -0400

    Support both versions of Symfony events

--- modules/common/src/EventDispatcherTrait.php
+++ modules/common/src/EventDispatcherTrait.php
@@ -5,9 +5,11 @@ namespace Drupal\common;
 use Drupal\common\Events\Event;
 
 /**
- * Trait EventDispatcherTrait.
+ * Event dispatcher trait.
  *
- * @package Drupal\common
+ * @todo Refactor as service.
+ *
+ * @codeCoverageIgnore
  */
 trait EventDispatcherTrait {
 
@@ -29,10 +31,54 @@ trait EventDispatcherTrait {
    *   If any of the subscribers registered and Exception it is thrown.
    */
   private function dispatchEvent($eventName, $data, $validator = NULL) {
-    /* @var ContainerAwareEventDispatcher $dispatcher */
+    if ($this->useLegacyDispatcher()) {
+      $this->legacyDispatchEvent($eventName, $data, $validator);
+    }    
     $dispatcher = \Drupal::service('event_dispatcher');
 
-    /* @var \Drupal\common\Events\Event $event */
+    if ($event = $dispatcher->dispatch(new Event($data, $validator), $eventName)) {
+      if ($e = $event->getException()) {
+        throw $e;
+      }
+
+      $data = $event->getData();
+    }
+
+    return $data;
+  }
+
+  /**
+   * If we're on Drupal 8.9 or 9.0, we have to use the old dispatch sig.
+   *
+   * @see https://www.drupal.org/node/3154407
+   *
+   * @return bool
+   *   True if the newer Symfony event system is available.
+   */
+  private function useLegacyDispatcher() {
+    return class_exists('\Symfony\Contracts\EventDispatcher\Event');
+  }
+
+  /**
+   * Legacy version of the dispatchEvent() method.
+   *
+   * @param mixed $eventName
+   *   The name of the event.
+   * @param mixed $data
+   *   The data that will be given to the listeners/subscribers.
+   * @param mixed $validator
+   *   A callable used to validate that the data in the event as it is modified
+   *   keeps its integrity.
+   *
+   * @return mixed
+   *   The data returned by the listeners/subscribers.
+   *
+   * @throws \Exception
+   *   If any of the subscribers registered and Exception it is thrown.
+   */
+  private function legacyDispatchEvent($eventName, $data, $validator = NULL) {
+    $dispatcher = \Drupal::service('event_dispatcher');
+
     if ($event = $dispatcher->dispatch($eventName, new Event($data, $validator))) {
       if ($e = $event->getException()) {
         throw $e;
