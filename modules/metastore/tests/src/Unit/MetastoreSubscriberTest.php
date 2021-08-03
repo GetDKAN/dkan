cd6085ccb5475c53aba1f912bb74391268762107
Dan Feder <dan.feder@civicactions.com>
2021-08-03 13:35:22 -0400

    Better distribution replacing & orphaning behavior (#3597)
    Co-authored-by: Clayton Liddell <clayton.liddell@civicactions.com>

--- modules/metastore/tests/src/Unit/MetastoreSubscriberTest.php
+++ modules/metastore/tests/src/Unit/MetastoreSubscriberTest.php
@@ -2,24 +2,37 @@
 
 namespace Drupal\Tests\metastore\Unit\EventSubscriber;
 
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\Logger\LoggerChannelInterface;
+
 use Drupal\common\Resource;
 use Drupal\common\Events\Event;
 use Drupal\common\Storage\JobStore;
-use MockChain\Chain;
-use MockChain\Options;
-use PHPUnit\Framework\TestCase;
-use Drupal\Core\Logger\LoggerChannelFactory;
-use Drupal\Core\Logger\LoggerChannelInterface;
-use Symfony\Component\DependencyInjection\Container;
 use Drupal\metastore\EventSubscriber\MetastoreSubscriber;
 use Drupal\metastore\Service;
 use Drupal\metastore\ResourceMapper;
 use Drupal\Tests\metastore\Unit\ServiceTest;
 
+use MockChain\Chain;
+use MockChain\Options;
+use PHPUnit\Framework\TestCase;
+use RootedData\RootedJsonData;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 /**
+ * Unit tests for the `MetastoreSubscriber` class.
  *
+ * @see \Drupal\metastore\EventSubscriber\MetastoreSubscriber
  */
 class MetastoreSubscriberTest extends TestCase {
+
+  /**
+   * Host protocol and domain for testing file path and download URL.
+   *
+   * @var string
+   */
+  const HOST = 'http://h-o.st';
 
   /**
    * The ValidMetadataFactory class used for testing.
@@ -28,72 +41,124 @@ class MetastoreSubscriberTest extends TestCase {
    */
   protected $validMetadataFactory;
 
+  /**
+   * @inheritDoc
+   */
   protected function setUp(): void {
     parent::setUp();
+    $this->initializeContainerWithRequestStack();
     $this->validMetadataFactory = ServiceTest::getValidMetadataFactory($this);
   }
 
   /**
-   * Test clean up.
+   * Initialize `\Drupal::$container` with a custom 'request_stack' service.
    */
-  public function testResourceMapperCleanUp() {
-
-    $url = 'http://hello.world/file.csv';
-    $resource = new Resource($url, 'text/csv');
-    $dist = '{"data":{"%Ref:downloadURL":[{"data":{"identifier":"qwerty","version":"uiop","perspective":"source"}}]}}';
-    $dist = $this->validMetadataFactory->get($dist, 'distribution');
-    $event = new Event($resource);
-
+  protected function initializeContainerWithRequestStack(): void {
     $options = (new Options())
-      ->add('logger.factory', LoggerChannelFactory::class)
-      ->add('dkan.metastore.service', Service::class)
-      ->add('dkan.metastore.resource_mapper', ResourceMapper::class)
-      ->add("database", Connection::class)
+      ->add('request_stack', RequestStack::class)
       ->index(0);
-
-    $chain = (new Chain($this))
+    $container = (new Chain($this))
       ->add(Container::class, 'get', $options)
-      ->add(Service::class, 'get', $dist)
-      ->add(ResourceMapper::class, 'get', $resource)
-      ->add(ResourceMapper::class, 'remove')
-      ->add(LoggerChannelFactory::class, 'get', LoggerChannelInterface::class)
-      ->add(LoggerChannelInterface::class, 'error', NULL, 'errors');
-
-    $subscriber = MetastoreSubscriber::create($chain->getMock());
-    $test = $subscriber->cleanResourceMapperTable($event);
-    $this->assertEmpty($chain->getStoredInput('errors'));
+      ->add(RequestStack::class, 'getCurrentRequest', new class { public function getHost() { return MetastoreSubscriberTest::HOST; } })
+      ->getMock();
+    \Drupal::setContainer($container);
   }
 
   /**
-   * Test exception.
+   * Create a JSON Metadata distribution with the given file path.
+   *
+   * @param string $file_path
+   *   Optional resource file path and download URL.
    */
-  public function testResourceMapperCleanUpException() {
+  protected function createDistribution(string $file_path = ''): RootedJsonData {
+    $file_path = $file_path ?: (self::HOST . '/' . uniqid() . '.csv');
+    return $this->validMetadataFactory->get(json_encode([
+      'identifier' => uniqid(),
+      'data' => [
+        '@type' => 'dcat:Distribution',
+        'title' => 'Test Distribution',
+        'format' => 'csv',
+        'downloadURL' => $file_path,
+        '%Ref:downloadURL' => [
+          [
+            'data' => [
+              'identifier' => uniqid(),
+              'filePath' => $file_path,
+              'mimeType' => 'text/csv',
+              'perspective' => 'source',
+              'version' => '1',
+            ],
+          ],
+        ],
+      ],
+    ]), 'distribution');
+  }
 
-    $url = 'http://hello.world/file.csv';
-    $resource = new Resource($url, 'text/csv');
-    $dist = '{"data":{"%Ref:downloadURL":[{"data":{"identifier":"qwerty","version":"uiop","perspective":"source"}}]}}';
-    $dist = $this->validMetadataFactory->get($dist, 'distribution');
-    $event = new Event($resource);
+  /**
+   * Tests that if a resource is not in use elsewhere, it is removed.
+   */
+  public function testCleanupWithResourceNotInUseElsewhere(): void {
+    $resource_path = self::HOST . '/single.csv';
+    // Create a test resource.
+    $resource = new Resource($resource_path, 'text/csv');
+    // Create a test distribution.
+    $distribution = $this->createDistribution($resource_path);
 
     $options = (new Options())
       ->add('logger.factory', LoggerChannelFactory::class)
       ->add('dkan.metastore.service', Service::class)
       ->add('dkan.metastore.resource_mapper', ResourceMapper::class)
-      ->add("database", Connection::class)
+      ->add('database', Connection::class)
+      ->index(0);
+
+    $removal_message = 'Removed Successfully!';
+
+    $chain = (new Chain($this))
+      ->add(Container::class, 'get', $options)
+      ->add(Service::class, 'get', $distribution)
+      ->add(Service::class, 'getAll', [$distribution])
+      ->add(ResourceMapper::class, 'get', $resource)
+      ->add(ResourceMapper::class, 'remove', new \Exception($removal_message))
+      ->add(LoggerChannelFactory::class, 'get', LoggerChannelInterface::class)
+      ->add(LoggerChannelInterface::class, 'error', NULL, 'errors');
+
+    $this->expectException(\Exception::class);
+    $this->expectExceptionMessage($removal_message);
+
+    $subscriber = MetastoreSubscriber::create($chain->getMock());
+    $subscriber->cleanResourceMapperTable(new Event($distribution->{'$.identifier'}));
+  }
+
+  /**
+   * Tests that if a resource is in use elsewhere, it is not removed.
+   *
+   * @doesNotPerformAssertions
+   */
+  public function testCleanupWithResourceInUseElsewhere(): void {
+    $resource_path = self::HOST . '/single.csv';
+    // Create a test resource.
+    $resource = new Resource($resource_path, 'text/csv');
+    // Create a test distribution.
+    $distribution_1 = $this->createDistribution($resource_path);
+    $distribution_2 = $this->createDistribution($resource_path);
+
+    $options = (new Options())
+      ->add('logger.factory', LoggerChannelFactory::class)
+      ->add('dkan.metastore.service', Service::class)
+      ->add('dkan.metastore.resource_mapper', ResourceMapper::class)
+      ->add('database', Connection::class)
       ->index(0);
 
     $chain = (new Chain($this))
       ->add(Container::class, 'get', $options)
-      ->add(Service::class, 'get', $dist)
-      ->add(ResourceMapper::class, 'get', NULL)
-      ->add(ResourceMapper::class, 'remove')
+      ->add(Service::class, 'get', $distribution_1)
+      ->add(Service::class, 'getAll', [$distribution_1, $distribution_2])
+      ->add(ResourceMapper::class, 'get', $resource)
+      ->add(ResourceMapper::class, 'remove', new \LogicException('Erroneous attempt to remove resource which is in use elsewhere'))
       ->add(LoggerChannelFactory::class, 'get', LoggerChannelInterface::class)
       ->add(LoggerChannelInterface::class, 'error', NULL, 'errors');
 
     $subscriber = MetastoreSubscriber::create($chain->getMock());
-    $test = $subscriber->cleanResourceMapperTable($event);
-    $this->assertContains('Failed to remove resource', $chain->getStoredInput('errors')[0]);
-
+    $subscriber->cleanResourceMapperTable(new Event($distribution_1->{'$.identifier'}));
   }
-
 }
