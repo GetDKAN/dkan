dc6a17d3626d79bae4b082455d2d50a4e21ae207
Clayton Liddell <clayton.liddell@civicactions.com>
2021-08-16 17:25:17 -0500

    Disambiguate check for potentially null column names.

--- modules/datastore/modules/datastore_mysql_import/src/Service/MysqlImport.php
+++ modules/datastore/modules/datastore_mysql_import/src/Service/MysqlImport.php
@@ -94,8 +94,11 @@ class MysqlImport extends Importer {
       return $this->setResultError($e->getMessage());
     }
 
-    // Extract the columns names using the header line.
-    $columns = str_getcsv($header_line);
+    // Extract the columns names from the header line, and ensure any empty
+    // column names are null-coalesced to an empty string.
+    $columns = array_map(function ($column) {
+      return $column ?? '';
+    }, str_getcsv($header_line));
     // Generate sanitized table headers from column names.
     $headers = $this->generateTableHeaders($columns);
 
@@ -214,7 +217,7 @@ class MysqlImport extends Importer {
   private function generateTableHeaders(array $columns): array {
     return array_replace([], ...array_map(function ($column) {
       // Sanitize the supplied table header to generate a unique column name.
-      $header = $this->sanitizeHeader($column ?? '');
+      $header = $this->sanitizeHeader($column);
 
       if (is_numeric($header) || in_array($header, self::RESERVED_WORDS)) {
         // Prepend "_" to column name that are not allowed in MySQL
