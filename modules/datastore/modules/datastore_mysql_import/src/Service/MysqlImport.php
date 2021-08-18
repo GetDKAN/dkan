aef72f3bb4bb947b4bd6788cfc9e2931b4bed77a
Clayton Liddell <clayton.liddell@civicactions.com>
2021-08-18 10:38:22 -0500

    Fix CSV header collisions in mysql importer (#3627)

--- modules/datastore/modules/datastore_mysql_import/src/Service/MysqlImport.php
+++ modules/datastore/modules/datastore_mysql_import/src/Service/MysqlImport.php
@@ -209,10 +209,12 @@ class MysqlImport extends Importer {
    *   List of column names.
    *
    * @return array
-   *   List of sanitized table headers keyed by original column names.
+   *   List of sanitized table headers.
    */
   private function generateTableHeaders(array $columns): array {
-    return array_replace([], ...array_map(function ($column) {
+    $headers = [];
+
+    foreach ($columns as $column) {
       // Sanitize the supplied table header to generate a unique column name;
       // null-coalesce potentially NULL column names to empty strings.
       $header = $this->sanitizeHeader($column ?? '');
@@ -228,8 +230,17 @@ class MysqlImport extends Importer {
       // column length.
       $header = $this->truncateHeader($header);
 
-      return [$column => $header];
-    }, $columns));
+      // Generate unique numeric suffix for the header if a header already
+      // exists with the same name.
+      for ($i = 2; isset($headers[$header]); $i++) {
+        $suffix = '_' . $i;
+        $header = substr($header, 0, self::MAX_COLUMN_LENGTH - strlen($suffix)) . $suffix;
+      }
+
+      $headers[$header] = $header;
+    }
+
+    return $headers;
   }
 
   /**
