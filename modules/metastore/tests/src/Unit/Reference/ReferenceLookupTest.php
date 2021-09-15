c8c0973c60e4d4f846c966feca6dc2b10e82e524
Dan Feder <dan.feder@civicactions.com>
2021-09-15 19:03:10 -0400

    Comment out unit tests for now

--- modules/metastore/tests/src/Unit/Reference/ReferenceLookupTest.php
+++ modules/metastore/tests/src/Unit/Reference/ReferenceLookupTest.php
@@ -19,56 +19,53 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- *
+ * @todo Finish these tests.
  */
 class OrphanCheckerTest extends TestCase {
 
-  /**
-   *
-   */
-  public function testGetReferencers() {
-    $options = (new Options)
-      ->add('dkan.metastore.reference_lookup', ReferenceLookup::class)
-      ->add('dkan.metastore.storage', DataFactory::class)
-      ->add('dkan.metasotre.metastore_item_factory', NodeDataFactory::class)
-      ->add('cache_tags.invalidator', CacheTagsInvalidator::class)
-      ->add('module_handler', ModuleHandler::class);
+  // /**
+  //  *
+  //  */
+  // public function testGetReferencers() {
+  //   $options = (new Options)
+  //     ->add('dkan.metastore.reference_lookup', ReferenceLookup::class)
+  //     ->add('dkan.metastore.storage', DataFactory::class)
+  //     ->add('dkan.metasotre.metastore_item_factory', NodeDataFactory::class)
+  //     ->add('cache_tags.invalidator', CacheTagsInvalidator::class)
+  //     ->add('module_handler', ModuleHandler::class);
 
-    $container = (new Chain($this))
-      ->add(ContainerInterface::class, 'get', $options)
-      ->add(DataFactory::class, 'getInstance', NodeData::class)
-      ->add(NodeDataFactory::class, 'getInstance', Data::class)
-      ->getMock();
+  //   $container = (new Chain($this))
+  //     ->add(ContainerInterface::class, 'get', $options)
+  //     ->add(DataFactory::class, 'getInstance', NodeData::class)
+  //     ->add(NodeDataFactory::class, 'getInstance', Data::class)
+  //     ->getMock();
 
-    \Drupal::setContainer($container);
-    $referenceLookup = \Drupal::service('dkan.metastore.reference_lookup');
+  //   \Drupal::setContainer($container);
+  //   $referenceLookup = \Drupal::service('dkan.metastore.reference_lookup');
 
-    $service = new OrphanChecker($config, $queue);
-    $service->processReferencesInDeletedDataset((object) ["name" => "1"]);
+  //   $referencers = $referenceLookup->getReferencers('dataset', '123', 'distribution')
 
-    $expected = [0 => ['name', 1]];
-    $this->assertEquals($expected, $queueChain->getStoredInput('items'));
-  }
+  // }
 
-  /**
-   *
-   */
-  public function testUpdate() {
-    $config = (new Chain($this))
-      ->add(ConfigFactory::class, 'get', ImmutableConfig::class)
-      ->add(ImmutableConfig::class, 'get', ['name'])
-      ->getMock();
+  // /**
+  //  *
+  //  */
+  // public function testUpdate() {
+  //   $config = (new Chain($this))
+  //     ->add(ConfigFactory::class, 'get', ImmutableConfig::class)
+  //     ->add(ImmutableConfig::class, 'get', ['name'])
+  //     ->getMock();
 
-    $queueChain = (new Chain($this))
-      ->add(QueueFactory::class, 'get', QueueInterface::class)
-      ->add(QueueInterface::class, 'createItem', NULL, 'items');
-    $queue = $queueChain->getMock();
+  //   $queueChain = (new Chain($this))
+  //     ->add(QueueFactory::class, 'get', QueueInterface::class)
+  //     ->add(QueueInterface::class, 'createItem', NULL, 'items');
+  //   $queue = $queueChain->getMock();
 
-    $service = new OrphanChecker($config, $queue);
-    $service->processReferencesInUpdatedDataset((object) ["name" => "1"], (object) ["name" => "2"]);
+  //   $service = new OrphanChecker($config, $queue);
+  //   $service->processReferencesInUpdatedDataset((object) ["name" => "1"], (object) ["name" => "2"]);
 
-    $expected = [0 => ['name', 1]];
-    $this->assertEquals($expected, $queueChain->getStoredInput('items'));
-  }
+  //   $expected = [0 => ['name', 1]];
+  //   $this->assertEquals($expected, $queueChain->getStoredInput('items'));
+  // }
 
 }
