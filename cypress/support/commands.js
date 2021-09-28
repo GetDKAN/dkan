8a2af60c28cf41f66f7da51f8a97380caee9b6c4
Benjamin Panga <bmdvpanga@gmail.com>
2021-09-28 15:30:29 -0400

    Adding cypress tests for file upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,6 @@
 /**
  * Drupal Collection
+ * Cypress file handling credit: https://stackoverflow.com/questions/47074225/how-to-test-file-inputs-with-cypress
  */
 Cypress.Commands.add("drupalLogin", (user, password) => {
     return cy.request({
@@ -33,3 +34,16 @@ Cypress.Commands.add("drupalDrushCommand", (command) => {
 
     return cy.exec(execCmd);
 });
+Cypress.Commands.add('uploadFile', { prevSubject: true }, (subject, fileName, fileType = '') => {
+  cy.fixture(fileName,'binary').then(content => {
+    const blob =  Cypress.Blob.binaryStringToBlob(content, fileType)
+      const el = subject[0];
+      const testFile = new File([blob], fileName, {type: fileType});
+      const dataTransfer = new DataTransfer();
+
+      dataTransfer.items.add(testFile);
+      el.files = dataTransfer.files;
+      cy.wrap(subject).trigger('change', { force: true });
+  });
+});
+
