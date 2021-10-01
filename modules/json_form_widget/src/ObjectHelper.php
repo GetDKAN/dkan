bbdc53925cddabf27e7d83c0d38f7f1c3f51c380
Clayton Liddell <clayton.liddell@civicactions.com>
2021-10-01 16:47:48 -0500

    Add support for dataset data dictionary specification

--- modules/json_form_widget/src/ObjectHelper.php
+++ modules/json_form_widget/src/ObjectHelper.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\json_form_widget;
 
+use Drupal\Core\Form\FormStateInterface;
+
+use Drupal\json_form_widget\FieldTypeRouter;
+
 /**
  * Class ObjectHelper.
  */
@@ -10,7 +14,7 @@ class ObjectHelper {
   /**
    * Handle form element for an object.
    */
-  public function handleObjectElement($definition, $data, $form_state, $parent) {
+  public function handleObjectElement(array $definition, $data, FormStateInterface $form_state, FieldTypeRouter $parent): array {
     $property_schema = $definition['schema'];
     $field_name = $definition['name'];
     $element[$field_name] = [
@@ -32,7 +36,39 @@ class ObjectHelper {
       ];
       $element[$field_name][$child] = $parent->getFormElement($type, $subdefinition, $value, $property_schema, $form_state);
     }
+
+    foreach ($property_schema->allOf ?? [] as $condition) {
+      $element[$field_name] = $this->handleCondition($condition, $element[$field_name], $data, $form_state, $parent);
+    }
+
     return $element;
   }
 
+  protected function handleCondition(object $condition, array $field, $data, FormStateInterface $form_state, FieldTypeRouter $parent): array {
+    if (isset($condition->if, $condition->then) && $field_states = $this->validateElementAgainstSubschema($field, $condition->if)) {
+      $subdefinition = [
+        'name' => 'temp',
+        'schema' => $condition->then,
+      ];
+      $field += $this->handleObjectElement($subdefinition, $data, $form_state, $parent)['temp'];
+      foreach ($field_states as $name => $value) {
+        $field['#states']['visible'][':input[name="' . $name . '"]']['value'] = $value;
+      }
+    }
+
+    return $field;
+  }
+
+  protected function validateElementAgainstSubschema(array $field, object $subschema): ?array {
+    $field_states = [];
+
+    foreach ($subschema->properties ?? [] as $prop => $value) {
+      if (!isset($field[$prop], $value->const)) {
+        return NULL;
+      }
+      $field_states[$prop] = $value->const;
+    }
+
+    return $field_states;
+  }
 }
