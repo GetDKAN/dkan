be0fe122635429239bc4ffdc43f70322f70901bc
Dharizza Espinach B <dharizza@gmail.com>
2021-10-02 18:56:20 -0600

    Add support to load form using language dependant schemas.

--- modules/json_form_widget/src/FormBuilder.php
+++ modules/json_form_widget/src/FormBuilder.php
@@ -3,6 +3,7 @@
 namespace Drupal\json_form_widget;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Language\LanguageManager;
 use Drupal\metastore\SchemaRetriever;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
@@ -48,6 +49,13 @@ class FormBuilder implements ContainerInjectionInterface {
   protected $loggerFactory;
 
   /**
+   * Language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManager
+   */
+  protected $languageManager;
+
+  /**
    * Inherited.
    *
    * @{inheritdocs}
@@ -57,18 +65,20 @@ class FormBuilder implements ContainerInjectionInterface {
       $container->get('dkan.metastore.schema_retriever'),
       $container->get('json_form.router'),
       $container->get('json_form.schema_ui_handler'),
-      $container->get('logger.factory')
+      $container->get('logger.factory'),
+      $container->get('language_manager')
     );
   }
 
   /**
    * Constructor.
    */
-  public function __construct(SchemaRetriever $schema_retriever, FieldTypeRouter $router, SchemaUiHandler $schema_ui_handler, LoggerChannelFactory $logger_factory) {
+  public function __construct(SchemaRetriever $schema_retriever, FieldTypeRouter $router, SchemaUiHandler $schema_ui_handler, LoggerChannelFactory $logger_factory, LanguageManager $language_manager) {
     $this->schemaRetriever = $schema_retriever;
     $this->router = $router;
     $this->schemaUiHandler = $schema_ui_handler;
     $this->loggerFactory = $logger_factory;
+    $this->languageManager = $language_manager;
   }
 
   /**
@@ -81,9 +91,11 @@ class FormBuilder implements ContainerInjectionInterface {
       if (!empty($type) && $type !== $schema_name) {
         $schema_name = $type;
       }
-      $schema = $this->schemaRetriever->retrieve($schema_name);
+
+      $language = $this->languageManager->getCurrentLanguage()->getId();
+      $schema = $this->schemaRetriever->retrieve($schema_name, $language);
       $this->schema = json_decode($schema);
-      $this->schemaUiHandler->setSchemaUi($schema_name);
+      $this->schemaUiHandler->setSchemaUi($schema_name, $language);
       $this->router->setSchema($this->schema);
     }
     catch (\Exception $exception) {
