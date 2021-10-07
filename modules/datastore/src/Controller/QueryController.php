GetDKAN/dkan

commit c75cdcd960d1fe3dc5c532ec89cc4296846a26cf

WCMS-6166: Get query maximum row limit from config (#3689)
* WCMS-6166: Get query row limits from config

* Add param comment

* Remove extraneous default `rows_limit` initialization.

Co-authored-by: Clayton Liddell <clayton.liddell@civicactions.com>

modules/datastore/src/Controller/QueryController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\datastore\Controller;
 
+use Drupal\Core\Config\ConfigFactory;
 use Drupal\common\DatasetInfo;
 use Drupal\datastore\Service\DatastoreQuery;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -46,18 +47,27 @@ class QueryController implements ContainerInjectionInterface {
   protected $datasetInfo;
 
   /**
+   * ConfigFactory object.
+   *
+   * @var Drupal\Core\Config\ConfigFactory
+   */
+  private $configFactory;
+
+  /**
    * Api constructor.
    */
   public function __construct(
     Service $datastoreService,
     RequestStack $requestStack,
     DatasetInfo $datasetInfo,
-    MetastoreApiResponse $metastoreApiResponse
+    MetastoreApiResponse $metastoreApiResponse,
+    ConfigFactory $configFactory
   ) {
     $this->datastoreService = $datastoreService;
     $this->requestStack = $requestStack;
     $this->datasetInfo = $datasetInfo;
     $this->metastoreApiResponse = $metastoreApiResponse;
+    $this->configFactory = $configFactory;
   }
 
   /**
@@ -68,7 +78,8 @@ class QueryController implements ContainerInjectionInterface {
       $container->get('dkan.datastore.service'),
       $container->get('request_stack'),
       $container->get('dkan.common.dataset_info'),
-      $container->get('dkan.metastore.api_response')
+      $container->get('dkan.metastore.api_response'),
+      $container->get('config.factory')
     );
   }
 
@@ -294,7 +305,8 @@ class QueryController implements ContainerInjectionInterface {
     }
     try {
       $payloadJson = RequestParamNormalizer::fixTypes($payloadJson, file_get_contents(__DIR__ . "/../../docs/query.json"));
-      $datastoreQuery = new DatastoreQuery($payloadJson);
+      $rows_limit = (int) $this->configFactory->get('datastore.settings')->get('rows_limit') ?: 500;
+      $datastoreQuery = new DatastoreQuery($payloadJson, $rows_limit);
       $result = $this->datastoreService->runQuery($datastoreQuery);
     }
     catch (\Exception $e) {

modules/datastore/src/Service/DatastoreQuery.php
@@ -14,9 +14,14 @@ class DatastoreQuery extends RootedJsonData {
    *
    * @param string $json
    *   JSON query string from API payload.
+   * @param int $rows_limit
+   *   Maxmimum rows of data to return.
    */
-  public function __construct(string $json) {
+  public function __construct(string $json, int $rows_limit) {
     $schema = file_get_contents(__DIR__ . "/../../docs/query.json");
+    $q = json_decode($schema);
+    $q->properties->limit->maximum = $rows_limit;
+    $schema = json_encode($q);
     parent::__construct($json, $schema);
     $this->populateDefaults();
   }

commit 3c10ff2a5c1fde19c85c94379741a8ed1a4a3f90

Fix API bug introduced in #3689

modules/datastore/src/Controller/QueryController.php
@@ -54,6 +54,13 @@ class QueryController implements ContainerInjectionInterface {
   private $configFactory;
 
   /**
+   * Default API rows limit.
+   *
+   * @var int
+   */
+  protected const DEFAULT_ROWS_LIMIT = 500;
+
+  /**
    * Api constructor.
    */
   public function __construct(
@@ -97,7 +104,7 @@ class QueryController implements ContainerInjectionInterface {
     );
 
     try {
-      $datastoreQuery = new DatastoreQuery($payloadJson);
+      $datastoreQuery = new DatastoreQuery($payloadJson, $this->getRowsLimit());
     }
     catch (\Exception $e) {
       return $this->getResponseFromException($e, 400);
@@ -305,8 +312,7 @@ class QueryController implements ContainerInjectionInterface {
     }
     try {
       $payloadJson = RequestParamNormalizer::fixTypes($payloadJson, file_get_contents(__DIR__ . "/../../docs/query.json"));
-      $rows_limit = (int) $this->configFactory->get('datastore.settings')->get('rows_limit') ?: 500;
-      $datastoreQuery = new DatastoreQuery($payloadJson, $rows_limit);
+      $datastoreQuery = new DatastoreQuery($payloadJson, $this->getRowsLimit());
       $result = $this->datastoreService->runQuery($datastoreQuery);
     }
     catch (\Exception $e) {
@@ -378,6 +384,16 @@ class QueryController implements ContainerInjectionInterface {
     $resource = (object) ["id" => $identifier, "alias" => "t"];
     $data->resources = [$resource];
     $json = json_encode($data);
+  }
+
+  /**
+   * Get the rows limit for datastore queries.
+   *
+   * @return int
+   *   API rows limit.
+   */
+  protected function getRowsLimit(): int {
+    return (int) ($this->configFactory->get('datastore.settings')->get('rows_limit') ?: self::DEFAULT_ROWS_LIMIT);
   }
 
 }