4902842bd34ca8f5bbdbbb9a6d748056a1d2a596
Janette Day <janette.day@civicactions.com>
2021-11-07 17:50:47 -0600

    more test fixin

--- cypress/integration/08_admin_views.spec.js
+++ cypress/integration/08_admin_views.spec.js
@@ -41,7 +41,7 @@ context('Admin content and dataset views', () => {
         cy.get('h1').should('have.text', 'Create Data')
     })
 
-    it('The dataset edit link should go to the json form.', () => {
+    it('User can archive, publish, and edit, and delete a dataset. The edit link on the admin view should go to the json form.', () => {
         // Create a dataset.
         cy.visit(baseurl + "/node/add/data")
         cy.wait(2000)
@@ -81,7 +81,7 @@ context('Admin content and dataset views', () => {
         cy.get('h1').should('contain.text', 'Edit Data')
         cy.get('#edit-delete').click({ force:true })
         cy.get('#edit-submit').click({ force:true })
-        cy.get('h2').should('contain.text','Welcome to DKAN' )
+        cy.get('.messages--status').should('contain','has been deleted')
     })
 
   })
