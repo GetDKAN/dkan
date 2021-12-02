43de4d124bf114809a5ee518a5a936708a429ab6
Clayton Liddell <clayton.liddell@civicactions.com>
2021-12-02 12:18:13 -0600

    Delay establishing datastore db connection.
    Do not establish a connection to the database for datastore interactions
    until absolutely necessary to prevent the DB connection from expiring
    before it's used.

--- modules/datastore/src/Storage/DatabaseConnectionFactory.php
+++ modules/datastore/src/Storage/DatabaseConnectionFactory.php
@@ -5,21 +5,27 @@ namespace Drupal\datastore\Storage;
 use Drupal\Core\Database\Database;
 
 /**
- * Create second datastore endpoint at runtime with unbuffered queries.
- *
- * @return \Drupal\Core\Database\Connection
- *   New datastore connection object.
+ * Creates a datastore DB connection with unbuffered queries.
  */
 class DatabaseConnectionFactory {
 
   /**
-   * Gets the connection object for the specified database key and target.
+   * Create a database connection factory object.
    */
-  public static function getConnection() {
+  public function __construct() {
     $info = Database::getConnectionInfo();
     $datastoreInfo = $info['default'];
     $datastoreInfo['pdo'][\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY] = FALSE;
     Database::addConnectionInfo('datastore', 'default', $datastoreInfo);
+  }
+
+  /**
+   * Gets the connection object for the specified database key and target.
+   *
+   * @return \Drupal\Core\Database\Connection
+   *   New datastore connection object.
+   */
+  public function getConnection() {
     return Database::getConnection('default', 'datastore');
   }
 
