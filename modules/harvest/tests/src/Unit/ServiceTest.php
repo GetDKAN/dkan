e178851d5d16a1f8834d59d9c4d323da4baa6619
Dan Feder <dan.feder@civicactions.com>
2022-01-13 23:09:01 -0500

    Complete test coverage for publish method

--- modules/harvest/tests/src/Unit/ServiceTest.php
+++ modules/harvest/tests/src/Unit/ServiceTest.php
@@ -224,17 +224,21 @@ class ServiceTest extends TestCase {
   public function testPublish() {
 
     $datasetUuids = ['abcd-1001', 'abcd-1002', 'abcd-1003', 'abcd-1004'];
-    $lastRunInfo = (object) [
-      'status' => [
-        'extracted_items_ids' => $datasetUuids,
-        'load' => [
-          'abcd-1001' => "SUCCESS",
-          'abcd-1002' => "SUCCESS",
-          'abcd-1003' => "SUCCESS",
-          'abcd-1004' => "FAILURE",
+    $lastRunInfo = (new Sequence())
+      ->add(json_encode((object) [
+        'status' => [
+          'extracted_items_ids' => $datasetUuids,
+          'load' => [
+            'abcd-1001' => "SUCCESS",
+            'abcd-1002' => "SUCCESS",
+            'abcd-1003' => "SUCCESS",
+            'abcd-1004' => "FAILURE",
+          ],
         ],
-      ],
-    ];
+      ]))
+      ->add(json_encode((object) [
+        'status' => [],
+      ]));
 
     $metastorePublicationResults = (new Sequence())
       // abcd-1001 will be skipped since already published.
@@ -249,7 +253,7 @@ class ServiceTest extends TestCase {
       ->add(LoggerChannelInterface::class, 'error', NULL, 'error');
 
     $container = $this->getCommonMockChain()
-      ->add(DatabaseTable::class, "retrieve", json_encode($lastRunInfo))
+      ->add(DatabaseTable::class, "retrieve", $lastRunInfo)
       ->add(Metastore::class, 'publish', $metastorePublicationResults);
 
     $service = HarvestService::create($container->getMock());
@@ -261,6 +265,9 @@ class ServiceTest extends TestCase {
     $loggerResult = $logger->getStoredInput('error')[0];
     $error = 'Error publishing dataset abcd-1002 in harvest 1: FooBar';
     $this->assertEquals($error, $loggerResult);
+
+    $result = $service->publish('2');
+    $this->assertEmpty($result);
   }
 
   /**
