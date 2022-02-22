62c5723163149a97a8ddd6512a384089fcd4c881
Clayton Liddell <clayton.liddell@civicactions.com>
2022-02-22 12:13:50 -0600

    Combine Cypress test setup into before functions.

--- cypress/integration/11_admin_dataset_file_upload.spec.js
+++ cypress/integration/11_admin_dataset_file_upload.spec.js
@@ -1,17 +1,13 @@
 import * as dkan from '../support/helpers/dkan'
 
 context('Admin dataset file upload', () => {
-  let baseurl = Cypress.config().baseUrl
-  beforeEach(() => {
-    cy.drupalLogin('testeditor', 'testeditor')
-  })
-
   context('Create dataset with remote file', () => {
-    it('can fill up the form with distribution and submit', () => {
-      const fileUrl = 'https://dkan-default-content-files.s3.amazonaws.com/phpunit/district_centerpoints_small.csv'
-      const title = dkan.generateRandomString()
+    const fileUrl = 'https://dkan-default-content-files.s3.amazonaws.com/phpunit/district_centerpoints_small.csv'
+    const title = dkan.generateRandomString()
 
-      cy.visit(baseurl + "/node/add/data")
+    before(() => {
+      cy.drupalLogin('testeditor', 'testeditor')
+      cy.visit('/node/add/data')
       cy.wait(2000)
       cy.get('#edit-field-json-metadata-0-value-title').type(title, { force:true } )
       cy.get('#edit-field-json-metadata-0-value-description').type('DKANTEST distribution description.', { force:true } )
@@ -45,7 +41,13 @@ context('Admin dataset file upload', () => {
         .click({ force:true })
       cy.get('.messages--status')
         .should('contain','has been created')
+    })
 
+    beforeEach(() => {
+      cy.drupalLogin('testeditor', 'testeditor')
+    })
+
+    it('can fill up the form with distribution and submit', () => {
       // run cron to import new dataset
       cy.visit('/admin/config/system/cron')
       cy.get('#edit-run')
@@ -58,45 +60,7 @@ context('Admin dataset file upload', () => {
     })
 
     it('uploaded dataset files show remote link on edit', () => {
-      const fileUrl = 'https://dkan-default-content-files.s3.amazonaws.com/phpunit/district_centerpoints_small.csv'
-      const title = dkan.generateRandomString()
-
-      cy.visit(baseurl + "/node/add/data")
-      cy.wait(2000)
-      cy.get('#edit-field-json-metadata-0-value-title').type(title, { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-description').type('DKANTEST distribution description.', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-accesslevel').select('public', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-modified-date').type('2021-02-02', { force:true } )
-      // Fill select2 field for publisher.
-      cy.get('#edit-field-json-metadata-0-value-publisher-publisher-name + .select2')
-        .find('.select2-selection')
-        .click({ force:true })
-      cy.get('input[aria-controls="select2-edit-field-json-metadata-0-value-publisher-publisher-name-results"]').type('DKANTEST Publisher{enter}')
-      // End filling up publisher.
-      cy.get('#edit-field-json-metadata-0-value-contactpoint-contactpoint-fn').type('DKANTEST Contact Name', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-contactpoint-contactpoint-hasemail').type('dkantest@test.com', { force:true } )
-      // Fill select2 field for keyword.
-      cy.get('#edit-field-json-metadata-0-value-keyword-keyword-0 + .select2')
-        .find('.select2-selection')
-        .click({ force: true })
-      cy.get('input[aria-controls="select2-edit-field-json-metadata-0-value-keyword-keyword-0-results"]').type('open data{enter}')
-      // End filling up keyword.
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-title')
-        .type('distribution title test', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-description')
-        .type('distribution description test', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-format-select')
-        .select('csv', { force:true })
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-file-url-type-remote')
-        .click({ force:true })
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-file-url-remote')
-        .type(fileUrl, { force:true })
-      cy.get('#edit-submit')
-        .click({ force:true })
-      cy.get('.messages--status')
-        .should('contain','has been created')
-
-      cy.visit(baseurl + '/admin/dkan/datasets')
+      cy.visit('/admin/dkan/datasets')
       cy.get('#edit-title').type(title)
       cy.get('#edit-submit-dkan-dataset-content').click()
       cy.get('.views-field-nothing > a').click()
@@ -107,13 +71,18 @@ context('Admin dataset file upload', () => {
   })
 
   context('Create dataset with file upload', () => {
-    it('can create and import dataset with uploaded file', () => {
-      const selectorDist = '#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-upload'
-      const fileName = 'example.csv'
-      const fileType = 'csv'
-      const title = dkan.generateRandomString()
+    const fileName = 'example.csv'
+    const fileType = 'csv'
+    const title = dkan.generateRandomString()
+    // generate a separate upload file name to prevent name collisions across
+    // tests
+    const uploadedFileName = dkan.generateCSVFileName()
 
-      cy.visit(baseurl + '/node/add/data')
+    before(() => {
+      const selectorDist = '#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-upload'
+
+      cy.drupalLogin('testeditor', 'testeditor')
+      cy.visit('/node/add/data')
       cy.wait(2000)
       cy.get('#edit-field-json-metadata-0-value-title').type(title, { force:true } )
       cy.get('#edit-field-json-metadata-0-value-description').type('DKANTEST distribution description.', { force:true } )
@@ -142,9 +111,6 @@ context('Admin dataset file upload', () => {
       cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-file-url-type-upload')
         .click({ force:true })
 
-      // generate a separate upload file name to prevent name collisions across
-      // tests
-      const uploadedFileName = dkan.generateCSVFileName()
       cy.get(selectorDist).uploadFile(fileName, fileType, uploadedFileName)
       //wait for the file to be fully loaded
       cy.get('.file--mime-text-csv', {timeout: 120000})
@@ -153,7 +119,13 @@ context('Admin dataset file upload', () => {
         .click({ force:true })
       cy.get('.messages--status')
         .should('contain','has been created')
+    })
 
+    beforeEach(() => {
+      cy.drupalLogin('testeditor', 'testeditor')
+    })
+
+    it('can create and import dataset with uploaded file', () => {
       // run cron to import new dataset
       cy.visit('/admin/config/system/cron')
       cy.get('#edit-run')
@@ -166,54 +138,8 @@ context('Admin dataset file upload', () => {
     })
 
     it('uploaded dataset files show local link on edit', () => {
-      const selectorDist = '#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-upload'
-      const fileName = 'example.csv'
-      const fileType = 'csv'
-      const title = dkan.generateRandomString()
-
-      cy.visit(baseurl + '/node/add/data')
-      cy.wait(2000)
-      cy.get('#edit-field-json-metadata-0-value-title').type(title, { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-description').type('DKANTEST distribution description.', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-accesslevel').select('public', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-modified-date').type('2021-02-02', { force:true } )
-      // Fill select2 field for publisher.
-      cy.get('#edit-field-json-metadata-0-value-publisher-publisher-name + .select2')
-        .find('.select2-selection')
-        .click({ force:true })
-      cy.get('input[aria-controls="select2-edit-field-json-metadata-0-value-publisher-publisher-name-results"]').type('DKANTEST Publisher{enter}')
-      // End filling up publisher.
-      cy.get('#edit-field-json-metadata-0-value-contactpoint-contactpoint-fn').type('DKANTEST Contact Name')
-      cy.get('#edit-field-json-metadata-0-value-contactpoint-contactpoint-hasemail').type('dkantest@test.com')
-      // Fill select2 field for keyword.
-      cy.get('#edit-field-json-metadata-0-value-keyword-keyword-0 + .select2')
-        .find('.select2-selection')
-        .click({ force: true })
-      cy.get('input[aria-controls="select2-edit-field-json-metadata-0-value-keyword-keyword-0-results"]').type('open data{enter}')
-      // End filling up keyword.
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-title')
-        .type('distribution title test', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-description')
-        .type('distribution description test', { force:true } )
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-format-select')
-        .select('csv', { force:true })
-
-      cy.get('#edit-field-json-metadata-0-value-distribution-distribution-0-distribution-downloadurl-file-url-type-upload')
-        .click({ force:true })
-
-      // generate a separate upload file name to prevent name collisions across
-      // tests
-      const uploadedFileName = dkan.generateCSVFileName()
-      console.log(uploadedFileName)
-      cy.get(selectorDist).uploadFile(fileName, fileType, uploadedFileName)
-      // wait for the file to be fully loaded
-      cy.get('.file--mime-text-csv', {timeout: 120000})
-        .should('be.visible')
-      cy.get('#edit-submit').click()
-      cy.get('.messages--status')
-        .should('contain','has been created')
       // validate URL of uploaded CSV file
-      cy.visit(baseurl + '/admin/dkan/datasets')
+      cy.visit('/admin/dkan/datasets')
       cy.get('#edit-title').type(title)
       cy.get('#edit-submit-dkan-dataset-content').click()
       cy.get('.views-field-nothing > a').click()
