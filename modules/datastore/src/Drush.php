f0f33f192bab8d4d1a4ca3febd47aa375dd71eca
Clayton Liddell <clayton.liddell@civicactions.com>
2022-03-18 11:12:56 -0500

    Restore commands and only remove deprecated aliases

--- modules/datastore/src/Drush.php
+++ modules/datastore/src/Drush.php
@@ -51,6 +51,78 @@ class Drush extends DrushCommands {
   }
 
   /**
+   * Import a datastore.
+   *
+   * @param string $uuid
+   *   The uuid of a resource.
+   * @param bool $deferred
+   *   Whether or not the process should be deferred to a queue.
+   *
+   * @todo pass configurable options for csv delimiter, quite, and escape characters.
+   * @command dkan:datastore:import
+   */
+  public function import($uuid, $deferred = FALSE) {
+
+    try {
+      $this->datastoreService->import($uuid, $deferred);
+    }
+    catch (\Exception $e) {
+      $this->logger->error("We were not able to load the entity with uuid {$uuid}");
+      $this->logger->debug($e->getMessage());
+    }
+  }
+
+  /**
+   * List information about all datastores.
+   *
+   * @field-labels
+   *   uuid: Resource UUID
+   *   fileName: File Name
+   *   fileFetcherStatus: FileFetcher
+   *   fileFetcherBytes: Processed
+   *   importerStatus: Importer
+   *   importerBytes: Processed
+   *
+   * @options format The format of the data.
+   * @options status Show imports of the given status.
+   * @options uuid-only Only the list of uuids.
+   *
+   * @command dkan:datastore:list
+   */
+  public function list($options = [
+    'format' => 'table',
+    'status' => NULL,
+    'uuid-only' => FALSE,
+  ]) {
+    $status = $options['status'];
+    $uuid_only = $options['uuid-only'];
+
+    $list = $this->datastoreService->list();
+    $rows = [];
+    foreach ($list as $uuid => $item) {
+      $rows[] = $this->createRow($uuid, $item);
+    }
+
+    if (!empty($status)) {
+      $rows = array_filter($rows, function ($row) use ($status) {
+        if ($row['fileFetcherStatus'] == $status || $row['importerStatus'] == $status) {
+          return TRUE;
+        }
+        return FALSE;
+      });
+    }
+
+    if ($uuid_only) {
+      foreach ($rows as $index => $row) {
+        $rows[$index] = $row['uuid'];
+      }
+      return new UnstructuredListData($rows);
+    }
+
+    return new RowsOfFields($rows);
+  }
+
+  /**
    * Private.
    */
   private function createRow($uuid, $item) {
@@ -62,6 +134,44 @@ class Drush extends DrushCommands {
       'importerStatus' => $item->importerStatus,
       'importerBytes' => \format_size($item->importerBytes) . " ($item->importerPercentDone%)",
     ];
+  }
+
+  /**
+   * Drop a datastore.
+   *
+   * @param string $uuid
+   *   The uuid of a dataset resource.
+   *
+   * @command dkan:datastore:drop
+   */
+  public function drop($uuid) {
+    try {
+      // Retrieve the UUID for the dataset's resource before dropping the
+      // dataset's resource mapper table entry.
+      $resource = $this->resourceLocalizer->get($uuid);
+      if (!isset($resource)) {
+        throw new \UnexpectedValueException("Resource not found with uuid {$uuid}");
+      }
+      $resource_uuid = $resource->getUniqueIdentifier();
+      // Drop the datastore table and corresponding resource mapper table entry.
+      $this->datastoreService->drop($uuid);
+      $this->logger->notice("Successfully dropped the datastore for {$uuid}");
+    }
+    catch (\Exception $e) {
+      $this->logger->error("Unable to find an entity with uuid {$uuid}");
+      $this->logger->debug($e->getMessage());
+      return;
+    }
+    catch (\TypeError $e) {
+      // This will catch all TypeErrors with all arguments. Since we only
+      // pass the UUID to the DataStore::Service::drop method we can safely
+      // assume the issue is with the uuid.
+      $this->logger->error("Unexpected entity uuid.");
+      $this->logger->debug($e->getMessage());
+      return;
+    }
+    // Drop any remaining jobstore entries for this resource.
+    $this->jobstorePrune($resource_uuid);
   }
 
   /**
