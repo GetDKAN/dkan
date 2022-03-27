7cdedd54bec92828c24b9fd2ed756938edcf7ab8
Dan Feder <dan.feder@civicactions.com>
2022-03-26 22:45:09 -0400

    Some documentation cleanup

--- modules/common/src/Plugin/DkanApiDocs/CommonApiDocs.php
+++ modules/common/src/Plugin/DkanApiDocs/CommonApiDocs.php
@@ -9,7 +9,7 @@ use Drupal\common\Plugin\DkanApiDocsBase;
  *
  * @DkanApiDocs(
  *  id = "common_dkan_api_docs",
- *  description = "Base API docs plugin."
+ *  description = @Translation("Base API docs plugin.")
  * )
  */
 class CommonApiDocs extends DkanApiDocsBase {
