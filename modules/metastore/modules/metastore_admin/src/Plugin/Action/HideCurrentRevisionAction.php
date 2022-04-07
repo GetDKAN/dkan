b93df4fe8210fe151f8ca03fb46afbc19aef3872
Janette Day <janette.day@civicactions.com>
2022-04-07 14:19:01 -0500

    Move hide function to plugin

--- modules/metastore/modules/metastore_admin/src/Plugin/Action/HideCurrentRevisionAction.php
+++ modules/metastore/modules/metastore_admin/src/Plugin/Action/HideCurrentRevisionAction.php
@@ -3,10 +3,13 @@
 namespace Drupal\metastore_admin\Plugin\Action;
 
 use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\metastore_admin\MetastoreAdminModeration;
-use Drupal\node\NodeInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Messenger\Messenger;
+use Drupal\Core\Messenger\MessengerInterface;
+use Psr\Container\ContainerInterface;
 
 /**
  * An example action covering most of the possible options.
@@ -28,29 +31,90 @@ class HideCurrentRevisionAction extends ActionBase {
   use StringTranslationTrait;
 
   /**
+   * Constructor.
+   *
+   * @param array $config
+   *   Details for reference definition. Possible keys:
+   *   - schemaId: For some reference definitions, a schemaId must be specified.
+   * @param string $pluginId
+   *   The plugin_id for the plugin instance.
+   * @param mixed $pluginDefinition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
+   *   Logger factory service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   Messenger.
+   * @param AccountInterface $currentUser
+   *   Current user.
+   */
+  public function __construct(
+    array $config,
+    $pluginId,
+    $pluginDefinition,
+    LoggerChannelFactoryInterface $loggerFactory,
+    MessengerInterface $messenger,
+    AccountInterface $currentUser
+  ) {
+    $this->messenger = $messenger;
+    $this->currentUser = $currentUser;
+    parent::__construct($config, $pluginId, $pluginDefinition, $loggerFactory);
+  }
+
+  /**
+   * Container injection.
+   *
+   * @param \Drupal\common\Plugin\ContainerInterface $container
+   *   The service container.
+   * @param array $config
+   *   A configuration array containing information about the plugin instance.
+   * @param string $pluginId
+   *   The plugin_id for the plugin instance.
+   * @param mixed $pluginDefinition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $config,
+    $pluginId,
+    $pluginDefinition
+  ) {
+    $loggerFactory = $container->get('logger.factory');
+    $messenger = $container->get('messenger');
+    $currentUser = $container->get('currenUser');
+    return new static($config, $pluginId, $pluginDefinition, $loggerFactory, $messenger, $currentUser);
+  }
+
+
+  /**
+   * Entity.
+   *
+   * @var entity
+   */
+  private $entity = NULL;
+
+  /**
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
 
-    $user = \Drupal::currentUser();
+    if ($this->currentUser->hasPermission('moderated content bulk archive')) {
+      $this->logger->notice("Executing hide latest revision of " . $entity->label());
 
-    if ($user->hasPermission('moderated content bulk archive')) {
-      \Drupal::logger('moderated_content_bulk_publish')->notice("Executing hide latest revision of " . $entity->label());
-
-      $adminModeration = new MetastoreAdminModeration($entity, NodeInterface::PUBLISHED);
-      $entity = $adminModeration->hide();
+      $this->hide($entity);
 
       // Check if published.
       if (!$entity->isPublished()) {
         $msg = "Something went wrong, the entity must be published by this point.  Review your content moderation configuration make sure you have the hidden state available and try again.";
-        \Drupal::Messenger()->addError(utf8_encode($msg));
-        \Drupal::logger('moderated_content_bulk_publish')->warning($msg);
+        $this->messenger->addError(utf8_encode($msg));
+        $this->logger->warning($msg);
         return $msg;
       }
       return sprintf('Example action (configuration: %s)', print_r($this->configuration, TRUE));
     }
     else {
-      \Drupal::messenger()->addWarning($this->t("You don't have access to execute this operation!"));
+      $this->messenger->addWarning($this->t("You don't have access to execute this operation!"));
       return;
     }
   }
@@ -66,6 +130,32 @@ class HideCurrentRevisionAction extends ActionBase {
     }
 
     return TRUE;
+  }
+
+  /**
+   * Hide (exclude from search) current revision.
+   */
+  public function hide($entity) {
+
+    $entity->set('moderation_state', 'hidden');
+    if ($entity instanceof RevisionLogInterface) {
+      //$entity->setRevisionCreationTime(\Drupal::time()->getRequestTime());
+      $msg = 'Bulk operation create hidden revision';
+      $entity->setRevisionLogMessage($msg);
+      $current_uid = $this->currentUser->id();
+      $entity->setRevisionUserId($current_uid);
+    }
+
+    if ($this->currentUser->hasPermission('use dkan_publishing transition hidden')) {
+      $entity->save();
+    }
+    else {
+      $this->logger->notice(
+        utf8_encode("Bulk hide not permitted, check permissions")
+      );
+    }
+
+    return $entity;
   }
 
 }
