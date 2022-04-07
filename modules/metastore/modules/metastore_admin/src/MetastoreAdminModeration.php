d678f4be54886f33afbe9367be79530374a9137c
Janette Day <janette.day@civicactions.com>
2022-04-06 23:40:13 -0500

    remove language checks

--- modules/metastore/modules/metastore_admin/src/MetastoreAdminModeration.php
+++ modules/metastore/modules/metastore_admin/src/MetastoreAdminModeration.php
@@ -3,14 +3,11 @@
 namespace Drupal\metastore_admin;
 
 use Drupal\Core\Entity\RevisionLogInterface;
-use Drupal\moderated_content_bulk_publish\AdminHelper;
 
 /**
  * A Helper Class to assist with the "hide current revision" bulk action.
  *
  * Based on the moderated_content_bulk_publish module.
- *
- * @codeCoverageIgnore
  */
 class MetastoreAdminModeration {
 
@@ -57,43 +54,27 @@ class MetastoreAdminModeration {
    * Hide (exclude from search) current revision.
    */
   public function hide() {
-    // @codeCoverageIgnoreStart
     $user = \Drupal::currentUser();
-    $currentLang = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $allLanguages = AdminHelper::getAllEnabledLanguages();
-    foreach ($allLanguages as $langcode => $languageName) {
-      if ($this->entity->hasTranslation($langcode)) {
-        \Drupal::logger('moderated_content_bulk_publish')->notice(
-          utf8_encode("Hide $langcode for " . $this->id . " in moderated_content_bulk_publish")
-        );
-        $this->entity = $this->entity->getTranslation($langcode);
-        $this->entity->set('moderation_state', 'hidden');
-        if ($this->entity instanceof RevisionLogInterface) {
-          $this->entity->setRevisionCreationTime(\Drupal::time()->getRequestTime());
-          $msg = 'Bulk operation create hidden revision';
-          $this->entity->setRevisionLogMessage($msg);
-          $current_uid = \Drupal::currentUser()->id();
-          $this->entity->setRevisionUserId($current_uid);
-        }
 
-        $this->entity->setRevisionTranslationAffected(TRUE);
-        if ($user->hasPermission('use dkan_publishing transition hidden')) {
-          if ($langcode == $currentLang) {
-            $this->entity->save();
-          }
-          else {
-            drupal_register_shutdown_function('Drupal\moderated_content_bulk_publish\AdminHelper::bulkPublishShutdown', $this->entity, $langcode, 'hidden');
-          }
-        }
-        else {
-          \Drupal::logger('moderated_content_bulk_publish')->notice(
-            utf8_encode("Bulk hide not permitted, check permissions")
-          );
-        }
-      }
+    $this->entity->set('moderation_state', 'hidden');
+    if ($this->entity instanceof RevisionLogInterface) {
+      $this->entity->setRevisionCreationTime(\Drupal::time()->getRequestTime());
+      $msg = 'Bulk operation create hidden revision';
+      $this->entity->setRevisionLogMessage($msg);
+      $current_uid = \Drupal::currentUser()->id();
+      $this->entity->setRevisionUserId($current_uid);
     }
+
+    if ($user->hasPermission('use dkan_publishing transition hidden')) {
+      $this->entity->save();
+    }
+    else {
+      \Drupal::logger('moderated_content_bulk_publish')->notice(
+        utf8_encode("Bulk hide not permitted, check permissions")
+      );
+    }
+
     return $this->entity;
-    // @codeCoverageIgnoreEnd
   }
 
 }
