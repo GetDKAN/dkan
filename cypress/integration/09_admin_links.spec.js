4210a93a7f3d76b2eecd6f377b1c6b9a6c5c2c16
Janette Day <janette.day@gmail.com>
2022-05-25 17:06:36 -0500

    Add moderation state to admin dataset view. Add hide action to VBO. (#3766)

--- cypress/integration/09_admin_links.spec.js
+++ cypress/integration/09_admin_links.spec.js
@@ -3,10 +3,10 @@ context('Administration pages', () => {
   beforeEach(() => {
     const user_credentials = Cypress.env('TEST_USER_CREDENTIALS')
     cy.drupalLogin(user_credentials.user, user_credentials.pass)
+    cy.visit(baseurl + "/admin/dkan")
   })
 
   it('I should see a link for the dataset properties configuration', () => {
-    cy.visit(baseurl + "/admin")
     cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
         cy.wrap($el).invoke('show')
         cy.wrap($el).contains('Metastore referencer')
@@ -16,7 +16,6 @@ context('Administration pages', () => {
   })
 
   it('I should see a link for the datastore configuration', () => {
-    cy.visit(baseurl + "/admin")
     cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
         cy.wrap($el).invoke('show')
         cy.wrap($el).contains('Datastore settings')
@@ -24,4 +23,30 @@ context('Administration pages', () => {
     cy.visit(baseurl + "/admin/dkan/datastore")
     cy.get('label[for="edit-rows-limit"]').should('have.text', 'Rows limit')
   })
+
+  it('I should see a link for the datastore status', () => {
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
+        cy.wrap($el).invoke('show')
+        cy.wrap($el).contains('Datastore Import Status')
+    })
+    cy.visit(baseurl + "/admin/dkan/datastore/status")
+    cy.contains('h1', 'Datastore Import Status');
+  })
+
+  it('I should see a link for the harvest status', () => {
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
+        cy.wrap($el).invoke('show')
+        cy.wrap($el).contains('Harvests')
+    })
+    cy.visit(baseurl + "/admin/dkan/harvest")
+    cy.contains('h1', 'Harvests');
+  })
+
+  it('There is a link in the admin menu to the datasets admin screen.', () => {
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=> {
+        cy.wrap($el).invoke('show')
+        cy.wrap($el).contains('Datasets')
+    })
+  })
+
 })
