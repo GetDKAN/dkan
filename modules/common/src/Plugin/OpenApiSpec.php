0eb895461b7df244ed683f913ad3509fbdeeef63
Janette Day <janette.day@civicactions.com>
2022-11-23 15:52:10 -0600

    Restore OpenApiSpec plugin

--- modules/common/src/Plugin/OpenApiSpec.php
+++ modules/common/src/Plugin/OpenApiSpec.php
@@ -16,19 +16,8 @@ class OpenApiSpec extends RootedJsonData {
    *   JSON query string from API payload.
    */
   public function __construct(string $json) {
-    // @todo Diagnose why validating against open-api-schema-3.0.json results,
-    //   at least in some scenarios, with error such as:
-    //
-    //   JSON Schema validation failed
-    //   keyword: format
-    //   pointer: paths//provider-data/api/1/metastore/schemas/dataset/items/post/requestBody/content/application/json/schema/properties/distribution/items/properties/downloadURL/anyOf/1/pattern
-    //   message: The attribute should match 'regex' format.
-//    $schema = file_get_contents(__DIR__ . "/../../docs/open-api-schema.3.0.json");
-//    parent::__construct($json, $schema);
-
-    // Until the above is addressed, keep validating against the null schema,
-    // as PDC as done for quite some time.
-    parent::__construct($json, '{}');
+    $schema = file_get_contents(__DIR__ . "/../../docs/open-api-schema.3.0.json");
+    parent::__construct($json, $schema);
   }
 
 }
