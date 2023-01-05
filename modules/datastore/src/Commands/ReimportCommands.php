4f209b8499344d0960a2e0febe6bc81859e4129b
Paul Mitchum <paul@mile23.com>
2023-01-05 11:10:11 -0800

    cs/complexity for codeclimate

--- modules/datastore/src/Commands/ReimportCommands.php
+++ modules/datastore/src/Commands/ReimportCommands.php
@@ -29,7 +29,7 @@ class ReimportCommands extends DrushCommands {
    * Constructor for DkanDatastoreCommands.
    */
   public function __construct(
-    Datastore   $datastore_service,
+    Datastore $datastore_service,
     DatasetInfo $dataset_info
   ) {
     $this->datastoreService = $datastore_service;
@@ -40,7 +40,7 @@ class ReimportCommands extends DrushCommands {
   /**
    * Re-import distribution based on dataset UUID.
    *
-   * @param $uuid
+   * @param string $uuid
    *   The UUID of the dataset.
    *
    * @usage dkan:datastore-reimport C66A2DF6-7BB2-40D2-8551-6E1104BBCC57
@@ -54,21 +54,14 @@ class ReimportCommands extends DrushCommands {
    *   dktl drush dkan:datastore-reimport $i
    * done
    */
-  public function datastoreReimport($uuid) {
+  public function datastoreReimport(string $uuid) {
+    // Find distributions for the given UUID.
     if ($info = $this->datasetInfo->gather($uuid) ?? FALSE) {
       if ($distributions = $info['latest_revision']['distributions'] ?? FALSE) {
+        // Reimport whatever distributions we found.
         $this->logger()
           ->success(count($info) . ' distribution(s) found for ' . $uuid);
-        foreach ($distributions as $distribution) {
-          if ($resource_id = $distribution['resource_id'] ?? FALSE) {
-            $this->logger()
-              ->success('Reimporting distribution: ' . $resource_id);
-            $this->datastoreService->drop($resource_id);
-            $result = $this->datastoreService->import($resource_id);
-            $status = $result['Import'] ? $result['Import']->getStatus() : 'failed, resource not found';
-            $this->logger->notice("Ran import for $resource_id; status: $status");
-          }
-        }
+        $this->reimportDistributions($distributions);
       }
       else {
         $this->logger()->error('Unable to find distributions for ' . $uuid);
@@ -76,6 +69,27 @@ class ReimportCommands extends DrushCommands {
     }
     else {
       $this->logger()->error('Unable to find dataset info for ' . $uuid);
+    }
+  }
+
+  /**
+   * Perform the reimport of given distributions.
+   *
+   * Each distribution will be dropped from the database and then re-added.
+   *
+   * @param array $distributions
+   *   The distributions to reimport.
+   */
+  protected function reimportDistributions(array $distributions) {
+    foreach ($distributions as $distribution) {
+      if ($resource_id = $distribution['resource_id'] ?? FALSE) {
+        $this->logger()
+          ->success('Reimporting distribution: ' . $resource_id);
+        $this->datastoreService->drop($resource_id);
+        $result = $this->datastoreService->import($resource_id);
+        $status = $result['Import'] ? $result['Import']->getStatus() : 'failed, resource not found';
+        $this->logger->notice("Ran import for $resource_id; status: $status");
+      }
     }
   }
 
