41d38ce53fcc373c526a513fa993c7c1551b1baa
jastraat <jess.straatmann@gmail.com>
2023-02-07 12:26:18 -0600

    Don’t pass a null value to json_decode()

--- modules/common/src/DataResource.php
+++ modules/common/src/DataResource.php
@@ -316,6 +316,9 @@ class DataResource implements \JsonSerializable {
     $storage = $factory->getInstance('distribution');
 
     $distroJson = $storage->retrieve($identifier);
+    if (is_null($distroJson)) {
+      $distroJson = '';
+    }
     return json_decode($distroJson);
   }
 
