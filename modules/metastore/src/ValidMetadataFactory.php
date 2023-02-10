83b13af4edc83deefc08a1b39b936a369acfa9ed
Paul Mitchum <paul@mile23.com>
2023-02-09 17:39:25 -0800

    ValidMetadataFactory is a villain.

--- modules/metastore/src/ValidMetadataFactory.php
+++ modules/metastore/src/ValidMetadataFactory.php
@@ -91,7 +91,10 @@ class ValidMetadataFactory implements ContainerInjectionInterface {
    *   Json string with the identifier.
    */
   private function addIdentifier(string $schema_id, string $json_string): string {
-    $json_data = json_decode($json_string);
+    // If $json_string is empty, json_decode() will return NULL. We coerce to
+    // an object, because it might already be an object, or it could be an
+    // array. NULL always coerces to an empty object.
+    $json_data = (object) json_decode($json_string);
     $uuid5 = new Uuid5();
     $json_data->identifier = $uuid5->generate($schema_id, $json_string);
     return json_encode($json_data);
