78206d2ab63354d8921b8092f0424211e4b98410
Paul Mitchum <paul@mile23.com>
2023-03-18 13:26:41 -0700

    rector config

---
+++ rector.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Call from the project directory like:
+ * ./vendor/bin/rector process --config=docroot/modules/contrib/dkan/rector.php
+ */
+
+use DrupalFinder\DrupalFinder;
+use DrupalRector\Set\Drupal8SetList;
+use DrupalRector\Set\Drupal9SetList;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Transform\ValueObject\ClassMethodReference;
+use Rector\Transform\Rector\ClassMethod\ReturnTypeWillChangeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+  $rectorConfig->sets([
+    LevelSetList::UP_TO_PHP_74,
+  ]);
+  $rectorConfig->skip([
+    JsonThrowOnErrorRector::class,
+    ClosureToArrowFunctionRector::class,
+  ]);
+  $rectorConfig->ruleWithConfiguration(
+    ReturnTypeWillChangeRector::class,
+    [new ClassMethodReference('JsonSerializable', 'jsonSerialize')]
+  );
+
+//  $rectorConfig->rule(\Rector\Php71\Rector\FuncCall\CountOnNullRector::class);
+
+  $parameters = $rectorConfig->parameters();
+
+  $drupalFinder = new DrupalFinder();
+  $drupalFinder->locateRoot(__DIR__);
+  $drupalRoot = $drupalFinder->getDrupalRoot();
+  $rectorConfig->autoloadPaths([
+    $drupalRoot . '/core',
+    $drupalRoot . '/modules',
+    $drupalRoot . '/profiles',
+    $drupalRoot . '/themes',
+  ]);
+
+  $rectorConfig->fileExtensions([
+    'php',
+    'module',
+    'theme',
+    'install',
+    'profile',
+    'inc',
+    'engine',
+  ]);
+  $rectorConfig->importNames(TRUE, FALSE);
+  $rectorConfig->importShortClasses(FALSE);
+  $parameters->set('drupal_rector_notices_as_comments', TRUE);
+};
