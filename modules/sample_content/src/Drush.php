bd7d75e5d0539d21c71761b2b57926bbe3b96031
jastraat <jess.straatmann@gmail.com>
2023-03-31 11:23:19 -0500

    Codeclimate fixes

--- modules/sample_content/src/Drush.php
+++ modules/sample_content/src/Drush.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\sample_content;
 
+use Drupal\Core\Extension\ModuleExtensionList;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Drupal\harvest\Commands\Helper;
@@ -11,6 +12,18 @@ use Drupal\harvest\Commands\Helper;
  */
 class Drush extends DrushCommands {
   use Helper;
+
+  /**
+   * @var Drupal\Core\Extension\ModuleExtensionList
+   */
+  protected $extensionListModule;
+
+  /**
+   * @param ModuleExtensionList $extension_list_module
+   */
+  public function __construct(ModuleExtensionList $extension_list_module) {
+    $this->extensionListModule = $extension_list_module;
+  }
 
   /**
    * Create sample content.
@@ -44,7 +57,7 @@ class Drush extends DrushCommands {
    * Protected.
    */
   protected function getHarvestPlan() {
-    $module_path = DRUPAL_ROOT . "/" . \Drupal::service('extension.list.module')->getPath('sample_content');
+    $module_path = DRUPAL_ROOT . "/" . $this->extensionListModule->getPath('sample_content');
 
     $plan_path = $module_path . "/harvest_plan.json";
     $json = file_get_contents($plan_path);
@@ -59,7 +72,7 @@ class Drush extends DrushCommands {
    * Private.
    */
   private function createJson() {
-    $sample_content_path = \Drupal::service('extension.list.module')->getPath('sample_content');
+    $sample_content_path = $this->extensionListModule->getPath('sample_content');
     $sample_content_template = DRUPAL_ROOT . "/" . $sample_content_path . "/sample_content.template.json";
     $content = file_get_contents($sample_content_template);
     $new = $this->detokenize($content);
@@ -70,7 +83,7 @@ class Drush extends DrushCommands {
    * Private.
    */
   private function detokenize($content) {
-    $absolute_module_path = DRUPAL_ROOT . "/" . \Drupal::service('extension.list.module')->getPath('sample_content') . "/files";
+    $absolute_module_path = DRUPAL_ROOT . "/" . $this->extensionListModule->getPath('sample_content') . "/files";
     return str_replace("<!*path*!>", $absolute_module_path, $content);
   }
 
