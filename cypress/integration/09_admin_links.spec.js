0e87ddd0efe9513b5738a259edba7e0a42ad46bc
Janette Day <janette.day@civicactions.com>
2023-04-28 15:18:12 -0500

    update admin menu tests

--- cypress/integration/09_admin_links.spec.js
+++ cypress/integration/09_admin_links.spec.js
@@ -4,48 +4,50 @@ context('Administration pages', () => {
     const user_credentials = Cypress.env('TEST_USER_CREDENTIALS')
     cy.drupalLogin(user_credentials.user, user_credentials.pass)
     cy.visit(baseurl + "/admin/dkan")
+    cy.viewport('macbook-15')
+    cy.wait(200)
   })
 
   it('I should see a link for the dataset properties configuration', () => {
-    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
-        cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Metastore referencer')
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-handle').then($el=>{
+        cy.wrap($el).click()
+        cy.get('.toolbar-menu').contains('Metastore referencer')
     })
     cy.visit(baseurl + "/admin/dkan/properties")
     cy.get('.option').should('contain.text', 'Distribution (distribution)')
   })
 
   it('I should see a link for the datastore configuration', () => {
-    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
-        cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Datastore settings')
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-handle').then($el=>{
+        cy.wrap($el).click()
+        cy.get('.toolbar-menu').contains('Datastore settings')
     })
     cy.visit(baseurl + "/admin/dkan/datastore")
     cy.get('label[for="edit-rows-limit"]').should('have.text', 'Rows limit')
   })
 
   it('I should see a link for the datastore status', () => {
-    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
-        cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Datastore Import Status')
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-handle').then($el=>{
+        cy.wrap($el).click()
+        cy.get('.toolbar-menu').contains('Datastore Import Status')
     })
     cy.visit(baseurl + "/admin/dkan/datastore/status")
     cy.contains('h1', 'Datastore Import Status');
   })
 
   it('I should see a link for the harvest status', () => {
-    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=>{
-        cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Harvests')
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-handle').then($el=>{
+        cy.wrap($el).click()
+        cy.get('.toolbar-menu').contains('Harvests')
     })
     cy.visit(baseurl + "/admin/dkan/harvest")
     cy.contains('h1', 'Harvests');
   })
 
   it('There is a link in the admin menu to the datasets admin screen.', () => {
-    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-menu').then($el=> {
-        cy.wrap($el).invoke('show')
-        cy.wrap($el).contains('Datasets')
+    cy.get('.toolbar-icon-system-admin-dkan').contains('DKAN').next('.toolbar-handle').then($el=> {
+        cy.wrap($el).click()
+        cy.get('.toolbar-menu').contains('Datasets')
     })
   })
 
