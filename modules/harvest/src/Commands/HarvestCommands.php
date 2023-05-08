5696f6f22f79fc086dbfed3b279b6f256102f6ce
Stefan Korn <drupal@stefan-korn.de>
2023-05-08 15:44:58 +0200

    Issue #3839: Disentangle harvest register and revert - confirm command fix

--- modules/harvest/src/Commands/HarvestCommands.php
+++ modules/harvest/src/Commands/HarvestCommands.php
@@ -127,7 +127,7 @@ class HarvestCommands extends DrushCommands {
    */
   public function deregister($id) {
     $this->logger->warning('If you deregister a harvest with published datasets, you will not be able to bulk revert the datasets connected to this harvest.');
-    if ($this->io()->confirm('Deregister harvest %id', ['%id' => $id])) {
+    if ($this->io()->confirm("Deregister harvest {$id}")) {
       try {
         if ($this->harvestService->deregisterHarvest($id)) {
           $message = "Successfully deregistered the {$id} harvest.";
