f3a4510f301cd9383cb4efd2389af1c93a7679b3
jastraat <jess.straatmann@gmail.com>
2023-05-11 17:00:36 -0500

    Add new config option for resource_perspective_display

--- modules/datastore/src/Form/ResourceSettingsForm.php
+++ modules/datastore/src/Form/ResourceSettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\datastore\Form;
 
+use Drupal\common\DataResource;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -52,6 +53,17 @@ class ResourceSettingsForm extends ConfigFormBase {
       '#default_value' => $this->config('datastore.settings')->get('delete_local_resource'),
       '#description' => $this->t('Delete local copy of remote files after the datastore import is complete'),
     ];
+    $form['resource_perspective_display'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Resource download url display'),
+      '#description' => $this->t('Choose to display either the source or local path to a resource file in the metadata.'),
+      '#options' => [
+        DataResource::DEFAULT_SOURCE_PERSPECTIVE => $this->t('Source'),
+        'local_url' => $this->t('Local URL'),
+      ],
+      '#default_value' => $this->config('metastore.settings')->get('resource_perspective_display')
+        ?: DataResource::DEFAULT_SOURCE_PERSPECTIVE,
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -63,6 +75,9 @@ class ResourceSettingsForm extends ConfigFormBase {
       ->set('purge_table', $form_state->getValue('purge_table'))
       ->set('purge_file', $form_state->getValue('purge_file'))
       ->set('delete_local_resource', $form_state->getValue('delete_local_resource'))
+      ->save();
+    $this->config('metastore.settings')
+      ->set('resource_perspective_display', $form_state->getValue('resource_perspective_display'))
       ->save();
     parent::submitForm($form, $form_state);
   }
