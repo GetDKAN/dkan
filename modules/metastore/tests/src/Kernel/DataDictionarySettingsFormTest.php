76fdccebb0ed7b00f5118145eaf49ba973a1cda0
Kaise Lafrai <lafrai.k@gmail.com>
2023-06-22 17:57:44 -0400

    WCMS-10463: update kernel test

--- modules/metastore/tests/src/Kernel/DataDictionarySettingsFormTest.php
+++ modules/metastore/tests/src/Kernel/DataDictionarySettingsFormTest.php
@@ -5,8 +5,6 @@ namespace Drupal\Tests\metastore\Kernel;
 use Drupal\Tests\common\Kernel\ConfigFormTestBase;
 use Drupal\metastore\Form\DataDictionarySettingsForm;
 use Drupal\metastore\DataDictionary\DataDictionaryDiscoveryInterface;
-use Drupal\Core\Form\FormState;
-use Drupal\Component\Render\FormattableMarkup;
 
 /**
  * Data Dictionary Settings Form class test.
@@ -20,7 +18,7 @@ class DataDictionarySettingsFormTest extends ConfigFormTestBase {
    *
    * @var string[]
    */
-  public static $modules = ['metastore', 'common', 'node', 'user'];
+  public static $modules = ['metastore', 'common'];
 
   /**
    * {@inheritdoc}
@@ -35,7 +33,7 @@ class DataDictionarySettingsFormTest extends ConfigFormTestBase {
             '#config_key' => 'data_dictionary_mode',
           ],
           'sitewide_dictionary_id' => [
-            '#value' => 'test',
+            '#value' => $this->randomString(),
             '#config_name' => DataDictionarySettingsForm::SETTINGS,
             '#config_key' => 'data_dictionary_sitewide',
           ],
@@ -51,36 +49,6 @@ class DataDictionarySettingsFormTest extends ConfigFormTestBase {
         ],
       ],
     ];
-  }
-
-    /**
-   * Test submitting config form.
-   *
-   * @param array $form_values
-   *   Form values to test.
-   *
-   * @dataProvider provideFormData
-   */
-  public function testConfigForm(array $form_values) {
-    // Programmatically submit the given values.
-    foreach ($form_values as $form_key => $data) {
-      $values[$form_key] = $data['#value'];
-    }
-    $form_state = (new FormState())->setValues($values);
-    \Drupal::formBuilder()->submitForm($this->form, $form_state);
-
-    // Check that the form returns an error when expected, and vice versa.
-    $errors = $form_state->getErrors();
-    $valid_form = empty($errors);
-    $args = [
-      '%values' => print_r($values, TRUE),
-      '%errors' => $valid_form ? t('None') : implode(' ', $errors),
-    ];
-    $this->assertTrue($valid_form, new FormattableMarkup('Input values: %values<br/>Validation handler errors: %errors', $args));
-
-    foreach ($form_values as $data) {
-      $this->assertEquals($data['#value'], $this->config($data['#config_name'])->get($data['#config_key']));
-    }
   }
 
   /**
