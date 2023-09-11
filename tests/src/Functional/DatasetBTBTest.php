00c3c34004692f74c10a06253c89cb20374177ed
Paul Mitchum <paul@mile23.com>
2023-09-11 12:10:34 -0700

    unneeded todo

--- tests/src/Functional/DatasetBTBTest.php
+++ tests/src/Functional/DatasetBTBTest.php
@@ -18,9 +18,6 @@ use RootedData\RootedJsonData;
  *
  * @group dkan
  * @group functional
- *
- * @todo Factor out some of the support methods to a trait or subclass to share
- *   with MetastoreApiPageCacheTest.
  */
 class DatasetBTBTest extends BrowserTestBase {
 
