cbe1a1ccf866e41fc9e5f0c5cdf0d54f2e3ebc0d
Paul Mitchum <paul@mile23.com>
2023-09-21 14:12:28 -0700

    localize test

--- modules/datastore/tests/src/Kernel/Plugin/QueueWorker/LocalizeQueueWorkerTest.php
+++ modules/datastore/tests/src/Kernel/Plugin/QueueWorker/LocalizeQueueWorkerTest.php
@@ -2,13 +2,11 @@
 
 namespace Drupal\Tests\datastore\Kernel\Plugin\QueueWorker;
 
+use Contracts\FactoryInterface;
 use Drupal\common\DataResource;
-use Drupal\common\Storage\ImportedItemInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Logger\LoggerChannelInterface;
-use Drupal\datastore\DatastoreService;
-use Drupal\datastore\Plugin\QueueWorker\ImportQueueWorker;
+use Drupal\Core\Queue\QueueFactory;
 use Drupal\datastore\Plugin\QueueWorker\LocalizeQueueWorker;
+use Drupal\datastore\Service\ResourceLocalizer;
 use Drupal\KernelTests\KernelTestBase;
 use Procrastinator\Result;
 
@@ -30,13 +28,28 @@ class LocalizeQueueWorkerTest extends KernelTestBase {
     'common',
     'datastore',
     'metastore',
+    'node',
   ];
 
-  public function testLocalizing() {
+  public function provideUseExisting() {
+    return [
+      'Use existing localized file' => [TRUE],
+      'Do not use existing localized file' => [FALSE],
+    ];
+  }
+
+  /**
+   * @dataProvider provideUseExisting
+   */
+  public function testLocalizing(bool $always_use_existing_local_perspective) {
+    $existing_local_file_contents = 'pre-existing,file';
     // Config for overwrite.
     $this->installConfig(['common']);
     $config = $this->config('common.settings');
-    $config->set('always_use_existing_local_perspective', FALSE);
+    $config->set(
+      'always_use_existing_local_perspective',
+      $always_use_existing_local_perspective
+    );
     $config->save();
 
     // Create a dataset.
@@ -47,13 +60,31 @@ class LocalizeQueueWorkerTest extends KernelTestBase {
     );
     /** @var \Drupal\metastore\ResourceMapper $mapper */
     $mapper = $this->container->get('dkan.metastore.resource_mapper');
+    // Make sure the resource isn't registered already.
+    $this->assertNull($mapper->get($source_resource->getIdentifier()));
     $mapper->register($source_resource);
+    $this->assertNotNull($mapper->get($source_resource->getIdentifier()));
 
     // Check the file system to be sure it's set the way it should be.
     /** @var \Drupal\datastore\Service\ResourceLocalizer $localizer */
     $localizer = $this->container->get('dkan.datastore.service.resource_localizer');
+    // Compute the public file URI for the resource.
     $public_uri = $localizer->localizeFilePath($source_resource);
-    // Assert file does not exist.
+
+    // Set up the file to either be pre-existing or not, based on arguments.
+    /** @var \Drupal\Core\File\FileSystem $fs */
+    $fs = $this->container->get('file_system');
+
+    if ($always_use_existing_local_perspective) {
+      file_put_contents($fs->realpath($public_uri), $existing_local_file_contents);
+      $this->assertFileExists($fs->realpath($public_uri));
+    }
+    else {
+      $this->assertFileDoesNotExist($fs->realpath($public_uri));
+      // Put the wrong contents into the file after the assertion, to ensure
+      // it is replaced.
+      file_put_contents($fs->realpath($public_uri), $existing_local_file_contents);
+    }
 
     // Make a localize queue worker.
     $queue_worker = LocalizeQueueWorker::create(
@@ -70,235 +101,51 @@ class LocalizeQueueWorkerTest extends KernelTestBase {
     ]);
 
     // Ensure the file was localized.
-  }
-
-  public function testErrorPath() {
-    // The result we'll mock to come from the datastore service.
-    $result = new Result();
-    $result->setStatus(Result::ERROR);
-    $result->setError('Oops');
-
-    // Mock the datastore service. All the services are real, we only want to
-    // mock import().
-    $datastore_service = $this->getMockBuilder(DatastoreService::class)
-      ->setConstructorArgs([
-        $this->container->get('dkan.datastore.service.resource_localizer'),
-        $this->container->get('dkan.datastore.service.factory.import'),
-        $this->container->get('queue'),
-        $this->container->get('dkan.common.job_store'),
-        $this->container->get('dkan.datastore.import_info_list'),
-        $this->container->get('dkan.datastore.service.resource_processor.dictionary_enforcer'),
-        $this->container->get('dkan.metastore.resource_mapper'),
-      ])
-      ->onlyMethods(['import'])
-      ->getMock();
-    $datastore_service->method('import')
-      ->willReturn([$result]);
-
-    // Add our mock to the container.
-    $this->container->set('dkan.datastore.service', $datastore_service);
-
-    // Mock the logger so we can tell when the error occurs.
-    $logger = $this->getMockForAbstractClass(LoggerChannelInterface::class);
-    // We expect an error to be logged.
-    $logger->expects($this->once())
-      ->method('error');
-    // We don't expect a notice to be logged.
-    $logger->expects($this->never())
-      ->method('notice');
-    $logger_factory = $this->getMockForAbstractClass(LoggerChannelFactoryInterface::class);
-    $logger_factory->method('get')
-      ->willReturn($logger);
-    // Add our log factory mock to the container.
-    $this->container->set('logger.factory', $logger_factory);
-
-    $queue_worker = ImportQueueWorker::create(
-      $this->container,
-      [],
-      'id',
-      ['cron' => ['lease_time' => 10]]
-    );
-    // Some random data to process.
-    $data = ['data' => ['identifier' => '12345', 'version' => '23456']];
-    $queue_worker->processItem((object) $data);
-  }
-
-  public function testRequeue() {
-    // The result we'll mock to come from the datastore service.
-    $result = new Result();
-    $result->setStatus(Result::STOPPED);
-
-    // Mock the datastore service. All the services are real, we only want to
-    // mock import().
-    $datastore_service = $this->getMockBuilder(DatastoreService::class)
-      ->setConstructorArgs([
-        $this->container->get('dkan.datastore.service.resource_localizer'),
-        $this->container->get('dkan.datastore.service.factory.import'),
-        $this->container->get('queue'),
-        $this->container->get('dkan.common.job_store'),
-        $this->container->get('dkan.datastore.import_info_list'),
-        $this->container->get('dkan.datastore.service.resource_processor.dictionary_enforcer'),
-        $this->container->get('dkan.metastore.resource_mapper'),
-      ])
-      ->onlyMethods(['import'])
-      ->getMock();
-    $datastore_service->method('import')
-      ->willReturn([$result]);
-
-    // Add our mock to the container.
-    $this->container->set('dkan.datastore.service', $datastore_service);
-
-    // Mock the logger so we can tell when the error occurs.
-    $logger = $this->getMockForAbstractClass(LoggerChannelInterface::class);
-    // We don't expect an error to be logged.
-    $logger->expects($this->never())
-      ->method('error');
-    // We expect a notice to be logged.
-    $logger->expects($this->once())
-      ->method('notice');
-    $logger_factory = $this->getMockForAbstractClass(LoggerChannelFactoryInterface::class);
-    $logger_factory->method('get')
-      ->willReturn($logger);
-    // Add our log factory mock to the container.
-    $this->container->set('logger.factory', $logger_factory);
-
-    $queue_worker = ImportQueueWorker::create(
-      $this->container,
-      [],
-      'id',
-      ['cron' => ['lease_time' => 10]]
-    );
-    // Some random data to process.
-    $data = ['data' => ['identifier' => '12345', 'version' => '23456']];
-    $queue_worker->processItem((object) $data);
+    $this->assertFileExists($fs->realpath($public_uri));
+    // Ensure the file was either replaced or not replaced.
+    if ($always_use_existing_local_perspective) {
+      $this->assertStringEqualsFile(
+        $fs->realpath($public_uri),
+        $existing_local_file_contents
+      );
+    }
+    else {
+      $this->assertStringNotEqualsFile(
+        $fs->realpath($public_uri),
+        $existing_local_file_contents
+      );
+    }
   }
 
   /**
    * @covers ::processItem
    */
-  public function testProcessItemAlreadyImported() {
-    $queue_worker = $this->createPartialMock(
-      ImportQueueWorker::class,
-      ['alreadyImported', 'importData']
-    );
-    // Always already imported.
-    $queue_worker->method('alreadyImported')
-      ->willReturn(TRUE);
-    // If it's already imported, then code flow should never hit importData().
-    $queue_worker->expects($this->once())
-      ->method('alreadyImported');
-    $queue_worker->expects($this->never())
-      ->method('importData');
-
-    // Set the state of the mock via constructor.
-    $queue_worker->__construct(
-      [],
-      'id',
-      ['cron' => ['lease_time' => 10]],
-      $this->container->get('config.factory'),
-      $this->container->get('dkan.datastore.service'),
-      $this->container->get('logger.factory'),
-      $this->container->get('dkan.metastore.reference_lookup'),
-      $this->container->get('dkan.common.database_connection_factory'),
-      $this->container->get('dkan.datastore.database_connection_factory')
-    );
-
-    // Some random data to process.
-    $data = ['data' => ['identifier' => '12345', 'version' => '23456']];
-    $queue_worker->processItem((object) $data);
-  }
-
-  /**
-   * @covers ::processItem
-   */
-  public function testProcessItemImportException() {
-    // Mock the logger so we can tell when the error occurs.
-    $logger = $this->getMockForAbstractClass(LoggerChannelInterface::class);
-    // We expect an error to be logged, and we set an expectation for the message.
-    $logger->expects($this->once())
-      ->method('error')
-      ->with('Import for 12345 returned an error: ' . __METHOD__);
-    // We don't expect a notice to be logged.
-    $logger->expects($this->never())
-      ->method('notice');
-    $logger_factory = $this->getMockForAbstractClass(LoggerChannelFactoryInterface::class);
-    $logger_factory->method('get')
-      ->willReturn($logger);
-    // Add our log factory mock to the container.
-    $this->container->set('logger.factory', $logger_factory);
-
-    $queue_worker = $this->createPartialMock(
-      ImportQueueWorker::class,
-      ['importData']
-    );
-    // Explosion on importData().
-    $queue_worker->method('importData')
-      ->willThrowException(new \Exception(__METHOD__));
-
-    // Set the state of the mock via constructor.
-    $queue_worker->__construct(
-      [],
-      'id',
-      ['cron' => ['lease_time' => 10]],
-      $this->container->get('config.factory'),
-      $this->container->get('dkan.datastore.service'),
-      $this->container->get('logger.factory'),
-      $this->container->get('dkan.metastore.reference_lookup'),
-      $this->container->get('dkan.common.database_connection_factory'),
-      $this->container->get('dkan.datastore.database_connection_factory')
-    );
-
-    // Some random data to process.
-    $data = ['data' => ['identifier' => '12345', 'version' => '23456']];
-    $queue_worker->processItem((object) $data);
-  }
-
-  /**
-   * @covers ::alreadyImported
-   */
-  public function testAlreadyImported() {
-    // Storage of the type that can know whether the import has happened already.
-    $storage = $this->getMockBuilder(ImportedItemInterface::class)
-      ->onlyMethods(['hasBeenImported'])
-      ->getMockForAbstractClass();
-    // Say the import has happened already.
-    $storage->method('hasBeenImported')
-      ->willReturn(TRUE);
-    // Ensure that this method is called during the test.
-    $storage->expects($this->once())
-      ->method('hasBeenImported');
-
-    // Datastore factory for the storage we mocked above.
-    $datastore_service = $this->getMockBuilder(DatastoreService::class)
-      // These are real services, so that we can put all the pieces in place.
+  public function testLocalizerNotDone() {
+    // Mock the localizer so it will not return DONE.
+    $localizer = $this->getMockBuilder(ResourceLocalizer::class)
       ->setConstructorArgs([
-        $this->container->get('dkan.datastore.service.resource_localizer'),
-        $this->container->get('dkan.datastore.service.factory.import'),
-        $this->container->get('queue'),
-        $this->container->get('dkan.common.job_store'),
-        $this->container->get('dkan.datastore.import_info_list'),
-        $this->container->get('dkan.datastore.service.resource_processor.dictionary_enforcer'),
+        // These are the real services, we just want to mock localizeTask().
         $this->container->get('dkan.metastore.resource_mapper'),
+        $this->container->get('dkan.common.file_fetcher'),
+        $this->container->get('dkan.common.drupal_files'),
+        $this->container->get('dkan.common.job_store'),
+        $this->container->get('queue'),
       ])
-      ->onlyMethods(['getStorage'])
+      ->onlyMethods(['localizeTask'])
       ->getMock();
-    $datastore_service->method('getStorage')
-      ->willReturn($storage);
-    $this->container->set('dkan.datastore.service', $datastore_service);
+    $not_done_result = new Result();
+    $not_done_result->setStatus(Result::IN_PROGRESS);
+    $not_done_result->setError('error message');
+    $localizer->method('localizeTask')
+      ->willReturn($not_done_result);
 
-    $queue_worker = ImportQueueWorker::create(
-      $this->container,
-      [],
-      'id',
-      ['cron' => ['lease_time' => 10]]
-    );
+    $this->container->set('dkan.datastore.service.resource_localizer', $localizer);
 
-    // Some random data to process.
-    $data = ['data' => ['identifier' => '12345', 'version' => '23456']];
+    $queue_worker = LocalizeQueueWorker::create($this->container, [], 'localize_import', []);
 
-    $ref_already_imported = new \ReflectionMethod($queue_worker, 'alreadyImported');
-    $ref_already_imported->setAccessible(TRUE);
-    $this->assertTrue($ref_already_imported->invokeArgs($queue_worker, [$data]));
+    $this->expectException(\Exception::class);
+    $this->expectExceptionMessage('Localization of resource 12345: error message');
+    $queue_worker->processItem(['identifier' => '12345', 'version' => 'version']);
   }
 
 }
