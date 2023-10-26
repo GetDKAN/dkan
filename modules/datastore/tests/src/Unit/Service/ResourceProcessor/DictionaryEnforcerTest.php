43b3d004dd9285687da3a6bd18ed2b9fbfd084a6
Paul Mitchum <paul@mile23.com>
2023-10-26 14:34:47 -0700

    case-sensitive

--- modules/datastore/tests/src/Unit/Service/ResourceProcessor/DictionaryEnforcerTest.php
+++ modules/datastore/tests/src/Unit/Service/ResourceProcessor/DictionaryEnforcerTest.php
@@ -16,7 +16,7 @@ use Drupal\datastore\Service\ResourceProcessorCollector;
 use Drupal\datastore\Service\ResourceProcessor\DictionaryEnforcer;
 use Drupal\metastore\DataDictionary\DataDictionaryDiscovery;
 use Drupal\metastore\DataDictionary\DataDictionaryDiscoveryInterface;
-use Drupal\datastore\service\PostImport;
+use Drupal\datastore\Service\PostImport;
 use Drupal\metastore\ResourceMapper;
 use Drupal\metastore\MetastoreService;
 
