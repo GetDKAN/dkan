c346dfe027d69cea4b7b9210dbe796d04c2c1f85
Paul Mitchum <paul@mile23.com>
2023-10-29 12:14:49 -0700

    refactor for cognitive complexity

--- modules/common/src/FileFetcher/DkanFileFetcher.php
+++ modules/common/src/FileFetcher/DkanFileFetcher.php
@@ -25,32 +25,46 @@ class DkanFileFetcher extends FileFetcher {
     // @todo Re-computing the custom processor classes should be in another
     //   method that is in the parent class.
     if ($use_local_file) {
-      // Set the state/config to use our remote class.
-      $this->setProcessors(['processors' => [FileFetcherRemoteUseExisting::class]]);
-      $this->setStateProperty('processor', FileFetcherRemoteUseExisting::class);
-      // Also check if we can just short-circuit here.
-      /** @var \Drupal\common\FileFetcher\FileFetcherRemoteUseExisting $processor */
-      $processor = $this->getProcessor();
-      $existing_status = $processor->discoverStatusForExistingFile(
-        $this->getState(),
-        $this->getResult()
-      );
-      $this->setState($existing_status['state']);
+      $this->dkanUseLocalFileProcessor();
     }
     else {
-      // @todo This ignores any other custom processor classes that might have
-      //   been configured. Improve this situation.
-      $this->customProcessorClasses = [];
-      $state = $this->getState();
-      foreach ($this->getProcessors() as $processor) {
-        if ($processor->isServerCompatible($state)) {
-          $state['processor'] = get_class($processor);
-          break;
-        }
-      }
-      $this->getResult()->setData(json_encode($state));
+      $this->dkanUseDefaultFileProcessor();
     }
     return $this;
+  }
+
+  /**
+   * Configure the processor to respect the local file if it already exists.
+   */
+  protected function dkanUseLocalFileProcessor() {
+    // Set the state/config to use our remote class.
+    $this->setProcessors(['processors' => [FileFetcherRemoteUseExisting::class]]);
+    $this->setStateProperty('processor', FileFetcherRemoteUseExisting::class);
+    // At this very early stage, update the status if the file already exists.
+    /** @var \Drupal\common\FileFetcher\FileFetcherRemoteUseExisting $processor */
+    $processor = $this->getProcessor();
+    $existing_status = $processor->discoverStatusForExistingFile(
+      $this->getState(),
+      $this->getResult()
+    );
+    $this->setState($existing_status['state']);
+  }
+
+  /**
+   * Configure the processor to use its default behavior.
+   */
+  protected function dkanUseDefaultFileProcessor() {
+    // @todo This ignores any other custom processor classes that might have
+    //   been configured. Improve this situation.
+    $this->customProcessorClasses = [];
+    $state = $this->getState();
+    foreach ($this->getProcessors() as $processor) {
+      if ($processor->isServerCompatible($state)) {
+        $state['processor'] = get_class($processor);
+        break;
+      }
+    }
+    $this->getResult()->setData(json_encode($state));
   }
 
 }
