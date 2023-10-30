714fa0b5a7ca890431fe467add2f615a3fb3389b
Daniel Gading <dgading@proton.me>
2023-10-30 10:42:42 -0400

    Only check for date on callback

--- modules/json_form_widget/src/Element/FlexibleDateTime.php
+++ modules/json_form_widget/src/Element/FlexibleDateTime.php
@@ -25,11 +25,8 @@ class FlexibleDateTime extends Datetime {
   /**
    * {@inheritdoc}
    */
-  public static function valueCallback(&$element, $input, FormStateInterface $form_state) {
-    if (empty($element['#default_value']) && !empty($input['date'])) {
-      $input['time'] = !empty($input['time']) ? $input['time'] : '00:00:00';
-    }
-    elseif (isset($element['#default_value']) && empty($input['time'])) {
+  public static function valueCallback(&$element, $input, FormStateInterface $form_state) {    
+    if (!empty($input["date"]) && empty($input['time'])) {
       $input['time'] = '00:00:00';
     }
     return parent::valueCallback($element, $input, $form_state);
