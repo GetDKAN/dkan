3215bbd6fd73197582a6543049a72e81e293f693
Paul Mitchum <paul@mile23.com>
2023-11-04 09:22:34 -0700

    rename test

--- modules/datastore/modules/datastore_mysql_import/tests/src/Functional/ImportLocalCopyOfRemoteFileTest.php
+++ modules/datastore/modules/datastore_mysql_import/tests/src/Functional/ImportLocalCopyOfRemoteFileTest.php
@@ -2,19 +2,16 @@
 
 namespace Drupal\Tests\datastore_mysql_import\Functional;
 
-use Drupal\Tests\datastore\Functional\UseLocalWithPrepareLocalizeTest as ParentTest;
+use Drupal\Tests\datastore\Functional\ImportLocalCopyOfRemoteFileTest as ParentTest;
 
 /**
  * Test dataset import when using existing localized files.
  *
  * @group datastore_mysql_import
- * @group datastore
  * @group btb
  * @group functional
- *
- * @see \Drupal\Tests\datastore\Kernel\UseLocalWithPrepareLocalizeTest
  */
-class UseLocalWithPrepareLocalizeTest extends ParentTest {
+class ImportLocalCopyOfRemoteFileTest extends ParentTest {
 
   /**
    * {@inheritdoc}
@@ -26,5 +23,7 @@ class UseLocalWithPrepareLocalizeTest extends ParentTest {
     'metastore',
     'node',
   ];
+
+  protected $defaultTheme = 'stark';
 
 }
