0df62f14223383ade2d591d02c02e546bbf0a056
Paul Mitchum <paul@mile23.com>
2023-12-21 20:58:09 -0800

    adds test for localization error

--- modules/datastore/tests/src/Kernel/Plugin/QueueWorker/LocalizeQueueWorkerTest.php
+++ modules/datastore/tests/src/Kernel/Plugin/QueueWorker/LocalizeQueueWorkerTest.php
@@ -3,18 +3,22 @@
 namespace Drupal\Tests\datastore\Kernel\Plugin\QueueWorker;
 
 use Drupal\common\DataResource;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\datastore\Plugin\QueueWorker\LocalizeQueueWorker;
 use Drupal\datastore\Service\ResourceLocalizer;
 use Drupal\KernelTests\KernelTestBase;
 use Procrastinator\Result;
 
 /**
- * @covers \Drupal\common\FileFetcher\FileFetcherFactory
  * @covers \Drupal\datastore\Plugin\QueueWorker\LocalizeQueueWorker
  * @coversDefaultClass \Drupal\datastore\Plugin\QueueWorker\LocalizeQueueWorker
  *
  * @group datastore
  * @group kernel
+ *
+ * @todo Expand test to ensure that no file is localized on ERROR, after
+ *   https://github.com/GetDKAN/file-fetcher/pull/36
  */
 class LocalizeQueueWorkerTest extends KernelTestBase {
 
@@ -144,6 +148,52 @@ class LocalizeQueueWorkerTest extends KernelTestBase {
 
     $this->expectException(\Exception::class);
     $this->expectExceptionMessage('Localization of resource 12345: error message');
+    $queue_worker->processItem(['identifier' => '12345', 'version' => 'version']);
+  }
+
+  /**
+   * @covers ::processItem
+   */
+  public function testNotRequeuedOnError() {
+    // The queue worker should log an error, and not throw an exception, if the
+    // result of localization is an error.
+
+    // Mock the localizer so it will return ERROR.
+    $localizer = $this->getMockBuilder(ResourceLocalizer::class)
+      ->setConstructorArgs([
+        // These are the real services, we just want to mock localizeTask().
+        $this->container->get('dkan.metastore.resource_mapper'),
+        $this->container->get('dkan.common.file_fetcher'),
+        $this->container->get('dkan.common.drupal_files'),
+        $this->container->get('dkan.common.job_store'),
+        $this->container->get('queue'),
+      ])
+      ->onlyMethods(['localizeTask'])
+      ->getMock();
+    $error_result = new Result();
+    $error_result->setStatus(Result::ERROR);
+    $error_result->setError('error message');
+    $localizer->method('localizeTask')
+      ->willReturn($error_result);
+
+    // Mock the logger so it expects error() once.
+    $logger = $this->getMockBuilder(LoggerChannelInterface::class)
+      ->onlyMethods(['error'])
+      ->getMockForAbstractClass();
+    $logger->expects($this->once())
+      ->method('error');
+
+    // Logger factory gives us mocked logger.
+    $logger_factory = $this->getMockBuilder(LoggerChannelFactoryInterface::class)
+      ->onlyMethods(['get'])
+      ->getMockForAbstractClass();
+    $logger_factory->method('get')
+      ->willReturn($logger);
+
+    $this->container->set('logger.factory', $logger_factory);
+    $this->container->set('dkan.datastore.service.resource_localizer', $localizer);
+
+    $queue_worker = LocalizeQueueWorker::create($this->container, [], 'localize_import', []);
     $queue_worker->processItem(['identifier' => '12345', 'version' => 'version']);
   }
 
