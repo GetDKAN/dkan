3cc47cb3a6a0f41fe921f6b89f8c599f5b174db4
Paul Mitchum <paul@mile23.com>
2024-01-29 15:17:12 -0800

    dashboard tests are now in HarvestPlanListBuilderTest

--- modules/harvest/tests/src/Kernel/HarvestPlanListBuilderTest.php
+++ modules/harvest/tests/src/Kernel/HarvestPlanListBuilderTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\harvest\Kernel;
 
 use Drupal\harvest\HarvestPlanListBuilder;
+use Drupal\harvest\HarvestService;
 use Drupal\KernelTests\KernelTestBase;
 use Harvest\ETL\Extract\DataJson;
 use Harvest\ETL\Load\Simple;
@@ -17,15 +18,109 @@ use Harvest\ETL\Load\Simple;
  */
 class HarvestPlanListBuilderTest extends KernelTestBase {
 
+  const HARVEST_HEADERS = [
+    'Harvest ID',
+    'Extract Status',
+    'Last Run',
+    '# of Datasets',
+  ];
+
   protected static $modules = [
     'common',
     'harvest',
     'metastore',
+    'node',
+    'user',
   ];
 
   protected function setUp() : void {
     parent::setUp();
     $this->installEntitySchema('harvest_plan');
+    $this->installEntitySchema('node');
+  }
+
+  public function testNoHarvests() {
+    /** @var \Drupal\harvest\HarvestService $harvest_service */
+    $harvest_service = $this->container->get('dkan.harvest.service');
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $this->container->get('entity_type.manager');
+
+    $list_builder = HarvestPlanListBuilder::createInstance(
+      $this->container,
+      $entity_type_manager->getDefinition('harvest_plan')
+    );
+
+    $response = $list_builder->render();
+    $json = json_encode($response);
+
+    $strings = array_merge(self::HARVEST_HEADERS, [
+      'There are no harvest plans yet.',
+    ]);
+
+    foreach ($strings as $string) {
+      $this->assertStringContainsString($string, $json);
+    }
+  }
+
+  public function testRegisteredHarvest() {
+    /** @var \Drupal\harvest\HarvestService $harvest_service */
+    $harvest_service = $this->container->get('dkan.harvest.service');
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $this->container->get('entity_type.manager');
+
+    $list_builder = HarvestPlanListBuilder::createInstance(
+      $this->container,
+      $entity_type_manager->getDefinition('harvest_plan')
+    );
+
+    // Register a harvest but don't run it.
+    $this->registerHarvestPlan($harvest_service);
+
+    $response = $list_builder->render();
+
+    $json = json_encode($response);
+    $strings = array_merge(self::HARVEST_HEADERS, [
+      'Total harvest plans: 1',
+    ]);
+
+    foreach ($strings as $string) {
+      $this->assertStringContainsString($string, $json);
+    }
+  }
+
+  public function testGoodHarvestRun() {
+    /** @var \Drupal\harvest\HarvestService $harvest_service */
+    $harvest_service = $this->container->get('dkan.harvest.service');
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = $this->container->get('entity_type.manager');
+
+    $list_builder = HarvestPlanListBuilder::createInstance(
+      $this->container,
+      $entity_type_manager->getDefinition('harvest_plan')
+    );
+
+    // Run the harvest.
+    $plan_id = 'run_harvest';
+    $this->registerHarvestPlan($harvest_service, $plan_id);
+    $run_result = $harvest_service->runHarvest($plan_id);
+    $this->assertEquals('SUCCESS', $run_result['status']['extract'] ?? 'not_a_successful_run');
+    // Get the actual run ID because it happens to be a timestamp for the last
+    // run, and it could be the next second by the time we assert against it.
+    $run_id = $harvest_service->getLastHarvestRunId($plan_id);
+
+    $response = $list_builder->render();
+
+    $json = json_encode($response);
+    $strings = array_merge(self::HARVEST_HEADERS, [
+      'harvest_link',
+      'SUCCESS',
+      json_encode(date('m/d/y H:m:s T', $run_id)),
+      '1',
+    ]);
+
+    foreach ($strings as $string) {
+      $this->assertStringContainsString($string, $json);
+    }
   }
 
   public function testRegisteredPlan() {
@@ -47,18 +142,7 @@ class HarvestPlanListBuilderTest extends KernelTestBase {
 
     // Register a harvest plan but don't run it...
     $plan_identifier = 'test_plan';
-    $plan = (object) [
-      'identifier' => $plan_identifier,
-      'extract' => (object) [
-        'type' => DataJson::class,
-        'uri' => 'file://' . __DIR__ . '/../../files/data.json',
-      ],
-      'transforms' => [],
-      'load' => (object) [
-        'type' => Simple::class,
-      ],
-    ];
-    $this->assertNotNull($harvest_service->registerHarvest($plan));
+    $this->registerHarvestPlan($harvest_service, $plan_identifier);
 
     // Revisit the dashboard. It should show the registered harvest.
     $table_render = $list_builder->render()['table']['table'] ?? 'phail';
@@ -71,6 +155,22 @@ class HarvestPlanListBuilderTest extends KernelTestBase {
     $this->assertEquals('REGISTERED', $row['extract_status']['data'] ?? NULL);
     $this->assertEquals('never', $row['last_run'] ?? NULL);
     $this->assertEquals('unknown', $row['dataset_count'] ?? NULL);
+  }
+
+  protected function registerHarvestPlan(HarvestService $harvest_service, string $plan_identifier = 'test_plan') {
+    $plan = (object) [
+      'identifier' => $plan_identifier,
+      'extract' => (object) [
+        'type' => DataJson::class,
+        'uri' => 'file://' . __DIR__ . '/../../files/data.json',
+      ],
+      'transforms' => [],
+      'load' => (object) [
+        'type' => Simple::class,
+      ],
+    ];
+    $this->assertNotNull($identifier = $harvest_service->registerHarvest($plan));
+    return $identifier;
   }
 
 }
