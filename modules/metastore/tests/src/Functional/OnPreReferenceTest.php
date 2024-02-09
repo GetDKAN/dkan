fb0da1870589b9bd10c52b03855b56f0dea1fae0
Tolase Akinwale <tolase.akinwale@civicactions.com>
2024-02-09 10:26:48 -0600

    update phpunit group

--- modules/metastore/tests/src/Functional/OnPreReferenceTest.php
+++ modules/metastore/tests/src/Functional/OnPreReferenceTest.php
@@ -9,7 +9,7 @@ use weitzman\DrupalTestTraits\ExistingSiteBase;
  * Run tests in separate processes, since they rely on the value of
  *   drupal_static().
  * @runTestsInSeparateProcesses
- * @group functional_1
+ * @group functional_2
  */
 class OnPreReferenceTest extends ExistingSiteBase {
   use CleanUp;
