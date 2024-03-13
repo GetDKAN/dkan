2519416531e0f348384476f7a6d8182d22dcfa1e
Kaise Lafrai <lafrai.k@gmail.com>
2024-03-13 15:27:54 -0400

    Added new tests that load the subform when editing an existing data dictionary field, added a new test to update a field and save the edited data dictionary field

--- modules/data_dictionary_widget/tests/src/Unit/DataDictionaryWidgetBuildTest.php
+++ modules/data_dictionary_widget/tests/src/Unit/DataDictionaryWidgetBuildTest.php
@@ -247,7 +247,7 @@ class DataDictionaryWidgetBuildTest extends TestCase {
     // Set up a triggering element with '#op' set to 'add'.
     $trigger= ['#op' => 'add'];
 
-    // Expect that getTriggeringElement will be called once and return the addTrigger.
+    // Expect that getTriggeringElement will be called once and return the add.
     $formState->expects($this->any())
       ->method('getTriggeringElement')
       ->willReturn($trigger);
@@ -274,7 +274,6 @@ class DataDictionaryWidgetBuildTest extends TestCase {
     // Convert JSON to array
     $data = json_decode($json_data, true);
 
-    // Assert keys and values exist in $user_input
     $this->assertEquals($data['identifier'], $user_input['field_json_metadata']['identifier']);
     $this->assertEquals($data['title'], $user_input['field_json_metadata']['title']);
     $this->assertEquals($data['data']['fields'][0]['name'], $user_input['field_json_metadata']['dictionary_fields']['field_collection']['group']['name']);
@@ -282,5 +281,224 @@ class DataDictionaryWidgetBuildTest extends TestCase {
     $this->assertEquals($data['data']['fields'][0]['type'], $user_input['field_json_metadata']['dictionary_fields']['field_collection']['group']['type']);
     $this->assertEquals($data['data']['fields'][0]['format'], $user_input['field_json_metadata']['dictionary_fields']['field_collection']['group']['format']);
     $this->assertEquals($data['data']['fields'][0]['description'], $user_input['field_json_metadata']['dictionary_fields']['field_collection']['group']['description']);
+  }
+
+  /**
+   * Test the creation of the edit buttons for a data dictionary field.
+   */
+  public function testEditButtonsCreation() {
+
+    // Create mock objects.
+    $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
+    $field_definition = $this->createMock(FieldDefinitionInterface::class);
+    $settings = [];
+    $third_party_settings = [];
+    $form = [];
+    $plugin_id = '';
+    $plugin_definition = [];
+
+    $current_fields = [
+      [
+        'name' => 'test',
+        'title' => 'test',
+        'type' => 'string',
+        'format' => 'default',
+        'description' => 'test'
+      ]
+    ];
+
+    $formState->expects($this->once())
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->once())
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->once())
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->exactly(5))
+      ->method('get')
+      ->willReturnOnConsecutiveCalls([], $current_fields, NULL, FALSE, NULL);
+
+    $dataDictionaryWidget = new DataDictionaryWidget (
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $third_party_settings
+    );
+
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    $dataDictionaryWidget->trustedCallbacks();
+    $dataDictionaryWidget->preRenderForm($element['dictionary_fields']);
+
+    $this->assertNotNull($element["dictionary_fields"]["edit_buttons"]);
+    $this->assertArrayHasKey('#name', $element["dictionary_fields"]["edit_buttons"][0]["edit_button"]);
+    $this->assertArrayHasKey('#id', $element["dictionary_fields"]["edit_buttons"][0]["edit_button"]);
+    $this->assertArrayHasKey('#op', $element["dictionary_fields"]["edit_buttons"][0]["edit_button"]);
+  }
+
+    /**
+   * Test edit a data dictionary field and save it.
+   */
+  public function testEditDataDictionaryField() {
+
+    // Create mock objects.
+    $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
+    $field_definition = $this->createMock(FieldDefinitionInterface::class);
+    $settings = [];
+    $third_party_settings = [];
+    $form = [];
+    $plugin_id = '';
+    $plugin_definition = [];
+
+    $current_fields = [
+      [
+        'name' => 'test',
+        'title' => 'test',
+        'type' => 'string',
+        'format' => 'default',
+        'description' => 'test'
+      ]
+    ];
+
+    $updated_current_fields = [
+      [
+        'name' => 'test_edit',
+        'title' => 'test_edit',
+        'type' => 'string',
+        'format' => 'default',
+        'description' => 'test_Edit'
+      ]
+    ];
+
+    $fields_being_modified = [
+      [
+        'name' => 'test',
+        'title' => 'test',
+        'type' => 'string',
+        'format' => 'default',
+        'description' => 'test'
+      ]
+    ];
+
+    $op = "edit_0";
+
+    $user_input = [
+      'field_json_metadata' => [
+        0 => [
+          'identifier' => 'Kaise',
+          'title' => 'Kaise',
+          'dictionary_fields' => [
+            'data' => [
+              0 => [
+                'field_collection' => [
+                    'name' => 'test_edit',
+                    'title' => 'test_edit',
+                    'type' => 'string',
+                    'format' => 'default',
+                    'format_other' => '',
+                    'description' => 'test_edit',
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ];
+
+    $formState->expects($this->exactly(2))
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->exactly(2))
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->exactly(2))
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->exactly(12))
+      ->method('get')
+      ->willReturnOnConsecutiveCalls([], $current_fields, $fields_being_modified, FALSE, NULL, $fields_being_modified, $fields_being_modified, [], $updated_current_fields, $fields_being_modified, FALSE, NULL);
+
+    $formState->expects($this->exactly(11))
+      ->method('getTriggeringElement')
+      ->willReturnOnConsecutiveCalls(['#op' => $op], ['#op' => $op], ['#op' => $op], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => $op], ['#op' => $op], ['#op' => $op], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0'], ['#op' => 'update_0']);
+
+    $formState->expects($this->any())
+      ->method('getUserInput')
+      ->willReturn($user_input);
+
+    $dataDictionaryWidget = new DataDictionaryWidget (
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $third_party_settings
+    );
+
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    // Assert that the subform to collect the edits for the data dictionary field exists.
+    $this->assertNotNull($element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('name', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('title', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('type', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('format', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('format_other', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('description', $element["dictionary_fields"]["edit_fields"][0]);
+    $this->assertArrayHasKey('update_field', $element["dictionary_fields"]["edit_fields"][0]);
+
+    $form["field_json_metadata"]["widget"][0]["dictionary_fields"]["data"]["#rows"][0] = [
+      'name' => 'test_edit',
+      'title' => 'test_edit',
+      'type' => 'string',
+      'format' => 'default',
+      'format_other' => '',
+      'description' => 'test_edit',
+    ];
+
+    // Trigger callback function to save the edited fields. 
+    FieldCallbacks::editSubformCallback($form, $formState);
+
+    // Second call to re-create data dictionary and apply the edits made to the fields.
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    $this->assertNotNull($element);
+    $this->assertEquals($element["dictionary_fields"]["data"]["#rows"][0]["name"], $updated_current_fields[0]["name"]);
+    $this->assertEquals($element["dictionary_fields"]["data"]["#rows"][0]["title"], $updated_current_fields[0]["title"]);
+    $this->assertEquals($element["dictionary_fields"]["data"]["#rows"][0]["type"], $updated_current_fields[0]["type"]);
+    $this->assertEquals($element["dictionary_fields"]["data"]["#rows"][0]["format"], $updated_current_fields[0]["format"]);
+    $this->assertEquals($element["dictionary_fields"]["data"]["#rows"][0]["description"], $updated_current_fields[0]["description"]);
   }
 }
