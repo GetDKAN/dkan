540cc85f380f55e6f1c444dbc222e2f8b57708e9
Paul Mitchum <paul@mile23.com>
2024-03-20 10:13:44 -0700

    CS

--- modules/harvest/src/Entity/HarvestPlan.php
+++ modules/harvest/src/Entity/HarvestPlan.php
@@ -2,9 +2,7 @@
 
 namespace Drupal\harvest\Entity;
 
-use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\harvest\HarvestPlanInterface;
 
