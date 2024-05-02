3123b731794125bbfaa1e408ce0cab6746d8a0b2
Stefan Korn <drupal@stefan-korn.de>
2024-05-02 09:17:44 +0200

    #4174: Allow to activate trailing delimiter for CSV parser

--- modules/datastore/src/Service/ImportService.php
+++ modules/datastore/src/Service/ImportService.php
@@ -202,11 +202,15 @@ class ImportService {
       'quote' => '"',
       'escape' => "\\",
       'record_end' => ["\n", "\r"],
+      'trailing_delimiter' => FALSE,
     ];
 
     $parserConfiguration = $this->dispatchEvent(self::EVENT_CONFIGURE_PARSER, $parserConfiguration);
 
     $parser = Csv::getParser($parserConfiguration['delimiter'], $parserConfiguration['quote'], $parserConfiguration['escape'], $parserConfiguration['record_end']);
+    if (!empty($parserConfiguration['trailing_delimiter'])) {
+      $parser->activateTrailingDelimiter();
+    }
     $parser->machine->stopRecording();
     return $parser;
   }
