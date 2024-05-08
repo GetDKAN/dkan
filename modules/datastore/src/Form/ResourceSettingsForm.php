b36f298f959b8eeff072bfec57e58015ed99d0e3
jastraat <jess.straatmann@gmail.com>
2024-05-07 20:10:46 -0500

    Use the new config_target property in the datastore settings form (Drupal 10.2+)

--- modules/datastore/src/Form/ResourceSettingsForm.php
+++ modules/datastore/src/Form/ResourceSettingsForm.php
@@ -26,7 +26,8 @@ class ResourceSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['datastore.settings', 'metastore.settings'];
+    // This form uses #config_target instead.
+    return [];
   }
 
   /**
@@ -41,17 +42,17 @@ class ResourceSettingsForm extends ConfigFormBase {
     $form['resources']['purge_table'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Datastore table'),
-      '#default_value' => $this->config('datastore.settings')->get('purge_table'),
+      '#config_target' => 'datastore.settings:purge_table',
     ];
     $form['resources']['purge_file'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('File'),
-      '#default_value' => $this->config('datastore.settings')->get('purge_file'),
+      '#config_target' => 'datastore.settings:purge_file',
     ];
     $form['delete_local_resource'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Delete local resource'),
-      '#default_value' => $this->config('datastore.settings')->get('delete_local_resource'),
+      '#config_target' => 'datastore.settings:delete_local_resource',
       '#description' => $this->t('Delete local copy of remote files after the datastore import is complete'),
     ];
     $form['resource_perspective_display'] = [
@@ -63,24 +64,9 @@ class ResourceSettingsForm extends ConfigFormBase {
         DataResource::DEFAULT_SOURCE_PERSPECTIVE => $this->t('Source'),
         ResourceLocalizer::LOCAL_URL_PERSPECTIVE => $this->t('Local URL'),
       ],
-      '#default_value' => $this->config('metastore.settings')->get('resource_perspective_display') ?: DataResource::DEFAULT_SOURCE_PERSPECTIVE,
+      '#config_target' => 'metastore.settings:resource_perspective_display',
     ];
     return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('datastore.settings')
-      ->set('purge_table', $form_state->getValue('purge_table'))
-      ->set('purge_file', $form_state->getValue('purge_file'))
-      ->set('delete_local_resource', $form_state->getValue('delete_local_resource'))
-      ->save();
-    $this->config('metastore.settings')
-      ->set('resource_perspective_display', $form_state->getValue('resource_perspective_display'))
-      ->save();
-    parent::submitForm($form, $form_state);
   }
 
 }
