a612bba80cb603511fb47bb5e0fcf5735ff448f6
Janette Day <janette.day@civicactions.com>
2024-07-05 18:59:41 -0500

    Update test to confirm the 500 error is addressed

--- modules/datastore/tests/src/Functional/Api1/DatastoreQueryApiTest.php
+++ modules/datastore/tests/src/Functional/Api1/DatastoreQueryApiTest.php
@@ -11,17 +11,21 @@ class DatastoreQueryApiTest extends Api1TestBase {
     return 'api/1/metastore/schemas/dataset/items';
   }
 
+  /**
+   * Confirm a 400 response prior to running the datastore_import.
+   */
   public function testBasicQuery() {
     $dataset = $this->getSampleDataset();
     $this->post($dataset, FALSE);
-
     $dataset_id = $dataset->identifier;
+
     $response = $this->httpClient->get("api/1/datastore/query/$dataset_id/0", [
       RequestOptions::HTTP_ERRORS => FALSE,
     ]);
     $this->assertEquals(400, $response->getStatusCode());
     $this->validator->validate($response, "/api/1/datastore/query/{datasetId}/{index}", 'get');
 
+    // Confirm a 200 response after the datastore_import has run.
     $dataset_info = \Drupal::service('dkan.common.dataset_info')->gather($dataset_id);
     $resource_id = $dataset_info['latest_revision']['distributions'][0]['resource_id'];
     \Drupal::service('dkan.datastore.service')->import($resource_id, FALSE);
@@ -31,4 +35,31 @@ class DatastoreQueryApiTest extends Api1TestBase {
     $this->assertEquals(200, $response->getStatusCode());
     $this->validator->validate($response, "/api/1/datastore/query/{datasetId}/{index}", 'get');
   }
+
+  /**
+   * Query on in progress datastore updates.
+   *
+   * Confirm a 400 response after a resource is updated, localize_import
+   * has run, but the datastore_import queue has not yet run.
+   */
+  public function testBasicQueryAfterTableDrop() {
+    $dataset = $this->getSampleDataset();
+    $this->post($dataset, FALSE);
+    $dataset_id = $dataset->identifier;
+
+    $dataset_info = \Drupal::service('dkan.common.dataset_info')->gather($dataset_id);
+    $resource_id = $dataset_info['latest_revision']['distributions'][0]['resource_id'];
+    \Drupal::service('dkan.datastore.service')->import($resource_id, FALSE);
+
+    $dataset_info = \Drupal::service('dkan.common.dataset_info')->gather($dataset_id);
+    $datastore_table = $dataset_info['latest_revision']['distributions'][0]['table_name'];
+    \Drupal::database()->schema()->dropTable($datastore_table);
+
+    $response = $this->httpClient->get("api/1/datastore/query/$dataset_id/0", [
+      RequestOptions::HTTP_ERRORS => FALSE,
+    ]);
+    $this->assertEquals(400, $response->getStatusCode());
+    $this->validator->validate($response, "/api/1/datastore/query/{datasetId}/{index}", 'get');
+  }
+
 }
