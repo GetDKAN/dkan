51a1017e3899f9335dbfd8e29b05086eef10f929
Paul Mitchum <paul@mile23.com>
2024-08-02 14:49:46 -0700

    moved DkanDocsTest to OpenApiControllerTest, convert to BTB

--- modules/common/tests/src/Functional/Controller/OpenApiControllerTest.php
+++ modules/common/tests/src/Functional/Controller/OpenApiControllerTest.php
@@ -1,12 +1,29 @@
 <?php
 
-namespace Drupal\Tests\common\Functional;
+namespace Drupal\Tests\common\Functional\Controller;
 
 use Drupal\common\Controller\OpenApiController;
 use Drupal\Core\Serialization\Yaml;
-use weitzman\DrupalTestTraits\ExistingSiteBase;
+use Drupal\Tests\BrowserTestBase;
 
-class DkanDocsTest extends ExistingSiteBase {
+/**
+ * @coversDefaultClass \Drupal\common\Controller\OpenApiController
+ *
+ * @group dkan
+ * @group common
+ * @group functional
+ * @group btb
+ */
+class OpenApiControllerTest extends BrowserTestBase {
+
+  protected static $modules = [
+    'common',
+    'datastore',
+    'harvest',
+    'node',
+  ];
+
+  protected $defaultTheme = 'stark';
 
   public function testGetVersions() {
     $controller = $this->getController();
@@ -23,14 +40,13 @@ class DkanDocsTest extends ExistingSiteBase {
     $data = json_decode($response->getContent(), TRUE);
 
     // Basic auth is included.
-    $this->assertTrue(isset($data["components"]["securitySchemes"]["basic_auth"]));
+    $this->assertTrue(isset($data['components']['securitySchemes']['basic_auth']));
 
     // Some sample paths from different modules are there
-    $this->assertArrayHasKey('/api/1/datastore/imports', $data["paths"]);
-    $this->assertArrayHasKey('/api/1/harvest/plans/{plan_id}', $data["paths"]);
-    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data["paths"]);
+    $this->assertArrayHasKey('/api/1/datastore/imports', $data['paths']);
+    $this->assertArrayHasKey('/api/1/harvest/plans/{plan_id}', $data['paths']);
+    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data['paths']);
   }
-
 
   public function testGetCompleteYaml() {
     $controller = $this->getController();
@@ -39,16 +55,16 @@ class DkanDocsTest extends ExistingSiteBase {
     $data = Yaml::decode($response->getContent(), TRUE);
 
     // Basic auth is included.
-    $this->assertTrue(isset($data["components"]["securitySchemes"]["basic_auth"]));
+    $this->assertTrue(isset($data['components']['securitySchemes']['basic_auth']));
 
     // Some sample paths from different modules are there
-    $this->assertArrayHasKey('/api/1/datastore/imports', $data["paths"]);
-    $this->assertArrayHasKey('/api/1/harvest/plans/{plan_id}', $data["paths"]);
-    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data["paths"]);
+    $this->assertArrayHasKey('/api/1/datastore/imports', $data['paths']);
+    $this->assertArrayHasKey('/api/1/harvest/plans/{plan_id}', $data['paths']);
+    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data['paths']);
   }
 
   public function testGetNoAuth() {
-    $controller = $this->getController(["authentication" => "false"]);
+    $controller = $this->getController(['authentication' => 'false']);
     $response = $controller->getComplete();
 
     // Simulate an authentication=false parameter in the request stack.
@@ -56,18 +72,18 @@ class DkanDocsTest extends ExistingSiteBase {
     $data = json_decode($response->getContent(), TRUE);
 
     // Basic auth is excluded.
-    $this->assertFalse(isset($data["components"]["securitySchemes"]["basic_auth"]));
+    $this->assertFalse(isset($data['components']['securitySchemes']['basic_auth']));
 
     // Authorized paths are not there.
-    $this->assertArrayNotHasKey('/api/1/datastore/imports', $data["paths"]);
-    $this->assertArrayNotHasKey('/api/1/harvest/plans/{plan_id}', $data["paths"]);
+    $this->assertArrayNotHasKey('/api/1/datastore/imports', $data['paths']);
+    $this->assertArrayNotHasKey('/api/1/harvest/plans/{plan_id}', $data['paths']);
     // Public paths still there.
-    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data["paths"]);
+    $this->assertArrayHasKey('/api/1/metastore/schemas/{schema_id}', $data['paths']);
   }
 
   private function getController(array $params = []) {
     $requestStack = \Drupal::service('request_stack');
-   
+
     if (!empty($params)) {
       $request = $requestStack->pop()->duplicate($params);
       $requestStack->push($request);
