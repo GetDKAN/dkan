83b74e0b27b7dba191b1887d754a58437edd3008
Daniel Mundra <daniel.mundra@civicactions.com>
2024-08-14 14:18:01 -0700

    Adjusted add index test and added tests for editing indexes.

--- modules/data_dictionary_widget/tests/src/Unit/DataDictionaryWidgetBuildIndexesTest.php
+++ modules/data_dictionary_widget/tests/src/Unit/DataDictionaryWidgetBuildIndexesTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\data_dictionary_widget\Unit;
 
+use Drupal\Core\Entity\EntityFormInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -24,7 +26,6 @@ class DataDictionaryWidgetBuildIndexesTest extends TestCase {
    * Test collecting indexes field information.
    */
   public function testIndexesFieldCollectionDictionaryWidget() {
-
     // Create mock objects.
     $formState = $this->createMock(FormStateInterface::class);
     $fieldItemList = $this->createMock(FieldItemListInterface::class);
@@ -35,7 +36,7 @@ class DataDictionaryWidgetBuildIndexesTest extends TestCase {
     $plugin_id = '';
     $plugin_definition = [];
 
-    $dataDictionaryWidget = new DataDictionaryWidget (
+    $dataDictionaryWidget = new DataDictionaryWidget(
       $plugin_id,
       $plugin_definition,
       $field_definition,
@@ -79,90 +80,142 @@ class DataDictionaryWidgetBuildIndexesTest extends TestCase {
   }
 
   /**
-   * Test creating a new dictionary field with indexes and adding it to the dictionary table.
+   * Test creating a new dictionary with indexes and adding it to the dictionary table.
    */
-  public function testAddNewFieldWithIndexDictionaryWidget() {
-
+  public function testAddNewIndexesDictionaryWidget() {
     // Create mock objects.
     $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
     $field_definition = $this->createMock(FieldDefinitionInterface::class);
     $settings = [];
     $third_party_settings = [];
+    $form = [];
     $plugin_id = '';
     $plugin_definition = [];
 
-    $form = [
+    $new_index = [
       'field_json_metadata' => [
-        'widget' => [
-          0 => [
-            'dictionary_fields' => [
-              'data' => [
-                '#rows' => null
-              ]
-            ],
-            'indexes' => [
-              'data' => [
-                '#rows' => null
-              ]
-            ]
-          ]
-        ]
-      ]
-    ];
-
-    $user_input = [
-      'field_json_metadata' => [
-        'identifier' => 'test',
-        'title' => 'test',
-        'indexes' => [
-          'field_collection' => [
-            'group' => [
-              'index' => [
-                'description' => 'test',
-                'type' => 'index',
-                'fields' => [
-                  'field_collection' => [
-                    'group' => [
-                      'index' => [
-                        'fields' => [
-                          'name' => 'test',
-                          'length' => 20,
-                        ]
-                      ]
-                    ]
-                  ]
-                ],
-                'data' => null,
-                'edit_buttons' => [],
-              ]
-            ]
-          ]
-        ]
-      ]
-    ];
-
-    $values = [
-      [
-        'identifier' => 'test',
-        'title' => 'test',
-        'indexes' => [
-          'data' => '',
-          'add_row_button' => 'Add index',
-          'field_collection' => [
-            'group' => [
-              'index' => [
-                'description' => 'test',
-                'type' => 'index',
-              ]
-            ]
-          ],
-          'fields' => [
+        [
+          'indexes' => [
             'field_collection' => [
               'group' => [
-                'indexes' => [
-                  'fields' => [
-                    'name' => 'test',
-                    'length' => 20,
+                'index' => [
+                  'description' => 'test',
+                  'type' => 'index',
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ];
+
+    $formState->expects($this->once())
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->once())
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->once())
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->exactly(14))
+      ->method('get')
+      ->willReturnOnConsecutiveCalls(
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        $new_index,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+      );
+
+    // Set up a triggering element with '#op' set to 'add_index'.
+    $trigger = ['#op' => 'add_index'];
+    // Expect that getTriggeringElement will be called once and return the add.
+    $formState->expects($this->any())
+      ->method('getTriggeringElement')
+      ->willReturn($trigger);
+
+    $dataDictionaryWidget = new DataDictionaryWidget(
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $third_party_settings
+    );
+
+    // Call the method under test.
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    $this->assertEquals('test', $element['indexes']['data']['#rows'][0]['description']);
+    $this->assertEquals('index', $element['indexes']['data']['#rows'][0]['type']);
+  }
+
+  /**
+   * Test creating a new dictionary with indexes with index fields and adding it to the dictionary table.
+   */
+  public function testAddNewIndexesWithFieldsDictionaryWidget() {
+    // Create mock objects.
+    $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
+    $field_definition = $this->createMock(FieldDefinitionInterface::class);
+    $settings = [];
+    $third_party_settings = [];
+    $form = [];
+    $plugin_id = '';
+    $plugin_definition = [];
+
+    $new_index = [
+      'field_json_metadata' => [
+        [
+          'indexes' => [
+            'field_collection' => [
+              'group' => [
+                'index' => [
+                  'description' => 'test',
+                  'type' => 'index',
+                ]
+              ]
+            ]
+          ]
+        ]
+      ]
+    ];
+
+    $new_index_fields = [
+      'field_json_metadata' => [
+        [
+          'indexes' => [
+            'fields' => [
+              'field_collection' => [
+                'group' => [
+                  'index' => [
+                    'fields' => [
+                      'name' => 'test',
+                      'length' => 20,
+                    ]
                   ]
                 ]
               ]
@@ -172,7 +225,45 @@ class DataDictionaryWidgetBuildIndexesTest extends TestCase {
       ]
     ];
 
-    $dataDictionaryWidget = new DataDictionaryWidget (
+    $formState->expects($this->once())
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->once())
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->once())
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->exactly(15))
+      ->method('get')
+      ->willReturnOnConsecutiveCalls(
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        $new_index_fields,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+      );
+
+    // Set up a triggering element with '#op' set to 'add_index'.
+    $trigger = ['#op' => 'add_index_field'];
+    // Expect that getTriggeringElement will be called once and return the add.
+    $formState->expects($this->any())
+      ->method('getTriggeringElement')
+      ->willReturn($trigger);
+
+    $dataDictionaryWidget = new DataDictionaryWidget(
       $plugin_id,
       $plugin_definition,
       $field_definition,
@@ -180,43 +271,289 @@ class DataDictionaryWidgetBuildIndexesTest extends TestCase {
       $third_party_settings
     );
 
-    // Set up a triggering element with '#op' set to 'add_index'.
-    $trigger = ['#op' => 'add_index'];
-
-    // Expect that getTriggeringElement will be called once and return the add.
-    $formState->expects($this->any())
-      ->method('getTriggeringElement')
-      ->willReturn($trigger);
-
-    $formState->expects($this->exactly(12))
-      ->method('set')
-      ->willReturnOnConsecutiveCalls (
-        '',
-        $this->equalTo($user_input),
-        TRUE,
-        FALSE,
-        '',
-        ''
-      );
-
-    IndexFieldCallbacks::indexAddCallback($form, $formState);
-
     // Call the method under test.
-    $json_data = $dataDictionaryWidget->massageFormValues(
-      $values,
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
       $form,
       $formState
     );
 
-    // Convert JSON to array
-    $data = json_decode($json_data, true);
+    $this->assertEquals('test', $element['indexes']['fields']['data']['#rows'][0]['name']);
+    $this->assertEquals(20, $element['indexes']['fields']['data']['#rows'][0]['length']);
+  }
 
-    $this->assertEquals($data['identifier'], $user_input['field_json_metadata']['identifier']);
-    $this->assertEquals($data['data']['title'], $user_input['field_json_metadata']['title']);
-    $this->assertEquals($data['data']['indexes'][0]['description'], $user_input['field_json_metadata']['indexes']['field_collection']['group']['index']['description']);
-    $this->assertEquals($data['data']['indexes'][0]['type'], $user_input['field_json_metadata']['indexes']['field_collection']['group']['index']['type']);
-    $this->assertEquals($data['data']['indexes'][0]['fields'][0]['name'], $user_input['field_json_metadata']['indexes']['field_collection']['group']['index']['fields']['field_collection']['group']['index']['fields']['name']);
-    $this->assertEquals($data['data']['indexes'][0]['fields'][0]['length'], $user_input['field_json_metadata']['indexes']['field_collection']['group']['index']['fields']['field_collection']['group']['index']['fields']['length']);
+  /**
+   * Test the creation of the edit buttons for a data dictionary indexes field.
+   */
+  public function testEditIndexesButtonsCreationDictionaryWidget() {
+    // Create mock objects.
+    $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
+    $field_definition = $this->createMock(FieldDefinitionInterface::class);
+    $settings = [];
+    $third_party_settings = [];
+    $form = [];
+    $plugin_id = '';
+    $plugin_definition = [];
+
+    $current_fields = [
+      [
+        'description' => 'test',
+        'type' => 'index',
+      ]
+    ];
+
+    $formState->expects($this->once())
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->once())
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->once())
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->exactly(14))
+      ->method('get')
+      ->willReturnOnConsecutiveCalls(
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        $current_fields,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+      );
+
+    $dataDictionaryWidget = new DataDictionaryWidget(
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $third_party_settings
+    );
+
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    $this->assertNotNull($element["indexes"]["edit_index_buttons"]);
+    $this->assertArrayHasKey('#name', $element["indexes"]["edit_index_buttons"]['index_key_0']["edit_index_button"]);
+    $this->assertArrayHasKey('#id', $element["indexes"]["edit_index_buttons"]['index_key_0']["edit_index_button"]);
+    $this->assertArrayHasKey('#op', $element["indexes"]["edit_index_buttons"]['index_key_0']["edit_index_button"]);
+  }
+
+  /**
+   * Test edit a data dictionary index field and save it.
+   */
+  public function testEditDataDictionaryIndexDictionaryWidget() {
+
+    // Create mock objects.
+    $formState = $this->createMock(FormStateInterface::class);
+    $formObject = $this->createMock(EntityFormInterface::class);
+    $entity = $this->createMock(FieldableEntityInterface::class);
+    $fieldItemList = $this->createMock(FieldItemListInterface::class);
+    $field_definition = $this->createMock(FieldDefinitionInterface::class);
+    $settings = [];
+    $third_party_settings = [];
+    $form = [];
+    $plugin_id = '';
+    $plugin_definition = [];
+
+    $current_index = [
+      [
+        'description' => 'test',
+        'type' => 'index',
+      ]
+    ];
+
+    $updated_index = [
+      [
+        'description' => 'test_edit',
+        'type' => 'fulltext',
+      ]
+    ];
+
+    $op = "edit_index_key_0";
+
+    $user_input = [
+      'field_json_metadata' => [
+        0 => [
+          'identifier' => 'test_identifier',
+          'title' => 'test_title',
+          'dictionary_fields' => [
+            'field_collection' => [
+              'group' => [
+                'name' => 'test_edit',
+                'title' => 'test_edit',
+                'type' => 'string',
+                'format' => 'default',
+                'format_other' => '',
+                'description' => 'test_edit',
+              ]
+            ],
+            'data' => [
+              [
+                'field_collection' => [
+                  'name' => 'test_edit',
+                  'title' => 'test_edit',
+                  'type' => 'string',
+                  'format' => 'default',
+                  'format_other' => '',
+                  'description' => 'test_edit',
+                ],
+              ],
+            ],
+          ],
+        ],
+      ],
+    ];
+
+    $form["field_json_metadata"]["widget"][0] = [
+      "dictionary_fields" => [
+        "data" => [
+          "#rows" => [
+            0 => [],
+          ],
+        ],
+      ],
+      'indexes' => [
+        'data' => [
+          "#rows" => [
+            [
+              'description' => 'test',
+              'type' => 'index',
+            ],
+          ],
+        ],
+        'fields' => [
+          'data' => [
+            "#rows" => [
+              0 => [],
+            ],
+          ],
+        ]
+      ],
+    ];
+
+    $formState->expects($this->exactly(2))
+      ->method('getFormObject')
+      ->willReturn($formObject);
+
+    $formObject->expects($this->exactly(2))
+      ->method('getEntity')
+      ->willReturn($entity);
+
+    $entity->expects($this->exactly(2))
+      ->method('set')
+      ->with('field_data_type', 'data-dictionary');
+
+    $formState->expects($this->any())
+      ->method('get')
+      ->willReturnOnConsecutiveCalls(
+        [],
+        $current_index,
+        $current_index,
+        [],
+        [],
+        [],
+        [],
+        [],
+        $current_index,
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        TRUE,
+        [],
+        $current_index,
+        $current_index,
+        [],
+        [],
+        [],
+        [],
+        [],
+        $updated_index,
+        [],
+        [],
+        [],
+        [],
+        []
+      );
+
+    $formState->expects($this->any())
+      ->method('getTriggeringElement')
+      ->willReturnOnConsecutiveCalls(
+        ['#op' => $op], ['#op' => $op], ['#op' => $op], ['#op' => $op], ['#op' => $op], ['#op' => $op],
+        ['#op' => 'update'], ['#op' => 'update'], ['#op' => 'update'], ['#op' => 'update'], ['#op' => 'update'],
+      );
+
+    $formState->expects($this->any())
+      ->method('getUserInput')
+      ->willReturn($user_input);
+
+    $dataDictionaryWidget = new DataDictionaryWidget(
+      $plugin_id,
+      $plugin_definition,
+      $field_definition,
+      $settings,
+      $third_party_settings
+    );
+
+    // Trigger callback function to edit fields.
+    IndexFieldCallbacks::indexEditCallback($form, $formState);
+
+    // First call to re-create data dictionary form with the editable fields.
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    // Assert edit feature loads form with the current field values.
+    $this->assertNotNull($element);
+    $this->assertEquals($element["indexes"]["data"]["#rows"][0]["description"], $current_index[0]["description"]);
+    $this->assertEquals($element["indexes"]["data"]["#rows"][0]["type"], $current_index[0]["type"]);
+
+    // Trigger callback function to save the edited fields.
+    IndexFieldCallbacks::indexEditCallback($form, $formState);
+
+    // Second call to re-create data dictionary and apply the edits made to the fields.
+    $element = $dataDictionaryWidget->formElement(
+      $fieldItemList,
+      0,
+      [],
+      $form,
+      $formState
+    );
+
+    // Assert update feature loads form with the edited field values.
+    $this->assertNotNull($element);
+    $this->assertEquals($element["indexes"]["data"]["#rows"][0]["description"], $updated_index[0]["description"]);
+    $this->assertEquals($element["indexes"]["data"]["#rows"][0]["type"], $updated_index[0]["type"]);
   }
 
 }
