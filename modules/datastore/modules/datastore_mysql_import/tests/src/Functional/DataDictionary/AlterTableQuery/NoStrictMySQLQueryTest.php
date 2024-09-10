7bef11ea3700fe941abae91da0490fc3525c83d6
Paul Mitchum <paul@mile23.com>
2024-09-10 08:22:11 -0700

    some work

---
+++ modules/datastore/modules/datastore_mysql_import/tests/src/Functional/DataDictionary/AlterTableQuery/NoStrictMySQLQueryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\Tests\datastore_mysql_import\Functional\DataDictionary\AlterTableQuery;
+
+use Drupal\Tests\BrowserTestBase;
+
+class NoStrictMySQLQueryTest extends BrowserTestBase {
+
+  protected static $modules = [
+    'datastore_mysql_import',
+  ];
+
+  protected $defaultTheme = 'stark';
+
+  public function testPostImport() {
+    // Create a data dictionary for wide_table.csv. (Columns are numeric.)
+    // Import wide_table.csv
+    // Run post-import process on dataset.
+    // Verify that the process was successful.
+    // Verify that our DD schema was applied.
+  }
+
+}
