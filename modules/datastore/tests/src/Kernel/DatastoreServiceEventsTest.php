ae7c06b3fb00fe52fd173726749ba63c98ddfd71
Paul Mitchum <paul@mile23.com>
2024-10-12 10:20:11 -0700

    adds test for datastoreservice events

---
+++ modules/datastore/tests/src/Kernel/DatastoreServiceEventsTest.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\datastore\Kernel;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\common\DataResource;
+use Drupal\common\Events\Event;
+use Drupal\common\Storage\DatabaseTableInterface;
+use Drupal\datastore\DatastoreService;
+use Drupal\datastore\Service\ResourceLocalizer;
+use Drupal\datastore\Storage\DatabaseTable;
+use Drupal\datastore\Storage\ImportJobStoreFactory;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @covers \Drupal\datastore\DatastoreService
+ * @coversDefaultClass \Drupal\datastore\DatastoreService
+ *
+ * @group dkan
+ * @group datastore
+ * @group kernel
+ */
+class DatastoreServiceEventsTest extends KernelTestBase implements EventSubscriberInterface {
+
+  protected static $modules = [
+    'common',
+    'datastore',
+    'metastore',
+  ];
+
+  /**
+   * Store the events we receive.
+   *
+   * @var array
+   */
+  protected array $events = [];
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      DatastoreService::EVENT_DATASTORE_PRE_DROP => 'catchPreDropEvent',
+      DatastoreService::EVENT_DATASTORE_DROPPED => 'catchDroppedEvent',
+    ];
+  }
+
+  /**
+   * Event handler.
+   *
+   * @param \Drupal\common\Events\Event $event
+   *   The event.
+   */
+  public function catchPreDropEvent(Event $event) {
+    $this->events[DatastoreService::EVENT_DATASTORE_PRE_DROP] = $event;
+  }
+
+  /**
+   * Event handler.
+   *
+   * @param \Drupal\common\Events\Event $event
+   *   The event.
+   */
+  public function catchDroppedEvent(Event $event) {
+    $this->events[DatastoreService::EVENT_DATASTORE_DROPPED] = $event;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function register(ContainerBuilder $container) {
+    parent::register($container);
+    $container->register('testing.datastore_drop_subscriber', self::class)
+      ->addTag('event_subscriber');
+    $container->set('testing.datastore_drop_subscriber', $this);
+  }
+
+  /**
+   * @covers ::drop
+   */
+  public function testEvents() {
+    // Mock a data resource.
+    $data_resource = $this->getMockBuilder(DataResource::class)
+      ->disableOriginalConstructor()
+      ->onlyMethods(['getUniqueIdentifier'])
+      ->getMock();
+
+    // Mock the resource localizer.
+    $resource_localizer = $this->getMockBuilder(ResourceLocalizer::class)
+      ->disableOriginalConstructor()
+      ->onlyMethods(['get'])
+      ->getMock();
+    $resource_localizer->expects($this->once())
+      ->method('get')
+      ->willReturn($data_resource);
+
+    // Mock the storage.
+    $storage = $this->getMockBuilder(DatabaseTable::class)
+      ->disableOriginalConstructor()
+      ->onlyMethods(['destruct'])
+      ->getMock();
+    $storage->expects($this->once())
+      ->method('destruct');
+
+    // Mock a database table so we don't need one.
+    $database_table = $this->getMockBuilder(DatabaseTableInterface::class)
+      ->onlyMethods(['remove'])
+      ->getMockForAbstractClass();
+    $database_table->expects($this->once())
+      ->method('remove');
+
+    // Mock a job store factory so we can avoid actually trying to drop an
+    // actual table.
+    $job_store_factory = $this->getMockBuilder(ImportJobStoreFactory::class)
+      ->disableOriginalConstructor()
+      ->onlyMethods(['getInstance'])
+      ->getMock();
+    $job_store_factory->expects($this->once())
+      ->method('getInstance')
+      ->willReturn($database_table);
+
+    // Mock the datastore service so we can isolate the dispatched events.
+    $datastore_service = $this->getMockBuilder(DatastoreService::class)
+      // We only want to mock a few of the services.
+      ->setConstructorArgs([
+        $resource_localizer,
+        $this->container->get('dkan.datastore.service.factory.import'),
+        $this->container->get('queue'),
+        $job_store_factory,
+        $this->container->get('dkan.datastore.service.resource_processor.dictionary_enforcer'),
+        $this->container->get('dkan.metastore.resource_mapper'),
+      ])
+      ->onlyMethods(['getStorage'])
+      ->getMock();
+    $datastore_service->expects($this->once())
+      ->method('getStorage')
+      ->willReturn($storage);
+
+    // This test class, being an event subscriber, will load up $this->events
+    // with the events we generated.
+    $this->assertCount(0, $this->events);
+    $datastore_service->drop('id', 'ver', FALSE);
+    // 2 events happened because of drop().
+    $this->assertCount(2, $this->events);
+
+    // Pre-drop happened first.
+    $this->assertEquals(
+      ['dkan_datastore_pre_drop', 'dkan_datastore_dropped'],
+      array_keys($this->events)
+    );
+
+    foreach ($this->events as $event) {
+      $this->assertIsArray($data = $event->getData());
+      // We don't know what the identifier or version actually will be, so we
+      // check that the data has the correct keys.
+      $this->assertEquals('id', $data['identifier']);
+      $this->assertEquals('ver', $data['version']);
+    }
+  }
+
+}
