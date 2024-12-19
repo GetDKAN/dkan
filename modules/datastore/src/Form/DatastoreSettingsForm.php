e6bf9d6a41a0f17c2b6497c08de363f4f2e3c7f5
Zach Edmonds <zachary.edmonds@civicactions.com>
2024-12-19 11:23:41 -0500

    update cypress test for datastore form to include new field

--- modules/datastore/src/Form/DatastoreSettingsForm.php
+++ modules/datastore/src/Form/DatastoreSettingsForm.php
@@ -65,15 +65,15 @@ class DatastoreSettingsForm extends ConfigFormBase {
       '#min' => 1,
       '#title' => $this->t('Rows limit'),
       '#default_value' => $this->config('datastore.settings')->get('rows_limit'),
-      '#description' => $this->t('Maximum number of rows the datastore endpoints can return 
-        in a single request. Caution: setting too high can lead to timeouts or memory issues. 
+      '#description' => $this->t('Maximum number of rows the datastore endpoints can return
+        in a single request. Caution: setting too high can lead to timeouts or memory issues.
         Default 500; values above 20,000 not recommended.'),
     ];
 
     $form['response_stream_max_age'] = [
       '#type' => 'number',
       '#title' => $this->t('Response Stream Max-Age'),
-      '#default_value' => $this->config('datastore.settings')->get('response_stream_max_age') ?: QueryController::DEFAULT_RESPONSE_STREAM_MAX_AGE,
+      '#default_value' => $this->config('datastore.settings')->get('response_stream_max_age'),
       '#min' => 0,
       '#description' => $this->t('Set the cache max-age for streaming CSV responses, in seconds. Default: 3600 (1 hour).'),
     ];
@@ -81,7 +81,7 @@ class DatastoreSettingsForm extends ConfigFormBase {
     $form['triggering_properties'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Datastore triggering properties'),
-      '#description' => $this->t('Metadata properties whose change will trigger a re-import of 
+      '#description' => $this->t('Metadata properties whose change will trigger a re-import of
         an associated resource to the datastore.'),
       '#options' => $this->schemaHelper->retrieveSchemaProperties('dataset'),
       '#default_value' => $this->config('datastore.settings')->get('triggering_properties'),
