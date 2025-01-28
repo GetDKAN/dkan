fc2dba11828f9e61d79501863bd1310cf556c54e
Rob Casey <robert.casey@civicactions.com>
2025-01-28 13:30:00 -0500

    Reverse lookup: data table name to dataset

--- modules/common/src/Commands/CommonCommands.php
+++ modules/common/src/Commands/CommonCommands.php
@@ -4,6 +4,11 @@ namespace Drupal\common\Commands;
 
 use Drupal\common\DatasetInfo;
 use Drush\Commands\DrushCommands;
+use Drupal\datastore\DatastoreService;
+use Drupal\Core\Database\Database;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Consolidation\OutputFormatters\StructuredData\UnstructuredListData;
+use Drupal\Core\StringTranslation\ByteSizeMarkup;
 
 /**
  * Drush commands providing utility common to DKAN's sub-modules.
@@ -41,6 +46,55 @@ class CommonCommands extends DrushCommands {
    */
   public function datasetInfo(string $uuid) {
     return json_encode($this->datasetInfo->gather($uuid), JSON_PRETTY_PRINT);
+  }
+
+  /**
+   * Return the dataset uuid associated with
+   * the provided data table name.
+   *
+   * Will do the following:
+   * - Deconstruct the data table id.
+   * -- datastore_
+   * -- identifier
+   * -- version
+   * -- perspective
+   * - Lookup the associated dataset uuid
+   * - Display the dataset uuid in the console.
+   *
+   * @param string $data_table_name
+   *   Data Table name, e.g., "datastore_8b7a21d442d603b113f1a17beac8bcdd".
+   *
+   * @command dkan:datastore:reverse-dataset-lookup
+   */
+  public function reverseDatasetLookup(string $data_table_name) {
+    if ($data_table_name) {
+
+      // Establish DB connection
+      $connection = \Drupal::database();
+
+      // Build the query
+      $query = $connection->select('dkan_metastore_resource_mapper', 'dm')
+        ->fields('dm', ['id', 'identifier', 'version', 'perspective']);
+
+        // Add the condition using a raw SQL expression.
+        $query->where(
+          'CONCAT(\'datastore_\', MD5(CONCAT(identifier, \'__\', version, \'__\', perspective))) = :data_table_name',
+          [':data_table_name' => $data_table_name]
+        );
+        
+        // Execute the query.
+        $results = $query->execute();
+
+        // Fetch the results as an array.
+        $data = $results->fetchAll(\PDO::FETCH_ASSOC);
+        return $data;
+    }
+    if ($data) {
+      $this->output()->writeln('Dataset Info: ' . $data);
+      return DrushCommands::EXIT_SUCCESS;
+    }
+    $this->output()->writeln('No resource for identifier: ' . $data_table_name);
+    return DrushCommands::EXIT_FAILURE;
   }
 
 }
