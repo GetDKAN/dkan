2499cb2a45b8b2a019c4593edb883feef7a5c362
Beth Jacobson <bethann@bajac.com>
2025-01-28 15:55:49 -0500

    Prevent infinite loop on 404.

--- modules/dkan_js_frontend/src/Controller/Page.php
+++ modules/dkan_js_frontend/src/Controller/Page.php
@@ -3,21 +3,25 @@
 namespace Drupal\dkan_js_frontend\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Path\CurrentPathStack;
 use Drupal\metastore\Exception\MissingObjectException;
 use Drupal\metastore\MetastoreService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * The Page controller.
  */
-class Page extends ControllerBase {
+class Page extends ControllerBase implements ContainerInjectionInterface {
 
   /**
    * Metastore service.
    */
-  private MetastoreService $service;
+  private MetastoreService $metastoreService;
+
+  protected RequestStack $requestStack;
 
   /**
    * The current path.
@@ -34,16 +38,18 @@ class Page extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('dkan.metastore.service'),
-      $container->get('path.current')
+      $container->get('path.current'),
+      $container->get('request_stack'),
     );
   }
 
   /**
    * Constructor.
    */
-  public function __construct(MetastoreService $service, CurrentPathStack $current_path) {
-    $this->service = $service;
+  public function __construct(MetastoreService $service, CurrentPathStack $current_path, RequestStack $request_stack) {
+    $this->metastoreService = $service;
     $this->currentPath = $current_path;
+    $this->requestStack = $request_stack;
   }
 
   /**
@@ -59,16 +65,19 @@ class Page extends ControllerBase {
 
     $path = $this->currentPath->getPath();
 
-    if (preg_match($dataset_data_path_match, $path, $matches)
-      || preg_match($dataset_path_match, $path, $matches)) {
 
-      try {
-        $this->service->get('dataset', $matches['id']);
-      }
-      catch (MissingObjectException $exception) {
-        throw new NotFoundHttpException();
-      }
-    }
+   if ($this->requestStack->getCurrentRequest()->query->get('_exception_statuscode') !== 404) {
+
+     if (preg_match($dataset_data_path_match, $path, $matches)
+       || preg_match($dataset_path_match, $path, $matches)) {
+
+       try {
+         $this->metastoreService->get('dataset', $matches['id']);
+       } catch (MissingObjectException $exception) {
+         throw new NotFoundHttpException();
+       }
+     }
+   }
 
     return [
       '#theme' => 'page__dkan_js_frontend',
