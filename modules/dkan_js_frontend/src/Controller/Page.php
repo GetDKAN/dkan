7393171290af1644f55357b0eff4b4f111cc503e
Beth Jacobson <bethann@bajac.com>
2025-01-29 13:14:43 -0500

    Expand test to include any path that starts with /dataset/ID.

--- modules/dkan_js_frontend/src/Controller/Page.php
+++ modules/dkan_js_frontend/src/Controller/Page.php
@@ -75,15 +75,12 @@ class Page extends ControllerBase implements ContainerInjectionInterface {
   protected function handleInvalidDatasetId() {
     // Path should always have leading slash.
     // @see \Symfony\Component\HttpFoundation\Request::getPathInfo()
-    // Dataset path is /dataset/[ID]/data.
-    $dataset_data_path_match = '/^\/dataset\/(?P<id>[^\/]+)\/data$/';
-    // Dataset path is /dataset/[ID].
-    $dataset_path_match = '/^\/dataset\/(?P<id>[^\/]+)$/';
+    // Match any path that equals or begins with /dataset/[ID].
+    $dataset_path_match = '/^\/dataset\/(?P<id>[^\/]+)/';
 
     $path = $this->currentPath->getPath();
 
-    if (preg_match($dataset_data_path_match, $path, $matches)
-      || preg_match($dataset_path_match, $path, $matches)) {
+    if (preg_match($dataset_path_match, $path, $matches)) {
       try {
         $this->metastoreService->get('dataset', $matches['id']);
       }
