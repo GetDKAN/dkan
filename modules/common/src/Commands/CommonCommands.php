81bbcf91efc7c2ae72be923d7bc63c5a1d74799d
Rob Casey <robert.casey@civicactions.com>
2025-02-12 15:55:28 -0500

    Remove commented out code, update each function to output more discriptively

--- modules/common/src/Commands/CommonCommands.php
+++ modules/common/src/Commands/CommonCommands.php
@@ -88,7 +88,7 @@ class CommonCommands extends DrushCommands {
         return $resource_identifier;
       }
       else {
-        return "Can not map data table name " . $data_table_name . " to resource ID.";
+        echo "Resource lookup: Can not map data table name " . $data_table_name . " to resource ID." . PHP_EOL;
       }
     }
   }
@@ -103,6 +103,12 @@ class CommonCommands extends DrushCommands {
    */
   public function resourceToDistribution(string $resource_id) {
     if ($resource_id) {
+      // Tack on an underscore to the
+      // end of the provided resource ID.
+      // We need this to search the json metadata table
+      // for the identifier property correctly without
+      // bringing back too many results.
+      // (An underscore always follows the complete resource ID).
       $search_id = $resource_id . '_';
       // Now we have our associated resource identifier so
       // Use it to find the associated distribution UUID
@@ -130,7 +136,7 @@ class CommonCommands extends DrushCommands {
         return $distribution_identifier;
       }
       else {
-        return "Can not map resource ID " . $resource_id . " to distribution UUID.";
+        return "Distribution lookup: Can not map resource ID " . $resource_id . " to distribution UUID." . PHP_EOL;
       }
     }
   }
@@ -170,10 +176,10 @@ class CommonCommands extends DrushCommands {
         return $dataset_identifier;
       }
       else {
-        return "Cannot map distribution UUID " . $distribution_uuid . " to dataset UUID.";
+        return "Dataset lookup: Cannot map distribution UUID " . $distribution_uuid . " to dataset UUID.";
       }
     }
-    return "Distribution ID needs to be 36 characters.";
+    return "Dataset lookup: Distribution UUID needs to be 36 characters.";
   }
 
   /**
@@ -196,6 +202,8 @@ class CommonCommands extends DrushCommands {
    * @command dkan:datastore:reverse-dataset-lookup
    */
   public function reverseDatasetLookup(string $data_table_name) {
+    $resource_id = '';
+    $distribution_uuid = '';
     if ($data_table_name) {
       $resource_id = $this->datatableToResourceLookup($data_table_name);
     }
@@ -205,96 +213,10 @@ class CommonCommands extends DrushCommands {
     if ($distribution_uuid) {
       $dataset_uuid = $this->distributionToDataset($distribution_uuid);
       // Output to console and end command.
-      $this->output()->writeln('Dataset UUID:' . PHP_EOL . $dataset_uuid);
+      $this->output()->writeln('Dataset UUID = ' . $dataset_uuid);
       return DrushCommands::EXIT_SUCCESS;
     }
     $this->output()->writeln('Can not map data table to dataset: ' . $data_table_name);
     return DrushCommands::EXIT_FAILURE;
   }
-
-
-
-
-
-
-      // Establish DB connection.
-      // $resource_query = $this->database->select('dkan_metastore_resource_mapper', 'dm')
-      //   ->fields('dm', ['identifier']);
-      // // Add the condition using a raw SQL expression.
-      // // We want just the identifier here which
-      // // is part of an amalgamation of an MD5 hash of the
-      // // identifier, version, and perspective
-      // // of the related resource which
-      // // in turn creates the data table name,
-      // // so we're reversing that with this query.
-      // $resource_query->where(
-      //   'CONCAT(\'datastore_\', MD5(CONCAT(identifier, \'__\', version, \'__\', perspective))) = :data_table_name',
-      //   [':data_table_name' => $data_table_name]
-      // );
-      // // Execute the query and fetch the results as an associative array.
-      // $resource_result = $resource_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-      // // If our query returns something...
-      // if ($resource_result) {
-      //   // Extract the identifier value
-      //   // from the returned associative array.
-      //   $resource_identifier = $resource_result[0]['identifier'];
-      //   // Echo for info's sake...
-      //   echo 'Associated Resource Identifier:' . PHP_EOL . $resource_identifier . PHP_EOL;
-      //   // Now we have our associated resource identifier so
-      //   // Use it to find the associated distribution UUID
-      //   // from the node__field_json_metadata table.
-      //   // Set our identifier as our search value for the query.
-      //   $search_value = $resource_identifier;
-      //   // Build the query for searching the json metadata table.
-      //   $distribution_query = $this->database->select('node__field_json_metadata', 'nfm');
-      //   // Add our JSON_EXTRACT expression
-      //   // targeting the identifier property.
-      //   $distribution_query->addExpression("JSON_UNQUOTE(JSON_EXTRACT(nfm.field_json_metadata_value, '$.identifier'))", 'identifier');
-      //   // Add a LIKE condition with our
-      //   // escaped search value (resource identifier).
-      //   $distribution_query->condition(
-      //     'nfm.field_json_metadata_value',
-      //     '%' . $this->database->escapeLike($search_value) . '%',
-      //     'LIKE'
-      //   );
-      //   // Get our result (distribution UUID) from our
-      //   // executed query as an associative array.
-      //   $distribution_result = $distribution_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-      //   // Extract the distribution identifier value
-      //   // from the associative array.
-      //   // This should only be one level deep.
-      //   $distribution_identifier = $distribution_result[0]['identifier'];
-      //   if ($distribution_identifier) {
-      //     // Echo for info's sake...
-      //     echo 'Associated Distribution UUID:' . PHP_EOL . $distribution_identifier . PHP_EOL;
-      //     // Now we have the distribution identifier,
-      //     // so lets get the dataset UUID
-      //     // Associated with it from
-      //     // the node__field_json_metadata table.
-      //     $dataset_query = $this->database->select('node__field_json_metadata', 'nfm');
-      //     // Add JSON_EXTRACT to get the identifier.
-      //     $dataset_query->addExpression("JSON_UNQUOTE(JSON_EXTRACT(nfm.field_json_metadata_value, '$.identifier'))", 'identifier');
-      //     // Add condition to check if the
-      //     // column contains the distribution UUID.
-      //     $dataset_query->condition(
-      //         'nfm.field_json_metadata_value',
-      //         '%' . $this->database->escapeLike($distribution_identifier) . '%',
-      //         'LIKE'
-      //     );
-      //     // Execute query.
-      //     $dataset_result = $dataset_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-      //     // Save the second item in the array to be the dataset identifier
-      //     // as this identifier is the dataset UUID related to the distribution
-      //     // which appears in the row that holds the distribution and dataset.
-      //     // The other is the row that holds the distribution and resource.
-      //     $dataset_identifier = $dataset_result[1]['identifier'];
-      //     if ($dataset_identifier) {
-      //       // Output to console and end command.
-      //       $this->output()->writeln('Dataset UUID:' . PHP_EOL . $dataset_identifier);
-      //       return DrushCommands::EXIT_SUCCESS;
-      //     }
-      //   }
-      // }
-      // $this->output()->writeln('Can not map data table to dataset: ' . $data_table_name);
-      // return DrushCommands::EXIT_FAILURE
 }
