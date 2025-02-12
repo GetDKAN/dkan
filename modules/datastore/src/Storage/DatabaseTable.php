9f661cbeb70b5c36e3b87ccc9e69f26caec81cf4
Dan Feder <dan.feder@civicactions.com>
2025-02-12 15:37:50 -0500

    mixed extra

--- modules/datastore/src/Storage/DatabaseTable.php
+++ modules/datastore/src/Storage/DatabaseTable.php
@@ -235,7 +235,7 @@ class DatabaseTable extends AbstractDatabaseTable implements \JsonSerializable {
    *
    * @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Database!database.api.php/group/schemaapi/9.2.x
    */
-  protected function translateType(string $describe_type, $extra = NULL) {
+  protected function translateType(string $describe_type, mixed $extra = NULL) {
     // Clean up things like "int(10) unsigned".
     $db_type = strtok($describe_type, ' ()');
     $driver = $this->connection->driver() ?? 'mysql';
