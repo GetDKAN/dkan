e428ffe21de7d774a96ae36350d43f151f7341d8
Rob Casey <robert.casey@civicactions.com>
2025-02-13 11:41:47 -0500

    Clear white space at end of comment lines

--- modules/common/src/Commands/CommonCommands.php
+++ modules/common/src/Commands/CommonCommands.php
@@ -59,7 +59,7 @@ class CommonCommands extends DrushCommands {
    *
    * @param string $data_table_name
    *   Data Table name, e.g., "datastore_8b7a21d442d603b113f1a17beac8bcdd".
-   * 
+   *
    * @throws \Exception
    *   If $data_table_name can not be found in DB.
    *
@@ -101,10 +101,10 @@ class CommonCommands extends DrushCommands {
    *
    * @param string $resource_id
    *   Resource ID, e.g., "6e5a8b0e5f9ae95d1e239844aaab2db4".
-   * 
+   *
    * @throws \Exception
    *   If $resource_id can not be found in DB.
-   * 
+   *
    * @command dkan:datastore:lookup-distribution
    */
   public function resourceToDistribution(string $resource_id) {
@@ -152,7 +152,7 @@ class CommonCommands extends DrushCommands {
    *
    * @param string $distribution_uuid
    *   Distribution ID, e.g., "d10163be-b7cc-5f76-a5e7-8d2bb4cda6bc".
-   * 
+   *
    * @throws \Exception
    *   If $distribution_id is not exactly 36 chars.
    *
