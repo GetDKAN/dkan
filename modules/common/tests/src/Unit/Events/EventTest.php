dd153a6fc3d04394632520e76497efd2fb6f62db
Zach Edmonds <zachary.edmonds@civicactions.com>
2025-02-28 13:00:19 -0500

    Updated EventTest

--- modules/common/tests/src/Unit/Events/EventTest.php
+++ modules/common/tests/src/Unit/Events/EventTest.php
@@ -2,24 +2,19 @@
 
 namespace Drupal\Tests\common\Unit\Events;
 
-use Drupal\common\EventDispatcherTrait;
 use Drupal\common\Events\Event;
 use MockChain\Chain;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class EventTest extends TestCase
 {
-  use EventDispatcherTrait;
-
   public function testDataIntegrityAcrossEventSubscribers() {
     $this->expectExceptionMessage("Invalid event data.");
-    $containerx = (new Chain($this))
-      ->add(Container::class)
-      ->getMock();
 
-    $dispatcher = new EventDispatcher($containerx);
+    $dispatcher = new EventDispatcher();
     $dispatcher->addListener('test_event', function(Event $event) {
       $event->setData(1);
     });
@@ -35,4 +30,27 @@ class EventTest extends TestCase
     });
   }
 
+  /**
+   * Mimics the original dispatchEvent() method from the deprecated trait.
+   *
+   * @param string   $eventName
+   *   The event name.
+   * @param mixed    $data
+   *   The initial event data.
+   * @param callable $validator
+   *   A callback that validates the event data.
+   *
+   * @throws \Exception
+   *   If the validator returns FALSE.
+   */
+  private function dispatchEvent($eventName, $data, callable $validator) {
+    /** @var EventDispatcherInterface $dispatcher */
+    $dispatcher = \Drupal::getContainer()->get('event_dispatcher');
+    $event = new Event($data);
+    $dispatcher->dispatch($event, $eventName);
+
+    if (!$validator($event->getData())) {
+      throw new \Exception("Invalid event data.");
+    }
+  }
 }
