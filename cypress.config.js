27c1482e1e8c90fa2fe0f20742b113478cafcc79
Kaise Lafrai <lafrai.k@gmail.com>
2025-03-12 15:15:28 -0400

    Migration from Cypress 8.7 to 14.1

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  apiUri: 'api/1',
+  env: {
+    TEST_USER_CREDENTIALS: {
+      user: 'testadmin',
+      pass: 'testadmin',
+    },
+    UUID_REGEX:
+      '^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
