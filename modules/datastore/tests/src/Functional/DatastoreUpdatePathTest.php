4d7f0a84c2e9c5fadac18edaef994fea3c2c07dd
Dan Feder <dan.feder@civicactions.com>
2025-03-14 15:46:29 -0400

    Finish up initial tests, fix datastore.install

---
+++ modules/datastore/tests/src/Functional/DatastoreUpdatePathTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\datastore\Functional;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+
+/**
+ * Tests update functions for the Database Logging module.
+ *
+ * @group dblog
+ */
+class DatastoreUpdatePathTest extends UpdatePathTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles() {
+    $this->databaseDumpFiles = [
+      __DIR__ . '/../../../../../tests/fixtures/update/update-2.20.0.php.gz',
+    ];
+  }
+
+  public function testUpdates9003on(): void {
+    $schema = \Drupal::database()->schema();
+    $config = \Drupal::configFactory()->getEditable('datastore.settings');
+
+    // Get a baseline for pre-9003.
+    $table = 'dkan_post_import_job_status';
+    $this->assertFalse($schema->fieldExists($table, 'id'));
+    $this->assertFalse($schema->fieldExists($table, 'timestamp'));
+
+    // Get baseline for pre-9005
+    $this->assertSame(1, $config->get('purge_file'));
+    $this->assertSame(1, $config->get('purge_table'));
+
+    $this->runUpdates();
+    $schema = \Drupal::database()->schema();
+    $config = \Drupal::configFactory()->getEditable('datastore.settings');
+
+    // Confirm results of update 9003.
+    $table = 'dkan_post_import_job_status';
+    $this->assertTrue($schema->fieldExists($table, 'id'));
+    $this->assertTrue($schema->fieldExists($table, 'timestamp'));
+
+    // Confirm results of update 9005 (purge settings now bools).
+    $this->assertSame(TRUE, $config->get('purge_file'));
+    $this->assertSame(TRUE, $config->get('purge_table'));
+  }
+}
---
+++ modules/metastore/tests/src/Functional/MetastoreUpdatePathTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\metastore\Functional;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+
+/**
+ * Tests update functions for the Database Logging module.
+ *
+ * @group dblog
+ */
+class MetastoreUpdatePathTest extends UpdatePathTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles() {
+    $this->databaseDumpFiles = [
+      __DIR__ . '/../../../../../tests/fixtures/update/update-2.20.0.php.gz',
+    ];
+  }
+
+  public function testUpdates8010on(): void {
+    $config = \Drupal::configFactory()->getEditable('metastore.settings');
+
+    // Get a baseline for pre-8010.
+    $this->assertNull($config->get('redirect_to_datasets'));
+
+    $this->runUpdates();
+
+    // Confirm results of update 8010.
+    $config = \Drupal::configFactory()->getEditable('metastore.settings');
+    $this->assertTrue($config->get('redirect_to_datasets'));
+  }
+}
