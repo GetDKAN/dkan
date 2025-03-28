b4408daa3b2fb2f94ef55530098d8aa26487f95f
Zach Edmonds <zachary.edmonds@civicactions.com>
2025-03-28 13:16:48 -0400

    updated getJsonForm to check weights

--- modules/json_form_widget/src/FormBuilder.php
+++ modules/json_form_widget/src/FormBuilder.php
@@ -103,18 +103,15 @@ class FormBuilder implements ContainerInjectionInterface {
    * Build form based on schema.
    */
   public function getJsonForm($data, $form_state = NULL) {
-    if (!$this->schema) {
-      return [];
-    }
+    if ($this->schema) {
 
-    // Get property names and weights (if available)
-    $properties = array_keys((array) $this->schema->properties);
-    $weights = $this->schemaUiHandler->getSchemaUi() ? $this->schemaUiHandler->getFieldWeights() : [];
+      $properties = array_keys((array) $this->schema->properties);
+      $weights = $this->schemaUiHandler->getSchemaUi() ? $this->schemaUiHandler->getFieldWeights() : [];
 
-    // Sort properties by weight
-    usort($properties, function ($a, $b) use ($weights) {
-      return ($weights[$a] ?? 0) <=> ($weights[$b] ?? 0);
-    });
+      // Sort properties by weight
+      usort($properties, function ($a, $b) use ($weights) {
+        return ($weights[$a] ?? 0) <=> ($weights[$b] ?? 0);
+      });
 
       foreach ($properties as $property) {
         $type = $this->schema->properties->{$property}->type ?? "string";
