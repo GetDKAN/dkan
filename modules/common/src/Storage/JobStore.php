bb81ee98919a1e76c3342b20b1acdaf39195c974
Zach Edmonds <zachary.edmonds@civicactions.com>
2025-04-04 11:24:54 -0400

    reordering constructor such that dispatcher is last

--- modules/common/src/Storage/JobStore.php
+++ modules/common/src/Storage/JobStore.php
@@ -27,16 +27,16 @@ class JobStore extends AbstractDatabaseTable {
    *   Table name for this jobstore table.
    * @param \Drupal\Core\Database\Connection $connection
    *   Database connection.
-   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
-   *   The event dispatcher service.
    * @param string $deprecatedTableName
    *   (Optional) Deprecated table name, if there is one.
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+   *    The event dispatcher service.
    */
   public function __construct(
     string $tableName,
     Connection $connection,
-    EventDispatcherInterface $eventDispatcher,
-    string $deprecatedTableName = ''
+    string $deprecatedTableName = '',
+    EventDispatcherInterface $eventDispatcher
   ) {
     $this->tableName = $tableName;
     $this->deprecatedTableName = $deprecatedTableName;
