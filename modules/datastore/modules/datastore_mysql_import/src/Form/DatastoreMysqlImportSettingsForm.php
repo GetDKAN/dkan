48277805464a2707e0dc40c74308c6ef5f3a9991
Paul Mitchum <paul@mile23.com>
2025-04-08 10:06:55 -0700

    reverts datastore_mysql_import

--- modules/datastore/modules/datastore_mysql_import/src/Form/DatastoreMysqlImportSettingsForm.php
+++ modules/datastore/modules/datastore_mysql_import/src/Form/DatastoreMysqlImportSettingsForm.php
@@ -38,12 +38,6 @@ class DatastoreMysqlImportSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Unlike the chunk harvester, which ignores empty rows in a CSV, the MySQL importer will import empty rows.'),
       '#default_value' => $config->get('remove_empty_rows'),
     ];
-    $form['load_data_chunk_size'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('LOAD DATA chunk size.'),
-      '#description' => $this->t('Large CSV data resources will be chunked and imported using LOAD DATA. This is the size of the chunk in records.'),
-      '#default_value' => $config->get('load_data_chunk_size'),
-    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -53,7 +47,6 @@ class DatastoreMysqlImportSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('datastore_mysql_import.settings')
       ->set('remove_empty_rows', $form_state->getValue('remove_empty_rows'))
-      ->set('load_data_chunk_size', $form_state->getValue('load_data_chunk_size'))
       ->save();
     parent::submitForm($form, $form_state);
   }
