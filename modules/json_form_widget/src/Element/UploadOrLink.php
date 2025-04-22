f51f0d82eb119b7882af30b7b89bf64b778df795
Dan Feder <dan.feder@civicactions.com>
2025-04-22 15:11:44 -0400

    Some fixes for UploadOrLink

--- modules/json_form_widget/src/Element/UploadOrLink.php
+++ modules/json_form_widget/src/Element/UploadOrLink.php
@@ -71,15 +71,18 @@ class UploadOrLink extends ManagedFile {
       $remove = TRUE;
     }
 
-    if (empty($input['fids']) && ($element['#uri'] ?? FALSE)) {
-      $file = static::getManagedFile(static::getFileUri($element['#uri']));
+    $uri = $element['#uri'] ?? $input['file_url_remote'] ?? FALSE;
+
+    if (empty($input['fids']) && $uri) {
+      $file = static::getManagedFile(static::getFileUri($uri));
       // If remove was clicked, we need to unset the uri. If not, we need to add
       // the fid to the input array.
       if ($remove) {
         $element['#uri'] = '';
+        $input['file_url_remote'] = '';
       }
       else {
-        // Add filet to input array and update the entity.
+        // Add fil ID to input array and update the entity.
         $fo = $form_state->getFormObject();
         $entity = $fo instanceof EntityFormInterface ? $fo->getEntity() : NULL;
         $input['fids'] = static::updateFile($file, $entity) ?? NULL;
@@ -392,7 +395,7 @@ class UploadOrLink extends ManagedFile {
       /** @var Drupal\file\FileUsage\FileUsageInterface $fu */
       $usage = $fu->listUsage($file);
       // If the file is already used by this entity, don't add usage again.
-      if (!isset($usage['json_form_widget'][$entity->getEntityTypeId()][$entity->id()])) {
+      if (!$entity->isNew() && !isset($usage['json_form_widget'][$entity->getEntityTypeId()][$entity->id()])) {
         $fu->add($file, 'json_form_widget', $entity->getEntityTypeId(), $entity->id());
       }
     }
