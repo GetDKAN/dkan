2bbb7f08334f726bd89fd67901632a48637bd1fe
Stefan Korn <drupal@stefan-korn.de>
2025-05-20 16:18:59 +0200

    Provide a way to hide actions via schema property

--- modules/json_form_widget/src/ArrayHelper.php
+++ modules/json_form_widget/src/ArrayHelper.php
@@ -255,7 +255,7 @@ class ArrayHelper implements ContainerInjectionInterface {
       'schema' => $definition['schema']->items,
     ];
     $element = $this->objectHelper->handleObjectElement($subdefinition, $data, $form_state, $context, $this->builder);
-    $element[$definition['name']]['actions'] = $this->buildElementActions($definition['name'], self::buildContextName($context));
+    $element[$definition['name']]['actions'] = empty($definition['schema']->no_actions) ? $this->buildElementActions($definition['name'], self::buildContextName($context)) : [];
     return $element;
   }
 
