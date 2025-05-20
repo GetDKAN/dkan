28166f389d37c5b132247b52fb89dd4fc6f4b767
Dan Feder <dan.feder@civicactions.com>
2025-05-20 14:36:06 -0400

    Revert "Add some fake methods to test code coverage"
    This reverts commit 8bcb24676b2f774fed20c188f9faccf3c5889103.

--- modules/common/src/FileFetcher/DkanFileFetcher.php
+++ modules/common/src/FileFetcher/DkanFileFetcher.php
@@ -10,38 +10,5 @@ use FileFetcher\FileFetcher;
  * @deprecated Uneeded. Use FileFetcher.
  */
 class DkanFileFetcher extends FileFetcher {
-  public function fakeMethod1() {
-    $a = 1;
-    $b = 2;
-    $c = $a + $b;
-    return $c;
-  }
 
-  public function fakeMethod2() {
-    $a = 1;
-    $b = 2;
-    $c = $a + $b;
-    return $c;
-  }
-
-  public function fakeMethod3() {
-    $a = 1;
-    $b = 2;
-    $c = $a + $b;
-    return $c;
-  }
-
-  public function fakeMethod4() {
-    $a = 1;
-    $b = 2;
-    $c = $a + $b;
-    return $c;
-  }
-
-  public function fakeMethod5() {
-    $a = 1;
-    $b = 2;
-    $c = $a + $b;
-    return $c;
-  }
 }
