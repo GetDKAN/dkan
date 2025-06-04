1af1a875d3260adc69d556f7a20a167899129878
Christia Troyer <christia.troyer@civicactions.com>
2025-06-04 00:05:13 -0700

    WCMS-25186: Update distributionToDataset method to use existing service

--- modules/datastore/src/DatastoreLookup.php
+++ modules/datastore/src/DatastoreLookup.php
@@ -95,39 +95,31 @@ class DatastoreLookup implements DatastoreLookupInterface {
   }
 
   /**
-   * {@inheritDoc}
+   * Get the dataset UUID for a given distribution UUID.
+   *
+   * @param string $distribution_id
+   *   The UUID of the distribution node.
+   *
+   * @return string
+   *   The UUID of the dataset node.
+   *
+   * @throws \RuntimeException
+   *   If no dataset is found.
    */
-  public function distributionToDataset(string $distribution_uuid): string {
-    if ($distribution_uuid && strlen($distribution_uuid) == 36) {
-      // Now we have the distribution identifier,
-      // so lets get the dataset UUID
-      // Associated with it from
-      // the node__field_json_metadata table.
-      $dataset_query = $this->database->select('node__field_json_metadata', 'nfm');
-      // Add JSON_EXTRACT to get the identifier.
-      $dataset_query->addExpression("JSON_UNQUOTE(JSON_EXTRACT(nfm.field_json_metadata_value, '$.identifier'))", 'identifier');
-      // Add condition to check if the
-      // column contains the distribution UUID.
-      $dataset_query->condition(
-          'nfm.field_json_metadata_value',
-          '%' . $this->database->escapeLike($distribution_uuid) . '%',
-          'LIKE'
-      );
-      // Execute query.
-      $dataset_result = $dataset_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-      // Save the second item in the array to be the dataset identifier
-      // as this identifier is the dataset UUID related to the distribution
-      // which appears in the row that holds the distribution and dataset.
-      // The other is the row that holds the distribution and resource.
-      if ($dataset_result) {
-        $dataset_identifier = $dataset_result[1]['identifier'];
-        return $dataset_identifier;
-      }
-      else {
-        throw new \Exception("Dataset lookup: Cannot map distribution UUID {$distribution_uuid} to dataset UUID. Please make sure the expected distribution exists in the daatabase.");
-      }
+  public function distributionToDataset(string $distribution_id): string {
+    if (strlen($distribution_id) !== 36) {
+      throw new \InvalidArgumentException("Dataset lookup: Distribution UUID must be 36 characters.");
     }
-    throw new \Exception("Dataset lookup: Distribution UUID needs to be 36 characters.");
+
+    $referencers = $this->referenceLookup->getReferencers($distribution_id, 'dcat:dataset');
+
+    if (empty($referencers)) {
+      throw new \RuntimeException("No dataset found for distribution ID: {$distribution_id}");
+    }
+
+    return $referencers[0];
   }
+
+
 
 }
