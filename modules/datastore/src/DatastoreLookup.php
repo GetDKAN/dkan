70a85087f563ea8844cb5f3a42dcbc8ea308360c
Dan Feder <dan.feder@civicactions.com>
2025-06-09 16:32:13 -0400

    A little cleanup

--- modules/datastore/src/DatastoreLookup.php
+++ modules/datastore/src/DatastoreLookup.php
@@ -32,6 +32,8 @@ class DatastoreLookup implements DatastoreLookupInterface {
    *
    * @param \Drupal\Core\Database\Connection $database
    *   Database connection service.
+   * @param \Drupal\metastore\Reference\ReferenceLookup $referenceLookup
+   *   Reference lookup service.
    */
   public function __construct(Connection $database, ReferenceLookup $referenceLookup) {
     $this->database = $database;
@@ -42,27 +44,24 @@ class DatastoreLookup implements DatastoreLookupInterface {
    * {@inheritDoc}
    */
   public function tableToResourceLookup(string $table_name): string {
-    // Maps the datastore table name to the resource ID
-    if ($table_name) {
-      // Establish DB connection.
-      $resource_query = $this->database->select('dkan_metastore_resource_mapper', 'dm')
-        ->fields('dm', ['identifier']);
-      // Add the condition using a raw SQL expression.
-      $resource_query->where(
-        'CONCAT(\'datastore_\', MD5(CONCAT(identifier, \'__\', version, \'__\', perspective))) = :table_name',
-        [':table_name' => $table_name]
-      );
-      // Execute the query and fetch the results as an associative array.
-      $resource_result = $resource_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-      // Extract the identifier value
-      if ($resource_result) {
-        $resource_identifier = $resource_result[0]['identifier'];
-        return $resource_identifier;
-      }
-      else {
-        throw new \Exception("Resource lookup: Can not map datastore table name {$table_name}
-        to resource ID. Please make sure your datastore table name exists as a table in the database.");
-      }
+    // Establish DB connection.
+    $resource_query = $this->database->select('dkan_metastore_resource_mapper', 'dm')
+      ->fields('dm', ['identifier']);
+    // Add the condition using a raw SQL expression.
+    $resource_query->where(
+      'CONCAT(\'datastore_\', MD5(CONCAT(identifier, \'__\', version, \'__\', perspective))) = :table_name',
+      [':table_name' => $table_name]
+    );
+    // Execute the query and fetch the results as an associative array.
+    $resource_result = $resource_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+    // Extract the identifier value.
+    if ($resource_result) {
+      $resource_identifier = $resource_result[0]['identifier'];
+      return $resource_identifier;
+    }
+    else {
+      throw new \Exception("Resource lookup: Can not map datastore table name {$table_name}
+      to resource ID. Please make sure your datastore table name exists as a table in the database.");
     }
   }
 
@@ -79,7 +78,7 @@ class DatastoreLookup implements DatastoreLookupInterface {
    *   If no distribution is found.
    */
   public function resourceToDistribution(string $resource_id): string {
-    // Maps the resource ID to the distribution ID
+    // Maps the resource ID to the distribution ID.
     $referencers = $this->referenceLookup->getReferencers('distribution', $resource_id, 'downloadURL');
     if (empty($referencers)) {
       throw new \RuntimeException("Distribution lookup: Can not map resource ID {$resource_id}
@@ -102,7 +101,7 @@ class DatastoreLookup implements DatastoreLookupInterface {
    *   If no dataset is found.
    */
   public function distributionToDataset(string $distribution_id): string {
-    // Maps the distribution ID to the dataset
+    // Maps the distribution ID to the dataset.
     if (strlen($distribution_id) !== 36) {
       throw new \InvalidArgumentException("Dataset lookup: Distribution UUID must be 36 characters.");
     }
