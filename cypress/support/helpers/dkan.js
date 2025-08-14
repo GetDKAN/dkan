a2eb12c597c0d6a9f61782ebbfa72d29936a63b7
Janette Day <janette.day@civicactions.com>
2025-08-14 16:26:01 -0500

    Wait for the dialog to display before clicking yes

--- cypress/support/helpers/dkan.js
+++ cypress/support/helpers/dkan.js
@@ -242,7 +242,7 @@ export function createDatasetWithModerationState(dataset_title, moderation_state
     .click({ force:true })
   // Dialog will only show if we're using published, click yes.
   if (moderation_state == 'published') {
-    cy.get('.button').contains('Yes')
+    cy.get('.button', { timeout: 10000 }).contains('Yes')
       .click({ force:true })
   }
   cy.get('.messages--status')
