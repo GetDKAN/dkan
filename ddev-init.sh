f9f2fcefa947521bd15f4f807d581d86fc46d7b6
Dan Feder <dan.feder@civicactions.com>
2025-10-20 11:44:38 -0400

    Remove set -x, add message at end of ddev-init

--- ddev-init.sh
+++ ddev-init.sh
@@ -7,7 +7,7 @@
 # Usage: ddev-init [DRUPAL_VERSION]
 # Example: ddev-init 10.5
 DRUPAL_VERSION=${1:-10.4}
-set -ex
+set -e
 
 # Validate semver format (X.Y or X.Y.Z)
 if [[ ! $DRUPAL_VERSION =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
@@ -18,8 +18,8 @@ fi
 DRUPAL_MAJOR_VERSION=$(echo "$DRUPAL_VERSION" | cut -d'.' -f1)
 ddev config --project-type=drupal$DRUPAL_MAJOR_VERSION --docroot=web --corepack-enable
 ddev add-on get ddev/ddev-drupal-contrib
-ddev dotenv set .ddev/.env.web --drupal-core $DRUPAL_VERSION 
-ddev dotenv set .ddev/.env.web --drupal-root "/var/www/html/web"
+ddev dotenv set .ddev/.env.web --drupal-core $DRUPAL_VERSION \
+  --drupal-root "/var/www/html/web"
 
 # Set up the ddev project. See https://github.com/ddev/ddev-drupal-contrib
 ddev start
@@ -28,3 +28,5 @@ ddev select2
 ddev symlink-project
 ddev config --update
 ddev restart
+
+echo -e "\033[33mRun 'ddev dkan-site-install' to finish setting up your DKAN site.\033[0m"
