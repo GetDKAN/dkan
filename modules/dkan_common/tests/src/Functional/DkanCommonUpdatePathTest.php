b4b334fc6d50c4a9d0f14fc66856488a7d0d7cb0
Dan Feder <dan.feder@civicactions.com>
2025-10-22 14:02:02 -0400

    More sensible common update path test

--- modules/dkan_common/tests/src/Functional/DkanCommonUpdatePathTest.php
+++ modules/dkan_common/tests/src/Functional/DkanCommonUpdatePathTest.php
@@ -25,36 +25,33 @@ class DkanCommonUpdatePathTest extends UpdatePathTestBase {
   }
 
   /**
-   * Test metastore module update 8010.
+   * Test dkan_common module update 10000.
    */
   public function testRenameUpdate(): void {
     // Drop cache container file to avoid issues with module moving.
-    \Drupal::database()->truncate('cache_container')->execute();
+    // \Drupal::database()->truncate('cache_container')->execute();
 
+    // Make sure site isn't broken before update.
     $this->drupalGet('<front>');
     $this->assertSession()->pageTextContains('Log in');
 
-    $config = \Drupal::configFactory()->getEditable('common.settings');
-    $this->assertFalse($config->get('always_use_existing_local_perspective'));
-    // Assert common module is installed
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('common'));
-
-    // Set always_use_existing_local_perspective to TRUE to ensure it survives
-    // the update.
-    $config->set('always_use_existing_local_perspective', TRUE);
-    $config->save();
-
-    // Get a baseline for pre-8010.
-    $this->assertTrue($config->get('always_use_existing_local_perspective'));
+    // Get a baseline for pre-10000. Check if exists in extension config.
+    $config = \Drupal::configFactory()->getEditable('core.extension');
+    $modules = $config->get('module');
+    $this->assertArrayHasKey('common', $modules);
+    // Assert common settings config exists.
+    $this->assertNotEmpty(\Drupal::configFactory()->get('common.settings')->getRawData());
 
     $this->runUpdates();
 
     // Confirm common is disabled
     $this->assertFalse(\Drupal::moduleHandler()->moduleExists('common'));
-    // Assert setting copied over
-    $newConfig = \Drupal::configFactory()->getEditable('dkan_common.settings');
-    $this->assertTrue($newConfig->get('always_use_existing_local_perspective'));
-
+    // Confirm common now gone from extension config.
+    $config = \Drupal::configFactory()->getEditable('core.extension');
+    $modules = $config->get('module');
+    $this->assertArrayNotHasKey('common', $modules);
+    // Confirm common settings config has been deleted.
+    $this->assertEmpty(\Drupal::configFactory()->get('common.settings')->getRawData());
   }
 
 }
