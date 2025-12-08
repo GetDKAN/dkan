f4bd3241c672e2dd98b53931cc0a4a6898848e45
Stefan Korn <drupal@stefan-korn.de>
2025-12-08 17:25:54 +0100

    #4614: Add resource id to Datastore Import-Status Dashboard

--- modules/datastore/src/Form/DashboardForm.php
+++ modules/datastore/src/Form/DashboardForm.php
@@ -575,6 +575,7 @@ class DashboardForm extends FormBase {
         'data' => [
           '#theme' => 'datastore_dashboard_resource_cell',
           '#uuid' => $dist['distribution_uuid'],
+          '#resource_id' => $dist['resource_id'],
           '#file_name' => basename((string) $dist['source_path']),
           '#file_path' => UrlHostTokenResolver::resolve($dist['source_path']),
         ],
