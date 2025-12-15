8110b1fb0688b9c936aeb467400139344d28b5f4
Dan Feder <dan.feder@civicactions.com>
2025-12-15 10:12:06 -0500

    More test

--- tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
+++ tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
@@ -59,6 +59,7 @@ class Dkan4xTransitionalUpdatePathTest extends UpdatePathTestBase {
     // Assert legacy modules are now uninstalled and their config removed.
     $this->assertFalse(\Drupal::moduleHandler()->moduleExists('common'));
     $this->assertFalse(\Drupal::moduleHandler()->moduleExists('metastore'));
+    $this->assertFalse(\Drupal::moduleHandler()->moduleExists('metastore_admin'));
     
     $config = \Drupal::configFactory()->getEditable('core.extension');
     $modules = $config->get('module');
@@ -69,11 +70,19 @@ class Dkan4xTransitionalUpdatePathTest extends UpdatePathTestBase {
     $this->assertEmpty(\Drupal::configFactory()->get('metastore.settings')->getRawData());
 
     // Misc checks to ensure config was migrated properly.
+  
     // Open /node/add/dataset and check that metastore fields exist.
     $this->drupalLogin($this->rootUser);
     $this->drupalGet('node/add/data');
     $this->assertSession()->fieldExists('edit-field-json-metadata-0-value-title');
     $this->assertSession()->fieldExists('edit-field-json-metadata-0-value-description');
+  
+    // Visit /admin/dkan/datasets and check that the page loads, contains a
+    // table with headers including "Title" and "Data Type".
+    $this->drupalGet('admin/dkan/datasets');
+    $this->assertSession()->elementTextContains('css', 'h1', 'DKAN Metastore (Datasets)');
+    $this->assertSession()->elementExists('css', 'th a:contains("Title")');
+    $this->assertSession()->elementExists('css', 'th a:contains("Data Type")');
   }
 
 }
