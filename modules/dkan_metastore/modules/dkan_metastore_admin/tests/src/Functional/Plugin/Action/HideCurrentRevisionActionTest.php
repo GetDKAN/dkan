df1787fd041bae74e70c5d823a87d90b95aef15b
Dan Feder <dan.feder@civicactions.com>
2026-01-08 13:24:25 -0500

    Refactor metastore_admin submodules (#4638)

---
+++ modules/dkan_metastore/modules/dkan_metastore_admin/tests/src/Functional/Plugin/Action/HideCurrentRevisionActionTest.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Drupal\Tests\dkan_metastore_admin\Functional\Plugin\Action;
+
+use Drupal\Core\Session\AccountProxy;
+use Drupal\dkan_metastore\MetastoreService;
+use Drupal\dkan_metastore\ValidMetadataFactory;
+use Drupal\dkan_metastore_admin\Plugin\Action\HideCurrentRevisionAction;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\dkan_metastore\Unit\MetastoreServiceTest;
+use Drupal\user\Entity\User;
+use RootedData\RootedJsonData;
+
+/**
+ * NodeData Functional Tests.
+ *
+ * @package Drupal\Tests\dkan\Functional
+ * @group dkan
+ * @group functional3
+ */
+class HideCurrentRevisionActionTest extends BrowserTestBase {
+
+  protected static $modules = [
+    'datastore',
+    'dkan_metastore_admin',
+    'node',
+  ];
+
+  protected $defaultTheme = 'stark';
+
+  protected ValidMetadataFactory $validMetadataFactory;
+  protected User $testUser;
+  protected User $testApiUser;
+
+  private const S3_PREFIX = 'https://dkan-default-content-files.s3.amazonaws.com/phpunit';
+
+  public function setUp(): void {
+    parent::setUp();
+    $this->setDefaultModerationState('published');
+
+    $this->testUser = $this->createUser([], 'testadmin', TRUE, ['mail' => 'testadmin@test.com']);
+    $this->testApiUser = $this->createUser([], 'testapiuser', FALSE, ['roles' => ['api_user'], 'mail' => 'testapiuser@test.com']);
+
+    $this->validMetadataFactory = MetastoreServiceTest::getValidMetadataFactory($this);
+  }
+
+  /**
+   * Test resource removal on distribution deleting.
+   */
+  public function testHide() {
+    $node_storage = $this->container->get('entity_type.manager')->getStorage('node');
+    $datasetRootedJsonData = $this->getData('456', 'Test Published', []);
+    $this->getMetastore()->post('dataset', $datasetRootedJsonData);
+    $result = $node_storage->loadByProperties(['uuid' => '456']);
+    $node = current($result);
+
+    $accountProxy = new AccountProxy($this->container->get('event_dispatcher'));
+    $accountProxy->setAccount($this->testUser);
+    $this->container->set('current_user', $accountProxy);
+
+    $hideCurrentRevisionAction = HideCurrentRevisionAction::create(
+      $this->container,
+      [],
+      'hide_current_revision_action',
+      [
+        'type' => 'node',
+        'class' => HideCurrentRevisionAction::class,
+        'confirm' => TRUE,
+        'provider' => 'dkan_metastore_admin',
+      ]
+    );
+
+    $this->assertEquals('published', $node->get('moderation_state')->getString());
+
+    $hideCurrentRevisionAction->execute($node);
+
+    $this->assertEquals('hidden', $node->get('moderation_state')->getString());
+
+    $this->setDefaultModerationState('draft');
+    $datasetRootedJsonData = $this->getData('457', 'Test Unpublished', []);
+    $this->getMetastore()->post('dataset', $datasetRootedJsonData);
+    $result = $node_storage->loadByProperties(['uuid' => '457']);
+    $node = current($result);
+    $this->assertEquals('draft', $node->get('moderation_state')->getString());
+    $hideCurrentRevisionAction->execute($node);
+    $this->assertEquals('hidden', $node->get('moderation_state')->getString());
+
+    $this->setDefaultModerationState('published');
+    $datasetRootedJsonData = $this->getData('458', 'Test Published no access', []);
+    $this->getMetastore()->post('dataset', $datasetRootedJsonData);
+    $result = $node_storage->loadByProperties(['uuid' => '458']);
+    $node = current($result);
+
+    $accountProxy = new AccountProxy($this->container->get('event_dispatcher'));
+    $accountProxy->setAccount($this->testApiUser);
+    $this->container->set('current_user', $accountProxy);
+    $hideCurrentRevisionAction = HideCurrentRevisionAction::create(
+      $this->container,
+      [],
+      'hide_current_revision_action',
+      [
+        'type' => 'node',
+        'class' => HideCurrentRevisionAction::class,
+        'confirm' => TRUE,
+        'provider' => 'dkan_metastore_admin',
+      ]
+    );
+
+    $this->assertEquals('published', $node->get('moderation_state')->getString());
+    $hideCurrentRevisionAction->execute($node);
+    $this->assertEquals('published', $node->get('moderation_state')->getString());
+
+  }
+
+  /**
+   * Generate dataset metadata, possibly with multiple distributions.
+   *
+   * @param string $identifier
+   *   Dataset identifier.
+   * @param string $title
+   *   Dataset title.
+   * @param array $downloadUrls
+   *   Array of resource files URLs for this dataset.
+   *
+   * @return \RootedData\RootedJsonData
+   *   Json encoded string of this dataset's metadata, or FALSE if error.
+   */
+  private function getData(string $identifier, string $title, array $downloadUrls): RootedJsonData {
+
+    $data = new \stdClass();
+    $data->title = $title;
+    $data->description = 'Some description.';
+    $data->identifier = $identifier;
+    $data->accessLevel = 'public';
+    $data->modified = '06-04-2020';
+    $data->keyword = ['some keyword'];
+    $data->distribution = [];
+
+    foreach ($downloadUrls as $key => $downloadUrl) {
+      $distribution = new \stdClass();
+      $distribution->title = 'Distribution #' . $key . ' for ' . $identifier;
+      $distribution->downloadURL = $this->getDownloadUrl($downloadUrl);
+      $distribution->mediaType = 'text/csv';
+
+      $data->distribution[] = $distribution;
+    }
+
+    return $this->validMetadataFactory->get(json_encode($data), 'dataset');
+  }
+
+  private function getMetastore(): MetastoreService {
+    return \Drupal::service('dkan.metastore.service');
+  }
+
+  private function setDefaultModerationState($state = 'published') {
+    $this->config('workflows.workflow.dkan_publishing')
+      ->set('type_settings.default_moderation_state', $state)
+      ->save();
+  }
+
+  private function getDownloadUrl(string $filename) {
+    return self::S3_PREFIX . '/' . $filename;
+  }
+
+}
---
+++ modules/dkan_metastore/modules/dkan_metastore_search/src/Plugin/DkanApiDocs/MetastoreSearchApiDocs.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\dkan_metastore_search\Plugin\DkanApiDocs;
+
+use Drupal\dkan_common\Plugin\DkanApiDocsBase;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\dkan_metastore_search\Search;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Docs plugin.
+ *
+ * @DkanApiDocs(
+ *  id = "dkan_metastore_search_api_docs",
+ *  description = "Search docs"
+ * )
+ *
+ * @codeCoverageIgnore
+ */
+class MetastoreSearchApiDocs extends DkanApiDocsBase {
+
+  /**
+   * Metastore search service.
+   */
+  private Search $metastoreSearch;
+
+  /**
+   * Constructs a \Drupal\Component\Plugin\PluginBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $pluginId
+   *   The plugin_id for the plugin instance.
+   * @param mixed $pluginDefinition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The module handler service.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
+   *   The module handler service.
+   * @param \Drupal\metastore_search\Search $metastoreSearch
+   *   The metastore search service.
+   */
+  public function __construct(
+    array $configuration,
+    $pluginId,
+    $pluginDefinition,
+    ModuleHandlerInterface $moduleHandler,
+    TranslationInterface $stringTranslation,
+    Search $metastoreSearch,
+  ) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition, $moduleHandler, $stringTranslation);
+    $this->metastoreSearch = $metastoreSearch;
+  }
+
+  /**
+   * Container injection.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The service container.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $pluginId
+   *   The plugin_id for the plugin instance.
+   * @param mixed $pluginDefinition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $pluginId,
+    $pluginDefinition,
+  ) {
+    return new static(
+      $configuration,
+      $pluginId,
+      $pluginDefinition,
+      $container->get('module_handler'),
+      $container->get('string_translation'),
+      $container->get('dkan.metastore_search.service')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function spec() {
+    $spec = $this->getDoc('dkan_metastore_search');
+
+    $propList = $this->t('Available properties: %list', [
+      '%list' => implode(", ", $this->getIndexedFields()),
+    ]);
+    $spec['paths']['/api/1/search']['get']['parameters'][3]['description'] .= " $propList";
+
+    $facetTypes = $this->getFacetTypes();
+    foreach ($facetTypes as $type => $example) {
+      $spec['paths']['/api/1/search']['get']['parameters'][] = [
+        'name' => $type,
+        'in' => 'query',
+        'description' => $this->t("Filter results using %facet facet.", ['%facet' => $type]),
+        'schema' => ['type' => 'string'],
+        'example' => $example,
+        'style' => 'form',
+      ];
+    }
+    return $spec;
+  }
+
+  /**
+   * Get names of availale facets.
+   *
+   * @return array
+   *   Array of facet names.
+   */
+  private function getFacetTypes(): array {
+    $facets = $this->metastoreSearch->facets();
+    $types = [];
+    foreach ($facets as $facet) {
+      $types[$facet->type] = $facet->name;
+    }
+    return $types;
+  }
+
+  /**
+   * Get a list of indexed fields for sort enum.
+   *
+   * @return array
+   *   All fields indexed in search API.
+   *
+   * @todo Add this function directly to search service so no Search API specific.
+   */
+  private function getIndexedFields(): array {
+    $index = $this->metastoreSearch->getSearchIndex();
+    $fields = array_keys($index->getFields());
+    $fields[] = 'search_api_relevance';
+    return $fields;
+  }
+
+}
