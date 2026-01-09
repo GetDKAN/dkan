fdfe3f6e4cd631ad9951c9cd36dabc57ca83db6f
Dan Feder <dan.feder@civicactions.com>
2026-01-09 14:40:57 -0500

    Update some paths

--- modules/dkan_metastore/tests/src/Functional/Plugin/Field/FieldWidget/AdminDatasetFileUploadTest.php
+++ modules/dkan_metastore/tests/src/Functional/Plugin/Field/FieldWidget/AdminDatasetFileUploadTest.php
@@ -106,7 +106,7 @@ class AdminDatasetFileUploadTest extends JsonFormTestBase {
    */
   public function testCreateDatasetWithFileUpload() {
     // The file we'll upload.
-    $upload_file = realpath(dirname(__DIR__, 7) . '/datastore/tests/data/Bike_Lane.csv');
+    $upload_file = realpath(dirname(__DIR__, 7) . '/dkan_datastore/tests/data/Bike_Lane.csv');
 
     $assert = $this->assertSession();
 
