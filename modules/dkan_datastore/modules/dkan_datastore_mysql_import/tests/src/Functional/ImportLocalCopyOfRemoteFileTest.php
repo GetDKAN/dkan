55bcd6428ab606c3553f3fc3b0cab38f4fd8ee1f
Dan Feder <dan.feder@civicactions.com>
2026-01-13 13:28:08 -0500

    Refactor datastore module as dkan_datastore (#4643)

---
+++ modules/dkan_datastore/modules/dkan_datastore_mysql_import/tests/src/Functional/ImportLocalCopyOfRemoteFileTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\dkan_datastore_mysql_import\Functional;
+
+use Drupal\Tests\dkan_datastore\Functional\ImportLocalCopyOfRemoteFileTest as ParentTest;
+
+/**
+ * Test dataset import when using existing localized files.
+ *
+ * @group datastore_mysql_import
+ * @group btb
+ * @group functional
+ * @group functional3
+ */
+class ImportLocalCopyOfRemoteFileTest extends ParentTest {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'dkan_common',
+    'dkan_datastore',
+    'dkan_datastore_mysql_import',
+    'dkan_metastore',
+    'node',
+  ];
+
+  protected $defaultTheme = 'stark';
+
+}
---
+++ modules/dkan_datastore/tests/src/Unit/Controller/AbstractQueryControllerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\Tests\dkan_common\Unit\Util;
+
+use Drupal\dkan_datastore\Controller\AbstractQueryController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @group dkan
+ * @group datastore
+ * @group unit
+ */
+class AbstractQueryControllerTest extends TestCase {
+
+  /**
+   * Make sure we get what we expect with a GET
+   */
+  public function testGetNormalizer() {
+    $queryString = "conditions[0][property]=state&conditions[0][value]=AL&conditions[0][operator]==&conditions[1][property]=record_number&conditions[1][value]=%1%&conditions[1][operator]=LIKE&sort[0][property]=record_number&sort[0][order]=asc&sort[1][property]=state&sort[1][order]=desc&limit=50&offset=25&results=true";
+
+    $request = Request::create("http://example.com?$queryString", "GET");
+    $requestJson = AbstractQueryController::getPayloadJson($request);
+    $this->assertEquals($requestJson, $this->getSampleJson());
+  }
+
+  /**
+   * Make sure we get what we expect with a POST
+   */
+  public function testPostNormalizer() {
+    $sampleJson = $this->getSampleJson();
+    $schema = $this->getSampleSchema();
+    $request = Request::create("http://example.com", "POST", [], [], [], [], $sampleJson);
+    $requestJson = AbstractQueryController::getPayloadJson($request, $schema);
+    $this->assertEquals($requestJson, $sampleJson);
+  }
+
+  /**
+   * Make sure we get what we expect with a PATCH
+   */
+  public function testPatchNormalizer() {
+    $sampleJson = $this->getSampleJson();
+    $schema = $this->getSampleSchema();
+
+    $request = Request::create("http://example.com", "PATCH", [], [], [], [], $sampleJson);
+    $requestJson = AbstractQueryController::getPayloadJson($request, $schema);
+    $this->assertEquals($requestJson, $sampleJson);
+  }
+
+  /**
+   * Make sure we get what we expect with a DELETE
+   */
+  public function testDeleteNormalizer() {
+    $this->expectExceptionMessage("Only POST, PUT, PATCH and GET requests can be normalized");
+    $schema = $this->getSampleSchema();
+
+    $request = Request::create("http://example.com", "DELETE");
+    AbstractQueryController::getPayloadJson($request, $schema);
+  }
+
+  /**
+   * Make sure we get what we expect with a PUT
+   */
+  public function testPutNormalizer() {
+    $sampleJson = $this->getSampleJson();
+    $schema = $this->getSampleSchema();
+
+    $request = Request::create("http://example.com", "PUT", [], [], [], [], $sampleJson);
+    $requestJson = AbstractQueryController::getPayloadJson($request, $schema);
+    $this->assertEquals($requestJson, $sampleJson);
+  }
+
+  /**
+   * Make sure we get what we expect with invalid JSON.
+   */
+  public function testInvalidJson() {
+    $this->expectException(\InvalidArgumentException::class);
+    $this->expectExceptionMessage('Invalid JSON');
+    $sampleJson = $this->getBadJson();
+    $schema = $this->getSampleSchema();
+    $request = Request::create("http://example.com", "POST", [], [], [], [], $sampleJson);
+    AbstractQueryController::getPayloadJson($request, $schema);
+  }
+
+  private function getSampleJson() {
+    return file_get_contents(__DIR__ . "/../../../data/query.json");
+  }
+
+  private function getBadJson() {
+    return file_get_contents(__DIR__ . "/../../../data/query/invalidJson.json");
+  }
+
+  private function getSampleSchema() {
+    return file_get_contents(__DIR__ . "/../../../data/querySchema.json");
+  }
+
+}
