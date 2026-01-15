2bc143bd85ec2afc3e94a26edc199c143244465f
Dan Feder <dan.feder@civicactions.com>
2026-01-15 16:13:24 -0500

    Add sample_content module

--- tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
+++ tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
@@ -34,6 +34,7 @@ class Dkan4xTransitionalUpdatePathTest extends UpdatePathTestBase {
     // Enable optional modules.
     \Drupal::service('module_installer')->install(['metastore_facets']);
     \Drupal::service('module_installer')->install(['datastore_mysql_import']);
+    \Drupal::service('module_installer')->install(['sample_content']);
 
     // Run all updates.
     $this->runUpdates();
@@ -47,6 +48,7 @@ class Dkan4xTransitionalUpdatePathTest extends UpdatePathTestBase {
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_datastore'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_datastore_mysql_import'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_harvest'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_sample_content'));
   }
 
 }
