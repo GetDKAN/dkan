8d85b5e0998b396ce87559690d836090ee50f70d
Dan Feder <dan.feder@civicactions.com>
2026-02-05 15:51:20 -0500

    Add assertion for dkan_data_dictionary_widget

--- tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
+++ tests/src/Functional/Dkan4xTransitionalUpdatePathTest.php
@@ -32,22 +32,25 @@ class Dkan4xTransitionalUpdatePathTest extends UpdatePathTestBase {
     $this->assertSession()->pageTextContains('Log in');
 
     // Enable optional modules.
-    \Drupal::service('module_installer')->install(['metastore_facets']);
-    \Drupal::service('module_installer')->install(['datastore_mysql_import']);
-    \Drupal::service('module_installer')->install(['sample_content']);
+    \Drupal::service('module_installer')->install([
+      'metastore_facets',
+      'datastore_mysql_import',
+      'sample_content',
+    ]);
 
     // Run all updates.
     $this->runUpdates();
 
     // Assert transitional modules are now installed.
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_common'));
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore'));
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_search'));
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_facets'));
-    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_admin'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_datastore'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_datastore_mysql_import'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_harvest'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_data_dictionary_widget'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_admin'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_facets'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_metastore_search'));
     $this->assertTrue(\Drupal::moduleHandler()->moduleExists('dkan_sample_content'));
   }
 
