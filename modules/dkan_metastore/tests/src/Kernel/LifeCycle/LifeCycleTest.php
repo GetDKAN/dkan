2205917af3daaa2672cf4ecf9ded5d34eb69448e
Dan Feder <dan.feder@civicactions.com>
2026-04-28 07:53:25 -0400

    LifeCycle test

---
+++ modules/dkan_metastore/tests/src/Kernel/LifeCycle/LifeCycleTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\dkan_metastore\LifeCycle;
+
+use Drupal\dkan_common\DataResource;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\dkan_common\Traits\QueueRunnerTrait;
+
+/**
+ * @group dkan
+ * @group dkan_metastore
+ * @group kernel
+ *
+ * @covers \Drupal\dkan_metastore\LifeCycle\LifeCycle
+ * @coversDefaultClass \Drupal\dkan_metastore\LifeCycle\LifeCycle
+ */
+class LifeCycleTest extends KernelTestBase {
+  use QueueRunnerTrait;
+
+  protected const DATASET_DATA = [
+    'title' => 'Test Dataset',
+    'identifier' => '123',
+    'description' => 'Test Description',
+    'modified' => '2026-01-01',
+    'accessLevel' => 'public',
+    'keyword' => ['test'],
+    'distribution' => [
+      [
+        'title' => 'Test Distribution 1',
+        'downloadURL' => 'http://example.com/1.csv',
+      ],
+    ],
+    "publisher" => [
+      "@type" => "org:Organization",
+      "name" => "Test Org",
+    ],
+    "theme" => [
+      "Tag 1",
+      "Tag 2",
+    ],
+  ];
+
+
+  public static $modules = [
+    'system',
+    'node',
+    'user',
+    'field',
+    'filter',
+    'text',
+    'dkan_metastore',
+    'dkan_common',
+    'dkan',
+    'content_moderation',
+    'workflows',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installConfig('system');
+    $this->installConfig('node');
+    $this->installConfig('dkan_common');
+    $this->installConfig('dkan_metastore');
+    $this->installEntitySchema('node');
+    $this->installSchema('node', ['node_access']);
+    $this->installEntitySchema('content_moderation_state');
+    $this->installConfig('field');
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('resource_mapping');
+
+    $config = $this->config('dkan_metastore.settings');
+    $config->set('orphan.delete', TRUE);
+    $config->save();
+  }
+
+  /**
+   * Make sure that distributionLoad properly creates references.
+   */
+  public function testDistributionLoad() {
+    /**
+     * @var \Drupal\dkan_metastore\MetastoreService $metastore
+     */
+    $metastore = $this->container->get('dkan.metastore.service');
+    $metadata = $metastore->getValidMetadataFactory()->get(json_encode(self::DATASET_DATA), 'dataset');
+    $identifier = $metastore->post('dataset', $metadata);
+    $result = $this->container->get('entity_type.manager')
+      ->getStorage('node')
+      ->loadByProperties(['type' => 'data', 'uuid' => $identifier]);
+    $node = reset($result);
+    // Get the raw value from the database for field_json_metadata.
+    $query = $this->container->get('database')->query(
+      'SELECT field_json_metadata_value FROM {node__field_json_metadata} WHERE entity_id = :entity_id',
+      [':entity_id' => $node->id()]
+    );
+    $json_raw = $query->fetchField();
+    $dataset_raw = json_decode($json_raw, TRUE);
+    $distribution_id = $dataset_raw['distribution'][0];
+    $result = $this->container->get('entity_type.manager')
+      ->getStorage('node')
+      ->loadByProperties(['type' => 'data', 'uuid' => $distribution_id]);
+    $distribution_node = reset($result);
+    $query = $this->container->get('database')->query(
+      'SELECT field_json_metadata_value FROM {node__field_json_metadata} WHERE entity_id = :entity_id',
+      [':entity_id' => $distribution_node->id()]
+    );
+    $json_raw = $query->fetchField();
+    $distribution_raw = json_decode($json_raw, TRUE);
+    $download_url_ref = $distribution_raw['data']['downloadURL'];
+    $resource_parts = DataResource::parseUniqueIdentifier($download_url_ref);
+    $resource_identifier = $resource_parts['identifier'];
+    $resource_version = $resource_parts['version'];
+    $resource_perspective = $resource_parts['perspective'];
+
+    // Delete the resource mapping entity.
+    $storage = $this->container->get('entity_type.manager')->getStorage('resource_mapping');
+    $entities = $storage->loadByProperties([
+      'identifier' => $resource_identifier,
+      'version' => $resource_version,
+      'perspective' => $resource_perspective,
+    ]);
+    foreach ($entities as $entity) {
+      $entity->delete();
+    }
+    $this->assertEmpty(
+      $storage->loadByProperties([
+        'identifier' => $resource_identifier,
+        'version' => $resource_version,
+        'perspective' => $resource_perspective,
+      ]),
+      'Resource mapping entities should be deleted before reload.'
+    );
+
+    // Clear the node cache.
+    $this->container->get('cache.entity')->deleteAll();
+
+    // Reset node storage static cache for this request.
+    $this->container->get('entity_type.manager')->getStorage('node')->resetCache();
+
+    // Clear the resource_mapping static cache for this request.
+    $this->container->get('entity_type.manager')->getStorage('resource_mapping')->resetCache();
+
+    // Re-load the original dataset via the metastore service
+    $dataset = $metastore->get('dataset', $identifier);
+    $retrieved = json_decode((string) $dataset);
+    $download_url = $retrieved->distribution[0]->downloadURL ?? NULL;
+    $this->assertSame(
+      '',
+      $download_url,
+      'Expected empty downloadURL when resource mapping is missing.'
+    );
+  }
+
+}
