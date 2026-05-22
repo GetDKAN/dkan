3787370b34122fe3792b8a33c5c963e24f2ac966
Dan Feder <dan.feder@civicactions.com>
2026-05-22 11:48:24 -0400

    Fix PageTest 2

--- modules/dkan_js_frontend/tests/src/Kernel/Controller/PageTest.php
+++ modules/dkan_js_frontend/tests/src/Kernel/Controller/PageTest.php
@@ -73,6 +73,12 @@ class PageTest extends KernelTestBase {
    * @covers ::content
    */
   public function testPathPresent() {
+    $metastore = $this->container->get('dkan.metastore.service');
+    $metadata = $metastore->getValidMetadataFactory()->get(json_encode($this->getDataset('123')), 'dataset');
+    $metastore->post('dataset', $metadata);
+    $item = $this->container->get('dkan.metastore.metastore_item_factory')->getInstance('123');
+    $nid = $item->getEntity()->id();
+
     $route_match = $this->getMockBuilder(RouteMatch::class)
       ->disableOriginalConstructor()
       ->getMock();
@@ -87,9 +93,37 @@ class PageTest extends KernelTestBase {
     $this->assertEquals(
       [
         '#theme' => 'page__dkan_js_frontend',
+        '#cache' => [
+          'max-age' => -1,
+          'contexts' => [],
+          'tags' => [
+            'config:dkan_js_frontend.config',
+            'node:' . $nid,
+          ],
+        ],
       ],
       $page->content($route_match, (new Request())),
     );
   }
 
+  protected function getDataset(string $identifier): array {
+    return [
+      'title' => 'Test Dataset',
+      'identifier' => $identifier,
+      'keyword' => ['test'],
+      'description' => 'Test Description',
+      'modified' => '2020-01-01',
+      'accessLevel' => 'public',
+      'distribution' => [
+        [
+          'title' => 'Test Distribution 1',
+          'downloadURL' => 'http://example.com/1.csv',
+        ],
+        [
+          'title' => 'Test Distribution 2',
+          'downloadURL' => 'http://example.com/2.csv',
+        ],
+      ],
+    ];
+  }
 }
