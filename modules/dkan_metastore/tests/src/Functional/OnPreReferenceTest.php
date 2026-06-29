23726b3a95e5dd9a76338fde09b75c79b6b848cc
Dan Feder <dan.feder@civicactions.com>
2026-06-29 16:49:59 -0500

    Check new revision with and w/o ref

--- modules/dkan_metastore/tests/src/Functional/OnPreReferenceTest.php
+++ modules/dkan_metastore/tests/src/Functional/OnPreReferenceTest.php
@@ -5,10 +5,11 @@ namespace Drupal\Tests\dkan_metastore\Functional;
 use Drupal\Tests\BrowserTestBase;
 
 /**
+ * Test the pre-reference event subscriber.
+ *
  * @group dkan
- * @group metastore
+ * @group dkan_metastore
  * @group functional
- * @group btb
  * @group functional1
  */
 class OnPreReferenceTest extends BrowserTestBase {
@@ -16,7 +17,6 @@ class OnPreReferenceTest extends BrowserTestBase {
   protected static $modules = [
     'dkan_datastore',
     'dkan_metastore',
-    'node',
   ];
 
   protected $defaultTheme = 'stark';
@@ -24,9 +24,9 @@ class OnPreReferenceTest extends BrowserTestBase {
   private $downloadUrl = 'https://dkan-default-content-files.s3.amazonaws.com/phpunit/district_centerpoints_small.csv';
 
   /**
-   *
+   * Get the dataset metadata for testing.
    */
-  private function getData($downloadUrl) {
+  private function getData(string $downloadUrl): string {
     return '
     {
       "title": "Test #1",
@@ -46,12 +46,21 @@ class OnPreReferenceTest extends BrowserTestBase {
   }
 
   /**
+   * Test the pre-reference event subscriber.
    *
+   * @dataProvider distributionReferenceProvider
    */
-  public function test() {
+  public function testPreReference($distribution_reference) {
     // Ensure the proper triggering properties are set for datastore comparison.
     $this->config('dkan_datastore.settings')
       ->set('triggering_properties', ['modified'])
+      ->save();
+
+    // Set the "distribution" property list item to use references or not.
+    $property_list = $this->config('dkan_metastore.settings')->get('property_list');
+    $property_list['distribution'] = $distribution_reference;
+    $this->config('dkan_metastore.settings')
+      ->set('property_list', $property_list)
       ->save();
 
     // Test posting a dataset to the metastore.
@@ -70,6 +79,20 @@ class OnPreReferenceTest extends BrowserTestBase {
 
     $rev = drupal_static('metastore_resource_mapper_new_revision');
     $this->assertEquals(1, $rev);
+  }
+
+  /**
+   * Two versions of metastore settings.
+   *
+   * Setting dkan_metastore.settings.property_list.distribution to "0" means
+   * we don't reference distributions. Tests that the pre-reference event
+   * subscriber still works in that case.
+   */
+  public static function distributionReferenceProvider() {
+    return [
+      ['distribution'],
+      ['0'],
+    ];
   }
 
 }
