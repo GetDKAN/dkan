9708d9717088d87217374225e44ed39221bc493d
Dan Feder <dan.feder@civicactions.com>
2026-06-30 10:58:39 -0500

    Foundational work for non-referenced distributions (#4745)

---
+++ modules/dkan_metastore/src/LifeCycle/ResourceDiscovery/SkippedEntryReasons.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\dkan_metastore\LifeCycle\ResourceDiscovery;
+
+/**
+ * Enum for reasons why a resource discovery entry was skipped.
+ *
+ * May someday include UnsupportedType, but for now that logic lives closer to
+ * the datastore.
+ */
+enum SkippedEntryReasons {
+  case InvalidUrl;
+  case MissingUrl;
+}
