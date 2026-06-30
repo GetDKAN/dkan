208db59b3fc409b88d32453353b6cd2915e9434d
Dan Feder <dan.feder@civicactions.com>
2026-06-30 10:58:39 -0500

    Foundational work for non-referenced distributions (#4745)

---
+++ modules/dkan_metastore/src/LifeCycle/ResourceDiscovery/ResourceDiscoveryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\dkan_metastore\LifeCycle\ResourceDiscovery;
+
+/**
+ * Interface for resource discovery classes.
+ */
+interface ResourceDiscoveryInterface {
+
+  /**
+   * Discover resources from a given metadata object.
+   *
+   * @param object $metadata
+   *   The json_decoded metadata object from which to discover resources.
+   *
+   * @return \Drupal\dkan_metastore\LifeCycle\ResourceDiscovery\ResourceDiscoveryResult
+   *   The result of the resource discovery process.
+   */
+  public function discoverResources(object $metadata): ResourceDiscoveryResult;
+
+}
