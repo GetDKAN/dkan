f326f87c09721aa2bd6b363ef6bba3f3b1401edc
Dan Feder <dan.feder@civicactions.com>
2026-07-13 10:28:35 -0500

    Remove HarvestUtility dead codce

--- modules/dkan_harvest/src/HarvestUtility.php
+++ modules/dkan_harvest/src/HarvestUtility.php
@@ -4,7 +4,6 @@ namespace Drupal\dkan_harvest;
 
 use Drupal\Component\Uuid\UuidInterface;
 use Drupal\Core\Database\Connection;
-use Drupal\Core\Database\Statement\FetchAs;
 use Drupal\dkan_harvest\Entity\HarvestRunRepository;
 use Drupal\dkan_harvest\Storage\DatabaseTableFactory;
 use Drupal\dkan_harvest\Storage\HarvestHashesDatabaseTableFactory;
@@ -301,76 +300,6 @@ class HarvestUtility {
       $this->logger->notice('Updating namespace for ' . $plan_id);
       $this->updateTypeNamespace($plan_id);
     }
-  }
-
-  /**
-   * Get the ids from the temp harvest run table.
-   *
-   * Only needed for harvest_update_8010.
-   *
-   * @param mixed $table_name_temp
-   *   The name of the temp table.
-   *
-   * @return array
-   *   The ids of all the harvest runs in the table sorted oldest to newest.
-   */
-  public function getTempRunIdsForUpdate($table_name_temp) : array {
-    $query = $this->connection->select($table_name_temp, 'hrt')
-      ->fields('hrt', ['id'])
-      ->orderBy('id', 'ASC');
-    $result = $query->execute()->fetchCol(0);
-    // Can't rely on orderBy as the sort ends up natural, not numeric.
-    asort($result, SORT_NUMERIC);
-
-    return $result ?? [];
-  }
-
-  /**
-   * Reads a single harvest row from the harvest run temp table.
-   *
-   * Only needed for harvest_update_8010.
-   *
-   * @param string $table_name_temp
-   *   Name of the table to read from.
-   * @param string $timestamp
-   *   The id to read from, which was also the timestamp.
-   *
-   * @return array
-   *   Elements from the row['id', 'harvest_plan_id', 'data', 'extract_status'].
-   */
-  public function readTempHarvestRunForUpdate(string $table_name_temp, string $timestamp): array {
-    $query = $this->connection->select($table_name_temp, 'hrt')
-      ->fields('hrt', ['id', 'harvest_plan_id', 'data', 'extract_status'])
-      ->condition('id', $timestamp, '=')
-      ->orderBy('id', 'ASC');
-    $result = $query->execute()->fetchAll(FetchAs::Associative);
-    return reset($result);
-  }
-
-  /**
-   * Creates a new entry in harvest_run based on data from harvest run temp.
-   *
-   * Only needed for harvest_update_8010.
-   *
-   * @param string $timestamp
-   *   The id from the old harvest run, which was a timestamp.
-   * @param string $harvest_plan_id
-   *   The harvest plan id.
-   * @param string $data
-   *   Data about the harvest.
-   * @param string $extract_status
-   *   The status of the harvest.
-   */
-  public function writeHarvestRunFromUpdate(string $timestamp, string $harvest_plan_id, string $data, string $extract_status): void {
-    $this->connection->insert('harvest_runs')
-      ->fields([
-        'timestamp' => (int) $timestamp,
-        'harvest_plan_id' => $harvest_plan_id,
-        'uuid' => $this->uuidService->generate(),
-        'data' => $data,
-        'extract_status' => $extract_status,
-      ])
-      ->execute();
   }
 
 }
