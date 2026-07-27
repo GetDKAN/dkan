6ddd38faaaa11d2ca5c5700559ed5877c7ec3db5
Dan Feder <dan.feder@civicactions.com>
2026-07-27 12:40:22 -0500

    First attempt at OrphanCheckerTest

--- modules/dkan_metastore/tests/src/Functional/OrphanCheckerTest.php
+++ modules/dkan_metastore/tests/src/Functional/OrphanCheckerTest.php
@@ -5,7 +5,9 @@ namespace Drupal\Tests\dkan_metastore\Functional;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\dkan_common\Traits\GetLocalDataTrait;
 use Drupal\Tests\dkan_common\Traits\QueueRunnerTrait;
+use Drupal\Tests\dkan_common\Traits\DistributionReferenceModeTrait;
 use Drupal\Tests\dkan_metastore\Unit\MetastoreServiceTest;
+use RootedData\RootedJsonData;
 
 /**
  * @group dkan
@@ -17,6 +19,7 @@ use Drupal\Tests\dkan_metastore\Unit\MetastoreServiceTest;
 class OrphanCheckerTest extends BrowserTestBase {
   use GetLocalDataTrait;
   use QueueRunnerTrait;
+  use DistributionReferenceModeTrait;
 
   protected static $modules = [
     'dkan_datastore',
@@ -26,7 +29,83 @@ class OrphanCheckerTest extends BrowserTestBase {
 
   protected $defaultTheme = 'stark';
 
-  public function test() {
+  /**
+   * Test orphan handling when distribution is removed from dataset.
+   *
+   * @dataProvider distributionReferenceProvider
+   */
+  public function testOrphanedResourceCleanup(string $distribution_reference) {
+    $this->setDistributionReferenceModeFromConfig($distribution_reference);
+    if ($distribution_reference === '0') {
+      $this->markTestSkipped('Skipping orphan cleanup test for distribution reference mode "id".');
+    }
+
+    $validMetadataFactory = MetastoreServiceTest::getValidMetadataFactory($this);
+    /** @var \Drupal\dkan_metastore\MetastoreService $service */
+    $service = $this->container->get('dkan.metastore.service');
+    $connection = $this->container->get('database');
+
+    // Create and post a dataset with a valid distribution/downloadURL.
+    $dataset_id = '123';
+    $dataset_data = $validMetadataFactory->get(
+      $this->getDataset($dataset_id, 'Test Dataset', ['district_centerpoints_small.csv']),
+      'dataset'
+    );
+    $service->post('dataset', $dataset_data);
+
+    // Run initial queues to localize and import the resource.
+    $this->runQueues(['localize_import', 'datastore_import']);
+
+    // Get dataset info to find the datastore table name and resource ID.
+    $datasetInfo = $this->container->get('dkan.common.dataset_info');
+    $metadata = $datasetInfo->gather($dataset_id);
+    $distribution_info = $metadata['latest_revision']['distributions'][0];
+    $table_name = $distribution_info['table_name'];
+    $resource_id = $distribution_info['resource_id'];
+    $resource_version = $distribution_info['resource_version'];
+
+    // Verify the datastore table exists.
+    $this->assertTrue(
+      $connection->schema()->tableExists($table_name),
+      "Datastore table $table_name should exist after initial import."
+    );
+
+    // Now make the distribution orphaned by removing the downloadURL
+    // (making it non-viable for import).
+    $full_dataset_json = $service->get('dataset', $dataset_id);
+    $full_dataset = json_decode($full_dataset_json);
+    // Remove the downloadURL to invalidate the distribution
+    unset($full_dataset->distribution[0]->downloadURL);
+    $service->patch('dataset', $dataset_id, json_encode($full_dataset));
+
+    // Run the orphan reference processor and resource purger queues.
+    $this->runQueues([
+      'orphan_reference_processor',
+      'resource_purger',
+      'orphan_resource_remover',
+    ]);
+
+    // Verify the datastore table has been dropped.
+    $this->assertFalse(
+      $connection->schema()->tableExists($table_name),
+      "Datastore table $table_name should not exist after orphan cleanup."
+    );
+
+    // Verify the resource is no longer in the resource mapper.
+    // Query for the latest version of the resource (without specifying version,
+    // which avoids the timestamp stale-version issue).
+    $resource_mapper = $this->container->get('dkan.metastore.resource_mapper');
+    $resource = $resource_mapper->get($resource_id, 'source');
+    $this->assertNull(
+      $resource,
+      "Resource $resource_id should no longer exist in the resource mapper after orphan cleanup."
+    );
+  }
+
+  /**
+   * Legacy test: basic orphan reference processor execution.
+   */
+  public function testOrphanReferenceProcessorExecution() {
     $validMetadataFactory = MetastoreServiceTest::getValidMetadataFactory($this);
     /** @var \Drupal\dkan_metastore\MetastoreService $service */
     $service = $this->container->get('dkan.metastore.service');
